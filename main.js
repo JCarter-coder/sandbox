671c1c81af8772378b8d07e183bc8dbef7432996
Jesse_Carter <jessetcarter@hotmail.com>
2024-02-15 11:00:57 -0700

    solved

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
-var rearrangeArray = function(nums) {
-    let container = {};
-    container.pos = [];
-    container.neg = [];
-    let arr = [];
-    for (let i = 0; i < nums.length; i++) {
-      if (nums[i] >= 0) {
-        container.pos.push(nums[i]);
-      } else container.neg.push(nums[i]);
-    }
-    for (let j = 0; j < nums.length/2; j++) {
-      arr.push(container.pos[j]);
-      arr.push(container.neg[j]);
-    }
-    //console.log(container.pos);
-    //console.log(container.neg);
-    console.log(arr);
+var promiseAll = function(functions) {
+    return new Promise((resolve, reject) => {
+      if (functions.length === 0) {
+        resolve([]);
+        return;
+      }
+      const res = new Array(functions.length).fill(null);
+
+      let resolvedCount = 0;
+
+      functions.forEach((el, idx) => {
+        el().then((subResult) => {
+          res[idx] = subResult;
+          resolvedCount++;
+          if (resolvedCount === functions.length) {
+            resolve(res);
+          }
+        }).catch((err) => {
+          reject(err);
+        })
+      });
+    })
 };
 
 rearrangeArray([3,1,-2,-5,2,-4]);
