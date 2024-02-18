5f6e3b55e1edf8fb4c5123da3d23c3203d36d18e
Jesse_Carter <jessetcarter@hotmail.com>
2024-02-18 16:22:35 -0700

    solved

--- main.js
+++ main.js
@@ -1,110 +1,51 @@
-var longestString = function(x, y, z) {
-    let sum = 0;
-    if (x >= y && y >= z) {
-        if (z > 0) {
-            // iterations of set x y z (e.g. "AABBAB")
-            sum += (2*3) * z;
-            x -= z;
-            y -= z;
-        }
-        // add y sets of x y
-        sum += (2*2) * y;
-        x -= y;
-        if (x > 0) {
-            sum += 2;
-        }
-        // return
-        console.log(sum);
-    } else if (x >= z && z >= y) {
-        if (y > 0) {
-            // iterations of set z x y (e.g. "ABAABB")
-            sum += (2*3) * y;
-            x -= y;
-            z -= y;
-        }
-        if (z > 0) {
-            // add remaining z
-            sum += 2 * z;
-        }
-        // add one x and return
-        if (x > 0) {
-            sum += 2;
-        }
-        // return
-        console.log(sum);
-    } else if (y >= x && x >= z) {
-        if (z > 0) {
-            //iterations of set y z x (e.g. "BBABAA")
-            sum += (2*3) * z;
-            y -= z;
-            x -= z;
-        }
-        // add x sets of y x
-        sum += (2*2) * x;
-        y -= x;
-        if (y > 0) {
-            sum += 2;
-        }
-        // return
-        console.log(sum);
-    } else if (y >= z && z >= x) {
-        // start at y if y is greater than x
-        if (y > x) {
-            sum += 2;
-            y -= 1;
-        }
-        if (x > 0) {
-            // iterations of set z x y (e.g. "ABAABB")
-            sum += (2*3) * x;
-            y -= x;
-            z -= x;
-        }
-        if (z > 0) {
-            // add remaining z
-            sum += 2 * z;
-        }
-        // return
-        console.log(sum);
-    } else if (z >= x && x >= y) {
-        if (y > 0) {
-            //iterations of set z x y (e.g. "ABAABB")
-            sum += (2*3) * y;
-            z -= y;
-            x -= y;
-        }
-        // add remaining z
-        if (z > 0) {
-            sum += 2 * z;
-        }
-        if (x > 0) {
-            // add one more x
-            sum += 2;
-        }
-        // return
-        console.log(sum);
-    } else if (z >= y && y >= x) {
-        if (x > 0) {
-            // iterations of set y z x (e.g. "BBABAA")
-            sum += (2*3) * x;
-            y -= x;
-            z -= x;
-        }
-        // add one more y
-        if (y > 0) {
-            sum += 2;
-        }
-        if (z > 0) {
-            // add remaining z
-            sum += 2 * z;
-        }
-        // return
-        console.log(sum);
+var mostBooked = function(n, meetings) {
+    //console.log(meetings);
+    let roomAvailabilityTime = [];
+    let meetingCount = [];
+    for (let a = 0; a < n; a++) {
+        roomAvailabilityTime.push(0);
+        meetingCount.push(0);
     }
+    meetings.sort((a,b) => a[0] - b[0]);
+    for (let meeting of meetings) {
+        let start = meeting[0];
+        let end = meeting[1];
+        let minRoomAvailabilityTime = Infinity;
+        let minAvailableTimeRoom = 0;
+        let foundUnusedRoom = false;
+
+        for (let i = 0; i < n; i++) {
+            if (roomAvailabilityTime[i] <= start) {
+                foundUnusedRoom = true;
+                meetingCount[i]++;
+                roomAvailabilityTime[i] = end;
+                break;
+            }
+
+            if (minRoomAvailabilityTime > roomAvailabilityTime[i]) {
+                minRoomAvailabilityTime = roomAvailabilityTime[i];
+                minAvailableTimeRoom = i;
+            }
+        }
+
+        if (!foundUnusedRoom) {
+            roomAvailabilityTime[minAvailableTimeRoom] += end - start;
+            meetingCount[minAvailableTimeRoom]++;
+        }
+    }
+    let maxMeetingCount = 0;
+    let maxMeetingCountRoom = 0;
+    for (let j = 0; j < n; j++) {
+        if (meetingCount[j] > maxMeetingCount) {
+            maxMeetingCount = meetingCount[j];
+            maxMeetingCountRoom = j;
+        }
+    }
+
+    console.log(maxMeetingCountRoom);
 };
 
-//longestString(2,5,1);
-//longestString(3,2,2);
-//longestString(1,39,14);
-//longestString(11,11,3);
-longestString(3,44,44);
+mostBooked(2,[[0,10],[1,5],[2,7],[3,4]]);
+mostBooked(3,[[1,20],[2,10],[3,5],[4,9],[6,8]]);
+
 
