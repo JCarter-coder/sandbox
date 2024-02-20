a6d2fc314802a2aa33beb0d88a11cb35eb0d558c
Jesse_Carter <jessetcarter@hotmail.com>
2024-02-20 08:27:49 -0700

    solved

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-var isPowerOfTwo = function(n) {
-    console.log(Math.log2(n));  
-    if (Math.log2(n) % 1 === 0) {
-        console.log('true');
-    } else console.log('false');
+var missingNumber = function(nums) {
+    let low = 0;
+    let hi = Math.max(...nums);
+    console.log(`[${low}, ${hi}]`)
+    nums.sort((a,b) => a - b);
+    console.log(nums);
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === (low + i) - low) {
+            continue;
+        } else console.log(i + low);
+    }
+    console.log(hi + 1);
 };
 
-isPowerOfTwo(1);
-isPowerOfTwo(16);
-isPowerOfTwo(3);
+missingNumber([3,0,1]);
+missingNumber([1]);
+missingNumber([9,6,4,2,3,5,7,0,1]);
