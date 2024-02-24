d3d16bd9d36d2c7b0befeb5285ad935df3782f97
Jesse_Carter <jessetcarter@hotmail.com>
2024-02-23 21:47:00 -0700

    solved

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 var maximumNumberOfStringPairs = function(words) {
-    
+    let container = {};
+    let pair = 0;
+    for (let i = 0; i < words.length; i++) {
+        if (Object.hasOwn(container, (words[i][1] + words[i][0]))) {
+            pair += 1;
+        } else if (!container[words[i]]) {
+            container[words[i]] = 1;
+        }
+    }
+    console.log(pair);
 };
 
 maximumNumberOfStringPairs(["cd","ac","dc","ca","zz"]);
