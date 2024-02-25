3298a481282ba69c3c972701fe39e15f0119c3ac
Jesse_Carter <jessetcarter@hotmail.com>
2024-02-24 22:28:51 -0700

    edge cases

--- main.js
+++ main.js
@@ -1,56 +1,24 @@
-var findAllPeople = function(n, meetings, firstPerson) {
-    // sort meetings by the time, i.e. meetings[element][2]
-    meetings.sort((a, b) => a[2] - b[2]);
-    let knowSecret = [0];
-    let concurrentMeetings = {};
-    let meetTime = meetings[0][2];
-    //console.log(meetTime);
-    //console.log(meetings);
-    for (let i = 0; i < meetings.length; i++) {
-        // if meetTime has changed
-        if (meetings[i][2] !== meetTime) {
-            // evaluate if anyone in concurrentMeetings
-            // knows the secret 
-            
-            console.log(concurrentMeetings);
-            // evaluate if secret is known by any in concurrentMeetings
-                // if true, 
-                    // push all people to the knowSecret list
-
-                // empty concurrentMeetings and continue
-            
-
-            // if false (i.e. concurrentMeetings === [])
-            
-            // empty concurrentMeetings then add the new people
-            concurrentMeetings = {};
-            concurrentMeetings[meetings[i][0]] = 1;
-            concurrentMeetings[meetings[i][1]] = 1;
-            // update meetTime
-            meetTime = meetings[i][2];
-            console.log(`New meet time: ${meetings[i][2]} at iteration ${i}.`)
-            
-        }
-        // else add people to concurrentMeetings
-        else {
-            console.log(`Meet time stayed the same at iteration ${i}.`)
-            if (!concurrentMeetings[meetings[i][0]]) {
-                concurrentMeetings[meetings[i][0]] = 1;
-            }
-            if (!concurrentMeetings[meetings[i][1]]) {
-                concurrentMeetings[meetings[i][1]] = 1;
-            }
-        }
-
+var canTraverseAllPairs = function(nums) {
+    if (nums.length === 1) {
+        // return edge case
+        console.log('true');
     }
-    console.log(concurrentMeetings);
-    // if secret is known by any in concurrentMeetings 
-        // push all people to the knowSecret list
-    // return an array of the people who know the secret
-    console.log(knowSecret);
+    const maxValue = new Array(Math.max(...nums));
+    console.log(...maxValue);
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 1) {
+            // return edge case
+            console.log(`false`)
+        }
+        
+    }
 };
 
-findAllPeople(6, [[1,2,5],[2,3,8],[1,5,10]], 1);
-findAllPeople(4, [[3,1,3],[1,2,2],[0,3,3]], 3);
-findAllPeople(5, [[3,4,2],[1,2,1],[2,3,1]], 1);
+canTraverseAllPairs([2,3,6]);
+canTraverseAllPairs([3,9,5]);
+canTraverseAllPairs([4,3,12,8]);
+
+//findAllPeople(6, [[1,2,5],[2,3,8],[1,5,10]], 1);
+//findAllPeople(4, [[3,1,3],[1,2,2],[0,3,3]], 3);
+//findAllPeople(5, [[3,4,2],[1,2,1],[2,3,1]], 1);
 
