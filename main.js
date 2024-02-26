827c4879b293cc4862155b0eb4fa1e973496e37d
Jesse_Carter <jessetcarter@hotmail.com>
2024-02-26 11:39:52 -0700

    solved

--- main.js
+++ main.js
@@ -1,93 +1,12 @@
-var canTraverseAllPairs = function(nums) {
-    let MAX = 100000;
-    let N = nums.length;
-    let has = new Array(MAX + 1);
-    for (let x of nums) {
-        has[x] = true;
-    }
-    console.log(has);
-    if (nums.length === 1) {
-        // return edge case
+var isSameTree = function(p, q) {
+    if (JSON.stringify(p) === JSON.stringify(q)) {
         console.log('true');
-    }
-    if (nums.includes(1)) {
-        // return edge case
-        console.log(`false`)
-    }
-    /* const maxValue = new Array(Math.max(...nums));
-    console.log(...maxValue); */
-    let sieve = new Array(MAX + 1);
-    for (let d = 2; d <= MAX; d++) {
-        if (sieve[d] === 0) {
-            for (let v = d; v <= MAX; v += d) {
-                sieve[v] = d;
-            }
-        }
-    }
-
-    let componentsEval = new DSU(2 * MAX + 1);
-    for (let x of nums) {
-        let val = x;
-        while (val > 1) {
-            let prime = sieve[val];
-            let root = prime+MAX;
-            if (componentsEval.find(root) !== componentsEval.find(x)) {
-                componentsEval.merge(root, x);
-            }
-            while (val % prime === 0) {
-                val /= prime;
-            }
-        }
-    }
-
-    let cnt = 0;
-    for (let i = 2; i <= MAX; i++) {
-        if (has[i] && componentsEval.find(i) === i) {
-            cnt++;
-        }
-    }
-    console.log(cnt);
-    return cnt;
-    
+    } else console.log('false');
 };
 
-class DSU {
-
-    constructor(N) {
-        this.dsu = new Array(N+1);
-        this.size = new Array(N+1);
-        for (let i = 0; i <= N; i++) {
-            this.dsu[i] = i;
-            this.size[i] = 1;
-        }
-    }
-    
-    find(x) {
-        return this.dsu[x] === x ? x : (this.dsu[x] = find(dsu[x]));
-    }
-
-    merge(x,y) {
-        let fx = find(x);
-        let fy = find(y);
-        if (fx === fy) {
-            return;
-        }
-        if (size[fx] > size[fy]) {
-            let temp = fx;
-            fx = fy;
-            fy = temp;
-        }
-        dsu[fx] = fy;
-        size[fy] += size[fx];
-    }
-
-}
-
-//canTraverseAllPairs([1]);
-//canTraverseAllPairs([1,2]);
-canTraverseAllPairs([2,3,6]);
-//canTraverseAllPairs([3,9,5]);
-//canTraverseAllPairs([4,3,12,8]);
+isSameTree([1,2,3],[1,2,3]); //expect true
+isSameTree([1,2],[1,null,2]); //expect false
+isSameTree([1,2,1],[1,1,2]); //expect false
 
 //findAllPeople(6, [[1,2,5],[2,3,8],[1,5,10]], 1);
 //findAllPeople(4, [[3,1,3],[1,2,2],[0,3,3]], 3);
