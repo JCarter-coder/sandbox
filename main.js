bf71cd9b53a0da5b822ca9d415c4ccad7018410a
Jesse_Carter <jessetcarter@hotmail.com>
2024-02-25 22:23:50 -0700

    working class

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 var canTraverseAllPairs = function(nums) {
+    let MAX = 100000;
+    let N = nums.length;
+    let has = new Array(MAX + 1);
+    for (let x of nums) {
+        has[x] = true;
+    }
+    console.log(has);
     if (nums.length === 1) {
         // return edge case
         console.log('true');
@@ -7,26 +14,56 @@ var canTraverseAllPairs = function(nums) {
         // return edge case
         console.log(`false`)
     }
-    const maxValue = new Array(Math.max(...nums));
-    console.log(...maxValue);
-    for (let i = 0; i < nums.length; i++) {
-        
-        
+    /* const maxValue = new Array(Math.max(...nums));
+    console.log(...maxValue); */
+    let sieve = new Array(MAX + 1);
+    for (let d = 2; d <= MAX; d++) {
+        if (sieve[d] === 0) {
+            for (let v = d; v <= MAX; v += d) {
+                sieve[v] = d;
+            }
+        }
     }
+
+    let componentsEval = new DSU(2 * MAX + 1);
+    for (let x of nums) {
+        let val = x;
+        while (val > 1) {
+            let prime = sieve[val];
+            let root = prime+MAX;
+            if (componentsEval.find(root) !== componentsEval.find(x)) {
+                componentsEval.merge(root, x);
+            }
+            while (val % prime === 0) {
+                val /= prime;
+            }
+        }
+    }
+
+    let cnt = 0;
+    for (let i = 2; i <= MAX; i++) {
+        if (has[i] && componentsEval.find(i) === i) {
+            cnt++;
+        }
+    }
+    console.log(cnt);
+    return cnt;
+    
 };
 
 class DSU {
+
     constructor(N) {
         this.dsu = new Array(N+1);
         this.size = new Array(N+1);
         for (let i = 0; i <= N; i++) {
-            dsu[i] = i;
-            size[i] = 1;
+            this.dsu[i] = i;
+            this.size[i] = 1;
         }
     }
     
     find(x) {
-        return dsu[x] === x ? x : (dsu[x] = find(dsu[x]));
+        return this.dsu[x] === x ? x : (this.dsu[x] = find(dsu[x]));
     }
 
     merge(x,y) {
@@ -46,11 +83,11 @@ class DSU {
 
 }
 
-canTraverseAllPairs([1]);
-canTraverseAllPairs([1,2]);
+//canTraverseAllPairs([1]);
+//canTraverseAllPairs([1,2]);
 canTraverseAllPairs([2,3,6]);
-canTraverseAllPairs([3,9,5]);
-canTraverseAllPairs([4,3,12,8]);
+//canTraverseAllPairs([3,9,5]);
+//canTraverseAllPairs([4,3,12,8]);
 
 //findAllPeople(6, [[1,2,5],[2,3,8],[1,5,10]], 1);
 //findAllPeople(4, [[3,1,3],[1,2,2],[0,3,3]], 3);
