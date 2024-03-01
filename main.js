d75aa9d0ce6aa1022bad2776ba2bc3819f463fcb
Jesse_Carter <jessetcarter@hotmail.com>
2024-02-29 23:05:40 -0700

    used Int32Array

--- main.js
+++ main.js
@@ -1,20 +1,11 @@
-var createCounter = function(init) {
-    let currentCount = init;
-    return {
-        increment: function() {
-            return ++currentCount;
-        },
-        decrement: function() {
-            return --currentCount;
-        },
-        reset: function() {
-            currentCount = init;
-            return currentCount;
-        }
+var map = function(arr, fn) {
+    let returnedArray = new Int32Array(arr.length);
+    for (let i = 0; i < arr.length; i++) {
+        returnedArray[i] = fn(arr[i], i);
     }
+    console.log(returnedArray);
 };
 
-const counter = createCounter(5);
-console.log(counter.increment());
-console.log(counter.reset());
-console.log(counter.decrement());
+map([1,2,3], function plusone(n) { return n + 1; });
+map([1,2,3], function plusI(n, i) { return n + i; });
+map([10,20,30], function constant(n, i) { return 42; });
