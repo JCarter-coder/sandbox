37a5f75386421cd0bcbaa35d8662aca67b722f41
Jesse_Carter <jessetcarter@hotmail.com>
2024-03-01 23:50:58 -0700

    solved

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
-var filter = function(arr, fn) {
-    let size = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (fn(arr[i], i)) {
-            arr[size] = arr[i];
-            size++;
-        }
+var reduce = function(nums, fn, init) {
+    let accum = init;
+    for (let i = 0; i < nums.length; i++) {
+        accum = fn(accum, nums[i]);
     }
-    while (arr.length > size) {
-        arr.pop();
-    }
-    console.log(arr);
+    console.log(accum);
 };
 
-filter([0,10,20,30], function greaterThan10(n) { return n > 10; });
-filter([1,2,3], function firstIndex(n, i) { return i === 0; });
-filter([-2,-1,0,1,2], function plusOne(n) { return n + 1 });
+reduce([1,2,3,4], function sum(accum, curr) { return accum + curr; }, 0);
+reduce([1,2,3,4], function sum(accum, curr) { return accum + curr * curr; }, 100);
+reduce([], function sum(accum, curr) { return 0; }, 25);
