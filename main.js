c78f39448f069729872b65147301e7273021e080
Jesse_Carter <jessetcarter@hotmail.com>
2024-03-01 23:18:42 -0700

    solved

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-var map = function(arr, fn) {
+var filter = function(arr, fn) {
+    let size = 0;
     for (let i = 0; i < arr.length; i++) {
-        arr[i] = fn(arr[i], i);
+        if (fn(arr[i], i)) {
+            arr[size] = arr[i];
+            size++;
+        }
+    }
+    while (arr.length > size) {
+        arr.pop();
     }
     console.log(arr);
 };
 
-map([1,2,3], function plusone(n) { return n + 1; });
-map([1,2,3], function plusI(n, i) { return n + i; });
-map([10,20,30], function constant(n, i) { return 42; });
+filter([0,10,20,30], function greaterThan10(n) { return n > 10; });
+filter([1,2,3], function firstIndex(n, i) { return i === 0; });
+filter([-2,-1,0,1,2], function plusOne(n) { return n + 1 });
