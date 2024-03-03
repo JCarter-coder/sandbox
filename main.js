5a0b94ed7b3b4ddc86f004f7567b27958a2c621c
Jesse_Carter <jessetcarter@hotmail.com>
2024-03-02 20:59:31 -0700

    halfway

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
-var resultArray = function(nums) {
-    let arr1 = [nums[0]];
-    let arr2 = [nums[1]];
-    for (let i = 2; i < nums.length; i++) {
-        if (arr1[arr1.length-1] > arr2[arr2.length-1]) {
-            arr1.push(nums[i]);
-        } else {
-            arr2.push(nums[i]);
+var countSubmatrices = function(grid, k) {
+    let m = grid.length; //height
+    let n = grid[0].length; //width
+    let count = 1;
+    let sum = grid[0][0];
+    let memoRows = {};
+    
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            memoRows[j] += grid[j][i];
+            console.log(`Row ${j}: ${memoRows[j]}`);
+            /* if (heightSum += grid[i][j] <= k) {
+                heightSum += grid[i][j];
+                count++;
+            } else break; */
         }
     }
-    console.log(arr1.concat(arr2));
+
+
+
+
+
+    /* for (let i = 1; i < n; i++) {
+        sum += grid[0][i];
+        temp = sum;
+        if (temp <= k) {
+            count++;
+        } else break;
+    }
+    sum = grid[0][0];
+    for (let j = 1; j < m; j++) {
+        sum += grid[j][0];
+        if (sum <= k) {
+            count++;
+        } else break;
+    } */
+    console.log(count);
 };
 
-resultArray([2,1,3]); //expect [2,3,1]
-resultArray([5,4,3,8]); //expect [5,3,4,8]
+countSubmatrices([[7,6,3],[6,6,1]], 18); //expect 4
+//countSubmatrices([[7,2,9],[1,5,0],[2,6,6]], 20); //expect 6
