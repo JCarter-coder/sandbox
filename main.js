88a3fc3243a06cb4cb32d481af664a4f2b036162
Jesse_Carter <jessetcarter@hotmail.com>
2024-03-02 19:53:44 -0700

    solved weekly pt1

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-var argumentsLength = function(...args) {  
-    let count = 0; 
-    for (const arg of args) {
-        count++;
+var resultArray = function(nums) {
+    let arr1 = [nums[0]];
+    let arr2 = [nums[1]];
+    for (let i = 2; i < nums.length; i++) {
+        if (arr1[arr1.length-1] > arr2[arr2.length-1]) {
+            arr1.push(nums[i]);
+        } else {
+            arr2.push(nums[i]);
+        }
     }
-    console.log(count);
+    console.log(arr1.concat(arr2));
 };
 
-argumentsLength(5);
-argumentsLength({}, null, "3");
+resultArray([2,1,3]); //expect [2,3,1]
+resultArray([5,4,3,8]); //expect [5,3,4,8]
