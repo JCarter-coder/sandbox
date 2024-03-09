f0a9392e2e294808205477ab5f6550a956e096ec
Jesse_Carter <jessetcarter@hotmail.com>
2024-03-09 14:12:22 -0700

    E1768 solved

--- main.js
+++ main.js
@@ -32,18 +32,31 @@ class LinkedList {
     }
 } */
 
-var cancellable = function(fn, args, t) {
-    // one-time function call before setting interval to repeat
-    fn(...args);
-    // now function executes once every t milliseconds
-    const timer = setInterval(() => fn(...args), t);
-    // clears intervals when called
-    const cancelFn = () => clearInterval(timer);
-
-    return cancelFn;
+var mergeAlternately = function(word1, word2) {
+    let mergedString = "";
+    let longestWord = (word1.length >= word2.length ?
+        word1.length : word2.length);
+    for (let i = 0; i < longestWord; i++) {
+        if (word1[i]) {
+            mergedString += word1[i];
+        } else if (word2.slice(i)) {
+            mergedString += word2.slice(i);
+            break;
+        }
+        if (word2[i]) {
+            mergedString += word2[i];
+        } else if (word1.slice(i + 1)) {
+            mergedString += word1.slice(i + 1);
+            break;
+        }
+    }
+    console.log(mergedString);
 };
 
-cancellable((x) => x*5, [2], 20);
+mergeAlternately("abc","pqr");
+mergeAlternately("ab","pqrs");
+mergeAlternately("abcd","pq");
+
 
 
 
