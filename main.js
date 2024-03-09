eafca95c0e3bed8e62e13e46b3ead663961a292d
Jesse_Carter <jessetcarter@hotmail.com>
2024-03-08 22:30:34 -0700

    E2715 solved

--- main.js
+++ main.js
@@ -32,40 +32,20 @@ class LinkedList {
     }
 } */
 
-var getCommon = function(nums1, nums2) {
-    let largestArrayLength = (nums1.length >= nums2.length ? nums1.length : nums2.length);
-    //console.log(largestArrayLength);
-    let hash = {};
-    let temp1 = undefined;
-    let temp2 = undefined;
-    for (let i = 0; i < largestArrayLength; i++) {
-        if (nums1[i] && temp1 !== nums1[i]) {
-            //temp1 = (temp1 === undefined || temp1 < nums1[i] ? nums1[i] : temp1)
-            if (!hash[nums1[i]]) {
-                hash[nums1[i]] = 1;
-            } else if (hash[nums1[i]]) {
-                console.log(nums1[i]);
-                return 0;
-            }
-            temp1 = nums1[i];
-        }
-        if (nums2[i] && temp2 !== nums2[i]) {
-            if (!hash[nums2[i]]) {
-                hash[nums2[i]] = 1;
-            } else if (hash[nums2[i]]) {
-                console.log(nums2[i]);
-                return 0;
-            }
-            temp2 = nums2[i];
-        }
-        console.log(hash);
-    }
-    console.log(-1);
+var cancellable = function(fn, args, t) {
+    const timeoutId = setTimeout(() => {
+        fn.apply(null, args);
+    }, t);
+
+    const cancelFn = function() {
+        clearTimeout(timeoutId);
+    };
+    
+    return cancelFn;
 };
 
-getCommon([1,2,3],[2,4]);
-getCommon([1,2,3,6],[2,3,4,5]);
-getCommon([1,1,2],[2,4]);
+cancellable((x) => x*5, [2], 20);
+
 
 
 //let chain = new LinkedList();
