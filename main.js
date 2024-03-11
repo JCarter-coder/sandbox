824c002f8703464611816594243a17421a739afb
Jesse_Carter <jessetcarter@hotmail.com>
2024-03-11 15:08:52 -0600

    E345 solved

--- main.js
+++ main.js
@@ -32,45 +32,33 @@ class LinkedList {
     }
 } */
 
-var customSortString = function(order, s) {
-    const permutedSet = new Set();
-    const splitS = s.split('');
-    //console.log(splitS);
-    const setIterator = permutedSet.keys();
-    let result = "";
-    for (let i = 0; i < order.length; i++) {
-        permutedSet.add(order[i]);
-    }
-    let index = 0;
-    while (s.length > index) {
-        // next char to evaluate in order Set
-        let currentOrderChar = setIterator.next().value;
-        // index of s string, if it is found in order Set
-        let indexEvaluated = splitS.indexOf(currentOrderChar);
-        //console.log(currentOrderChar);
-        while (indexEvaluated >= 0 && currentOrderChar !== undefined) {
-            //console.log(currentOrderChar);
-            //console.log(indexEvaluated);
-            splitS.splice(indexEvaluated, 1);
-            result += currentOrderChar;
-            indexEvaluated = splitS.indexOf(currentOrderChar);
-        } 
-        index++;
-    }
-    index = 0;
-    if (splitS.length >= 0) {
-        while (splitS[index]) {
-            result += splitS[index];
-            index++;
+var reverseVowels = function(s) {
+    let vowels = ['a','e','i','o','u','A','E','I','O','U'];
+    let tempVowels = [];
+    let result = s.split('');
+    console.log(result);
+    for (let i = 0; i < result.length; i++) {
+        // if string includes a vowel push
+        // the value to the tempVowels array
+        if (vowels.includes(result[i])) {
+            tempVowels.push(result[i]);
         }
     }
-    console.log(result);
+    console.log(tempVowels);
+    for (let j = 0; j < result.length; j++) {
+        if (vowels.includes(result[j])) {
+            // pop the last value of the tempVowels array
+            // and assign it to the next evaluated vowel
+            result[j] = tempVowels.pop();
+        }
+    }
+    console.log(result.join(''));
 };
 
-customSortString("cba","abcd");
-customSortString("bcafg","abcd");
-customSortString("kqep","pekeq");
-
+reverseVowels("hello");
+reverseVowels("leetcode");
+reverseVowels("aA");
+reverseVowels("race car");
 
 
 //let chain = new LinkedList();
