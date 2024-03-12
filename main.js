afb5a890f01626fd5793d75fedeabf7152b3e202
Jesse_Carter <jessetcarter@hotmail.com>
2024-03-12 14:19:34 -0600

    M1171 solved

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* class ListNode {
+class ListNode {
     constructor(val) {
         this.val = val;
         this.next = null;
@@ -30,28 +30,35 @@ class LinkedList {
         }
         //console.log(`Head: ${this.head}, Size: ${this.size}`);
     }
-} */
+}
 
-var timeLimit = function(fn, t) {
-      
-    return async function(...args) {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                reject("Time Limit Exceeded");
-            }, t);
-            fn(...args).then(resolve).catch(reject);
-        })
+var removeZeroSumSublists = function(head) {
+    let front = new ListNode(0, head);
+    let start = front;
+    
+    while (start !== null) {
+        let prefixSum = 0;
+        let end = start.next;
+
+        while (end !== null) {
+            prefixSum += end.val;
+            if (prefixSum === 0) {
+                start.next = end.next;
+            }
+            end = end.next;
+        }
+        start = start.next;
     }
+    console.log(front.next)
+    return front.next;
 };
 
-timeLimit(async (n) => {
-    await new Promise(res => setTimeout(res, 100));
-    return n * n; 
-},50);
+// May be issues with how I'm creating lists compared to 
+// what LeetCode is using. Solution works on LeetCode but
+// there is something different in this environment
+let chain = new LinkedList();
+chain.turnArrayToList([1,2,-3,3,1]);
+//chain.removeZeroSumSublists([1,2,3,-3,4]);
+//chain.removeZeroSumSublists([1,2,3,-3,-2]);
 
-
-
-//let chain = new LinkedList();
-//chain.turnArrayToList([1,1,2,1,2,3]);
-
-//frequenciesOfElements(chain.head);
+removeZeroSumSublists(chain);
