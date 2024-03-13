a95bfdde462ecd70001da7fb8dd4b02ac945ca6a
Jesse_Carter <jessetcarter@hotmail.com>
2024-03-13 11:22:19 -0600

    E2485 solved 2

--- main.js
+++ main.js
@@ -36,37 +36,12 @@ let chain = new LinkedList();
 chain.turnArrayToList([1,2,-3,3,1]); */
 
 var pivotInteger = function(n) {
-    let leftSum = 0;
-    let rightSum = n;
-    let leftPointer = 1;
-    let rightPointer = n;
-    for (leftPointer = 1; leftPointer <= rightPointer; leftPointer++) {
-        if (leftPointer === rightPointer) {
-            // when the pointers are the same add the newly
-            // incremented leftPointer value and break
-            leftSum += leftPointer;
-            break;
-        }
-        else if (leftSum + leftPointer < rightSum) {
-            leftSum += leftPointer;
-        } else {
-            rightPointer--;
-            rightSum += rightPointer;
-            // reset leftPointer to reevaluate
-            leftPointer--;
-        }
-    }
-    if (leftSum === rightSum) {
-        console.log(leftPointer);
-        return leftPointer;
-    } else {
-        console.log(-1);
-        return -1;
-    }
-    //console.log(`L sum: ${leftSum}, R sum: ${rightSum}`)
-    //console.log(`L: ${leftPointer}, R: ${rightPointer}`);
+    let sum = (n * (n + 1) / 2);
+    let pivot = Math.floor(Math.sqrt(sum));
+    console.log(pivot * pivot === sum ? pivot : -1);
 };
 
 pivotInteger(8);
 pivotInteger(1);
 pivotInteger(4);
+pivotInteger(15);
