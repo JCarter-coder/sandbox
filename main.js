296b6c1b73c4b931767ef7b1f43b4d0743c73471
Jesse_Carter <jessetcarter@hotmail.com>
2024-03-19 15:11:38 -0600

    M621 solved

--- main.js
+++ main.js
@@ -35,59 +35,37 @@ class LinkedList {
 let chain = new LinkedList();
 chain.turnArrayToList([1,2,-3,3,1]); */
 
-var findMinArrowShots = function(points) {
-    // find intersects of points, create an array of these intersects
-    // and return the length of this array for number of arrows needed
-    points.sort((a,b) => a[0] - b[0]);
-    let array = [];
-    let intersectLeft = points[0][0];
-    let intersectRight = points[0][1];
-    for (let i = 1; i < points.length; i++) {
-        // if intersects are undefined define them and continue
-        // this allows to push an interval when this loop is completed
-        // and the interval has assigned variables
-        if (intersectLeft === undefined && intersectRight === undefined) {
-            intersectLeft = points[i][0];
-            intersectRight = points[i][1];
-            continue;
+var leastInterval = function(tasks, n) {
+    let taskFreq = {};
+    let maxFreq = 0;
+    for (let task of tasks) {
+        if (!taskFreq[task]) {
+            taskFreq[task] = 1;
+        } else {
+            taskFreq[task]++;
         }
-        // if intersectRight < points[i][0]
-        // then push [intersectLeft, intersectRight]
-        // assign points[i] to intersect values
-        if (intersectRight < points[i][0]) {
-            array.push([intersectLeft, intersectRight]);
-            intersectLeft = undefined;
-            intersectRight = undefined;
-            i--;
-        }
-        // if interectRight === points[i][0]
-        // then intersectLeft = intersectRight
-        else if (intersectRight === points[i][0]) {
-            intersectLeft = intersectRight;
-        }
-        // if interval overlaps and extends beyond intersection,
-        // then narrow the left, intersectLeft = points[i][0]
-        else if (intersectRight > points[i][0] && intersectRight <= points[i][1]) {
-            intersectLeft = points[i][0];
-        }
-        // if the interval is completely within the defined
-        // intersection, narrow the intersection
-        else if (intersectRight > points[i][0] && intersectRight > points[i][1]) {
-            intersectLeft = points[i][0];
-            intersectRight = points[i][1];
+        if (taskFreq[task] > maxFreq) {
+            maxFreq = taskFreq[task];
         }
     }
-    if (intersectLeft !== undefined && intersectRight !== undefined) {
-        array.push([intersectLeft, intersectRight]);
-    }
+    // number of tasks with maxFreq
+    let maxCount = (Object.values(taskFreq)).filter((val) => val === maxFreq).length;
+    console.log(taskFreq);
+    console.log(maxFreq);
+    console.log(maxCount);
     
-    console.log(array.length);
+    let partCount = maxFreq - 1;
+    let partLength = n - (maxCount - 1);
+    let emptySlots = partCount * partLength;
+    let availableTasks = tasks.length - maxFreq * maxCount;
+    let idles = Math.max(0, emptySlots - availableTasks);
+
+    console.log(tasks.length + idles);
 };
 
-findMinArrowShots([[10,16],[2,8],[1,6],[7,12]]);
-findMinArrowShots([[1,2],[3,4],[5,6],[7,8]]);
-findMinArrowShots([[1,2],[2,3],[3,4],[4,5]]);
-findMinArrowShots([[3,9],[7,12],[3,8],[6,8],[9,10],[2,9],[0,9],[3,9],[0,6],[2,8]]);
+leastInterval(["A","A","A","B","B","B"],2);
+leastInterval(["A","C","A","B","D","B"],1);
+leastInterval(["A","A","A", "B","B","B"],3);
 
 /* insert([[1,3],[6,9]],[2,5]);
 insert([[1,2],[3,5],[6,7],[8,10],[12,16]],[4,8]);
