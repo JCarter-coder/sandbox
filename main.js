ebe6a8b25bfc93a35ecfcd4cc54941967514b9c9
Jesse_Carter <jessetcarter@hotmail.com>
2024-03-20 17:54:43 -0600

    M1669 solved

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* class ListNode {
+class ListNode {
     constructor(val) {
         this.val = val;
         this.next = null;
@@ -32,40 +32,62 @@ class LinkedList {
     }
 }
 
-let chain = new LinkedList();
-chain.turnArrayToList([1,2,-3,3,1]); */
+let chain1 = new LinkedList();
+let chain2 = new LinkedList();
+chain1.turnArrayToList([10,1,13,6,9,5]);
+chain2.turnArrayToList([1000000,1000001,1000002]);
+//chain1.turnArrayToList([0,1,2,3,4,5,6]);
+//chain2.turnArrayToList([1000000,1000001,1000002,1000003,1000004]);
 
-var leastInterval = function(tasks, n) {
-    let taskFreq = {};
-    let maxFreq = 0;
-    for (let task of tasks) {
-        if (!taskFreq[task]) {
-            taskFreq[task] = 1;
-        } else {
-            taskFreq[task]++;
-        }
-        if (taskFreq[task] > maxFreq) {
-            maxFreq = taskFreq[task];
+// couldn't get the solution to pass LeetCode though it works in 
+// IDE. I think I have a misunderstanding of a fundamental setup
+// for the LinkedList definition that they are using in LeetCode.
+// They show a ListNode definition but there is no discussion on
+// how they are implementing the linked list. I have had this happen
+// on several occassions where my code passes and other times it 
+// doesn't. I even save the linkedList/ListNode to be consistent...
+var mergeInBetween = function(list1, a, b, list2) {
+    let insertionStart = 0;
+    let insertionEnd = 0;
+    let inserted = false;
+    let currentList1Node = list1.head;
+    let currentList2Node = list2.head;
+    let result = new LinkedList();
+    while (currentList1Node !== null) {
+        //console.log(currentList1Node.val);
+        if (insertionStart < a) {
+            result.add(currentList1Node.val);
+            currentList1Node = currentList1Node.next;
+            insertionStart++;
+        } else if (insertionStart === a && inserted === false) {
+            while (currentList2Node !== null) {
+                result.add(currentList2Node.val);
+                currentList2Node = currentList2Node.next;
+            }
+            inserted = true;
+        } else if (a + insertionEnd < b + 1) {
+            currentList1Node = currentList1Node.next;
+            insertionEnd++;
+        } else if (a + insertionEnd === b + 1) {
+            while (currentList1Node !== null) {
+                result.add(currentList1Node.val);
+                currentList1Node = currentList1Node.next;
+            }
+            break;
         }
     }
-    // number of tasks with maxFreq
-    let maxCount = (Object.values(taskFreq)).filter((val) => val === maxFreq).length;
-    console.log(taskFreq);
-    console.log(maxFreq);
-    console.log(maxCount);
-    
-    let partCount = maxFreq - 1;
-    let partLength = n - (maxCount - 1);
-    let emptySlots = partCount * partLength;
-    let availableTasks = tasks.length - maxFreq * maxCount;
-    let idles = Math.max(0, emptySlots - availableTasks);
+    console.log(result.head);
+    /* let currentResultNode = result.head;
+    while (currentResultNode !== null) {
+        console.log(currentResultNode.val);
+        currentResultNode = currentResultNode.next;
+    } */
 
-    console.log(tasks.length + idles);
 };
 
-leastInterval(["A","A","A","B","B","B"],2);
-leastInterval(["A","C","A","B","D","B"],1);
-leastInterval(["A","A","A", "B","B","B"],3);
+mergeInBetween(chain1, 3, 4, chain2);
+//mergeInBetween(chain1, 2, 5, chain2);
+
 
 /* insert([[1,3],[6,9]],[2,5]);
 insert([[1,2],[3,5],[6,7],[8,10],[12,16]],[4,8]);
