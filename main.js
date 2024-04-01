958ffcfc372572fc8271f2b7c012de1e8c0c2da0
Jesse_Carter <jessetcarter@hotmail.com>
2024-04-01 14:50:25 -0600

    M159 solved

--- main.js
+++ main.js
@@ -38,12 +38,28 @@ let chain2 = new LinkedList();
 chain2.turnArrayToList([1,2]);
  */
 
-var lengthOfLastWord = function(s) {
-    let trimmedString = s.trim();
-    let array = trimmedString.split(' ');
-    console.log(array[array.length - 1].length);
+var lengthOfLongestSubstringTwoDistinct = function(s) {
+    const seen = {};
+    let longestSubString = 0;
+    let leftPointer = 0;
+    for (let rightPointer = 0; rightPointer < s.length; rightPointer++) {
+        if (!seen[s[rightPointer]]) {
+            seen[s[rightPointer]] = 1;
+        } else seen[s[rightPointer]]++;
+
+        while (Object.keys(seen).length > 2) {
+            seen[s[leftPointer]]--;
+            if (seen[s[leftPointer]] === 0) {
+                delete seen[s[leftPointer]];
+            }
+            leftPointer++;
+        }
+        
+        longestSubString = Math.max(longestSubString, rightPointer - leftPointer + 1);
+    }
+    console.log(longestSubString);
+    return longestSubString;
 };
 
-lengthOfLastWord("Hello World");
-lengthOfLastWord("   fly me   to   the moon  ");
-lengthOfLastWord("luffy is still joyboy");
+lengthOfLongestSubstringTwoDistinct("eceba");
+lengthOfLongestSubstringTwoDistinct("ccaabbb");
