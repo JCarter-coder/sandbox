da5b36da603ac393914f458e659b9129c1821a1d
Jesse_Carter <jessetcarter@hotmail.com>
2024-04-24 09:23:23 -0600

    E1137 solved

--- main.js
+++ main.js
@@ -42,61 +42,23 @@ class BinaryTree {
     }
 }
 
-var findMinHeightTrees = function(n, edges) {
-    // edge cases
-    if (n < 2) {
-        let centroids = new Array();
-        for (let i = 0; i < n; i++) {
-            centroids.push(i);
-        }
-        console.log(centroids);
-        return centroids;
-    }
+var tribonacci = function(n) {
+    let sequence = {};
+    let i;
 
-    // build the graph with the adjacency list
-    let neighbors = new Array();
-    for (let i = 0; i < n; i++) {
-        let hashSet = new Set();
-        neighbors.push(hashSet);
-    }
-    for (let edge of edges) {
-        let start = edge[0];
-        let end = edge[1];
-        neighbors[start].add(end);
-        neighbors[end].add(start);
-    }
-
-    // initialize the first layer of leaves
-    let leaves = new Array();
-    for (let i = 0; i < n; i++) {
-        if (neighbors[i].size === 1) {
-            leaves.push(i);
+    for (i = 0; i <= n; i++) {
+        if (i === 0) {
+            sequence[i] = 0;
+        } else if (i === 1) {
+            sequence[i] = 1;
+        } else if (i === 2) {
+            sequence[i] = 1;
+        } else {
+            sequence[i] = sequence[i - 3] + sequence[i - 2] + sequence[i - 1]
         }
     }
-
-    // trim the leaves until reaching the centroids
-    let remainingNodes = n;
-    while (remainingNodes > 2) {
-        remainingNodes -= leaves.length;
-        let newLeaves = new Array();
-
-        // remove the current leaves along with the edges
-        for (let leaf of leaves) {
-            // the only neighbor left for the leaf node
-            let iterator = neighbors[leaf].values();
-            let neighbor = iterator.next().value;
-            // remove the only edge left
-            neighbors[neighbor].delete(leaf);
-            if (neighbors[neighbor].size === 1) {
-                newLeaves.push(neighbor);
-            }
-        }
-        leaves = newLeaves;
-    }
-    console.log(leaves);
-    return leaves;
+    console.log(sequence[i-1]);
 };
 
-findMinHeightTrees(4,[[1,0],[1,2],[1,3]]);
-findMinHeightTrees(6,[[3,0],[3,1],[3,2],[3,4],[5,4]]);
-findMinHeightTrees(6,[[0,1],[0,2],[0,3],[3,4],[4,5]]);
+tribonacci(4);
+tribonacci(25);
