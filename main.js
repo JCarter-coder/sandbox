602824d91388645f516bfbdf88f7caa938cdcbcc
Jesse_Carter <jessetcarter@hotmail.com>
2024-04-27 15:46:07 -0600

    H305 solved

--- main.js
+++ main.js
@@ -42,38 +42,88 @@ class BinaryTree {
     }
 }
 
-var findRotateSteps = function(ring, key) {
-    let ringLen = ring.length;
-    let keyLen = key.length;
-    let curr = new Array(ringLen);
-    let prev = new Array(ringLen);
-    prev.fill(0);
+class UnionFind {
 
-    var countSteps = function(curr, next, ringLength) {
-        let stepsBetween = Math.abs(curr - next);
-        let stepsAround = ringLength - stepsBetween;
-        return Math.min(stepsBetween, stepsAround);
+    constructor(size) {
+        this.parent = new Array(size);
+        this.rank = new Array(size);
+        for (let i = 0; i < size; i++) {
+            this.parent[i] = -1;
+        }
+        this.count = 0;
+    }
+    addLand(x) {
+        if (this.parent[x] >= 0) {
+            return;
+        }
+        this.parent[x] = x;
+        this.count++;
     }
 
-    // for each occurrence of the character at key_index
-    // of key in ring. Stores minimum steps to the character
-    // from ringIndex of ring
-    for (let keyIndex = keyLen - 1; keyIndex >= 0; keyIndex--) {
-        curr.fill(Number.MAX_SAFE_INTEGER);
-        for (let ringIndex = 0; ringIndex < ringLen; ringIndex++) {
-            for (let charIndex = 0; charIndex < ringLen; charIndex++) {
-                if (ring.charAt(charIndex) === key.charAt(keyIndex)) {
-                    curr[ringIndex] = Math.min(curr[ringIndex],
-                        (1 + countSteps(ringIndex, charIndex, ringLen) +
-                        prev[charIndex]));
-                }
+    isLand(x) {
+        if (this.parent[x] >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    numOfIslands() {
+        return this.count;
+    }
+
+    find(x) {
+        if (this.parent[x] !== x) {
+            this.parent[x] = this.find(this.parent[x]);
+        }
+        return this.parent[x];
+    }
+
+    union(x, y) {
+        let xset = this.find(x);
+        let yset = this.find(y);
+        if (xset === yset) {
+            return;
+        } else if (this.rank[xset] < this.rank[yset]) {
+            this.parent[xset] = yset;
+        } else if (this.rank[xset] > this.rank[yset]) {
+            this.parent[yset] = xset;
+        } else {
+            this.parent[yset] = xset;
+            this.rank[xset]++;
+        }
+        this.count--;
+    }
+}
+
+var numIslands2 = function(m, n, positions) {
+    let x = [-1, 1, 0, 0];
+    let y = [0, 0, -1, 1];
+    let dsu = new UnionFind(m * n);
+    let answer = new Array();
+
+    for (let position of positions) {
+        let landPosition = position[0] * n + position[1];
+        dsu.addLand(landPosition);
+
+        for (let i = 0; i < 4; i++) {
+            let neighborX = position[0] + x[i];
+            let neighborY = position[1] + y[i];
+            let neighborPosition = neighborX * n + neighborY;
+            // if neighborX and neighborY correspond to a
+            // point in the grid and there is a land at that
+            // point, then merge it with the current land
+            if (neighborX >= 0 && neighborX < m && neighborY >= 0 &&
+                neighborY < n && dsu.isLand(neighborPosition)) {
+                    dsu.union(landPosition, neighborPosition);
             }
         }
-        prev = Array.from(curr);
+        answer.push(dsu.numOfIslands());
     }
-    console.log(prev[0]);
-    return prev[0];
+    console.log(answer);
+    return answer;
 };
 
-findRotateSteps("godding","gd");
-findRotateSteps("godding","godding");
+numIslands2(3,3,[[0,0],[0,1],[1,2],[2,1]]);
+numIslands2(1,1,[[0,0]]);
+numIslands2(3,3,[[0,1],[1,2],[2,1],[1,0],[0,2],[0,0],[1,1]]);
