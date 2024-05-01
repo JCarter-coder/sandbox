04de0d748bcc1687ed5e4be2ae242ee76a20b0ec
Jesse_Carter <jessetcarter@hotmail.com>
2024-04-30 19:46:16 -0600

    E2000 solved

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 var reversePrefix = function(word, ch) {
+    let indexCh = word.indexOf(ch);
+    let result = "";
     
+    if (indexCh === -1) {
+        console.log(word);
+        return word;
+    } else {
+        for (let i = indexCh; i >= 0; i--) {
+            result += word[i];
+        }
+        for (let j = indexCh + 1; j < word.length; j++) {
+            result += word[j];
+        }
+    }
+    console.log(result);
+    return result;
 };
 
 reversePrefix("abcdefd","d");
