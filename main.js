92566e5728745d4c8856abfccfc93826a811eb36
Jesse_Carter <jessetcarter@hotmail.com>
2024-05-01 23:18:12 -0600

    E2441 solved

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 var findMaxK = function(nums) {
-    
+    const container = {};
+    // default value of -1
+    let result = -1;
+    for (let i = 0; i < nums.length; i++) {
+        if (!container[(nums[i])]) {
+            container[nums[i]] = 0;
+        }
+        let compare = (-nums[i]).toString();
+        if (Object.keys(container).includes(compare)) {
+            if (Math.abs(nums[i]) > result) {
+                result = Math.abs(nums[i]);
+            }
+        }
+    }
+    console.log(result);
+    return result;
 };
 
 findMaxK([-1,2,-3,3]);
