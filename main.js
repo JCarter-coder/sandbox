9267016091213cf8cecc8e809e92fca441bf522d
Jesse_Carter <jessetcarter@hotmail.com>
2024-05-05 20:36:12 -0600

    M2487 setup

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
-var addMinimum = function(word) {
-    let count = 0;
-    let array = word.split('');
-    let i = 0;
-    let next = 'a';
-    while (array[i]) {
-        // if array[i] is the next value, recalculate
-        // next value and analyze the next index
-        if (array[i] === next) {
-            i++;
-        } else count++;
-        
-        next = (next === 'a' ? 'b' : (next === 'b' ? 'c' : 'a'));        
+class ListNode {
+    constructor(val, next) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
     }
-    if (array[array.length - 1] === 'a') {
-        count += 2;
-    } else if (array[array.length - 1] === 'b') {
-        count++;
-    }
+}
 
-    console.log(count);
-    return count;
+class LinkedList {
+    constructor(array) {
+        this.head = null;
+
+        let currentNode;
+        for (let i = 0; i < array.length; i++) {
+            
+            if (this.head === null) {
+                this.head = new ListNode(array[i]);
+                currentNode = this.head;
+            } else {
+                currentNode.next = new ListNode(array[i]);
+                currentNode = currentNode.next;
+            }
+        }
+    }
+}
+
+var removeNodes = function(head) {
+    let list = new LinkedList(head);
+    console.log(list);
 };
 
-addMinimum("b");
-addMinimum("aaa");
-addMinimum("abc");
+removeNodes([5,2,13,3,8]);
+removeNodes([1,1,1,1]);
