8f1ec2d31f40f08fc62a1a41db25bd0ef3c51569
Jesse_Carter <jessetcarter@hotmail.com>
2024-05-06 11:12:56 -0600

    M2487 solved

--- main.js
+++ main.js
@@ -24,9 +24,29 @@ class LinkedList {
 }
 
 var removeNodes = function(head) {
-    let list = new LinkedList(head);
-    console.log(list);
+    let current = head;
+    let stack = [];
+
+    while (current !== null) {
+        while (stack.length > 0 && stack[stack.length - 1].val < current.val) {
+            stack.pop();
+        }
+        stack.push(current);
+        current = current.next;
+    }
+
+    let nextVal = null;
+    while (stack.length > 0) {
+        current = stack.pop();
+        current.next = nextVal;
+        nextVal = current;
+    }
+
+    console.log(current);
+    return current;
 };
 
-removeNodes([5,2,13,3,8]);
-removeNodes([1,1,1,1]);
+let list1 = new LinkedList([5,2,13,3,8])
+removeNodes(list1.head);
+let list2 = new LinkedList([1,1,1,1])
+removeNodes(list2.head);
