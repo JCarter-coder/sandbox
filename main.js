897e1f1afa9aee7209f4866e83900d551fc6f18f
Jesse_Carter <jessetcarter@hotmail.com>
2024-05-09 22:40:59 -0600

    M786 solved

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 var kthSmallestPrimeFraction = function(arr, k) {
-    
+    let sortedArray = [];
+    for (let i = 0; i < arr.length - 1; i++) {
+        let arrI = arr[i];
+        for (let j = i + 1; j < arr.length; j++) {
+            let arrJ = arr[j];
+            sortedArray.push([arrI, arrJ]);
+        }
+    }
+    sortedArray.sort((a,b) => a[0]/a[1] - b[0]/b[1]);
+    console.log(sortedArray[k - 1]);
 };
 
 kthSmallestPrimeFraction([1,2,3,5],3);
