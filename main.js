8a8fc4a986247bbf4806ee82d63350a23325895a
Jesse_Carter <jessetcarter@hotmail.com>
2024-05-09 22:27:55 -0600

    M786 setup

--- main.js
+++ main.js
@@ -1,28 +1,6 @@
-var findRelativeRanks = function(score) {
-    let result = [];
-    let orderedScore = score.toSorted((a,b) => b - a);
-    const ranks = new Map();
-    for (let i = 0; i < orderedScore.length; i++) {
-        ranks.set(orderedScore[i], i);
-    }
-    console.log(ranks);
-    console.log(score);
-    console.log(orderedScore);
-
-    let index = 0;
-
-    while (index < score.length) {
-        if (score[index] === orderedScore[0]) {
-            result.push("Gold Medal");
-        } else if (score[index] === orderedScore[1]) {
-            result.push("Silver Medal");
-        } else if (score[index] === orderedScore[2]) {
-            result.push("Bronze Medal");
-        } else result.push((ranks.get(score[index]) + 1).toString());
-        index++;
-    }
-    console.log(result);
+var kthSmallestPrimeFraction = function(arr, k) {
+    
 };
 
-findRelativeRanks([5,4,3,2,1]);
-findRelativeRanks([10,3,8,9,4]);
+kthSmallestPrimeFraction([1,2,3,5],3);
+kthSmallestPrimeFraction([1,7],1);
