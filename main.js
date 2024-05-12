5bd2ccba772ccf5694fb8cd0133b3593650e7394
Jesse_Carter <jessetcarter@hotmail.com>
2024-05-12 12:52:05 -0600

    E2373 solved

--- main.js
+++ main.js
@@ -1,46 +1,32 @@
-class QualityRatio {
-    constructor(ratio, quality) {
-        this.ratio = ratio;
-        this.quality = quality;
-    }
-}
+var largestLocal = function(grid) {
+    let result = [];
+    let width = grid[0].length;
+    let height = grid.length;
 
-var mincostToHireWorkers = function(quality, wage, k) {
-    let n = quality.length;
-    let totalCost = Number.MAX_VALUE;
-    let currentTotalQuality = 0;
-    let qualityRatioArray = [];
-
-    // calculate wage-to-quality ratio for each worker
-    for (let i = 0; i < n; i++) {
-        let temp = new QualityRatio(wage[i]/quality[i], quality[i]);
-        qualityRatioArray.push(temp);
-    }
-
-    // sort wrokers based on their wage-to-quality ratio
-    qualityRatioArray.sort((a,b) => a.ratio - b.ratio);
-
-    // max-heap to store qualities
-    let highQualityWorkers = [];
-
-    for (let worker of qualityRatioArray) {
-        let ratio = worker.ratio;
-        let qua = worker.quality;
-
-        currentTotalQuality += qua;
-        highQualityWorkers.push(-qua);
-        highQualityWorkers.sort((a,b) => a - b);
-
-        if (highQualityWorkers.length > k) {
-            currentTotalQuality += highQualityWorkers.shift();
+    var maxLocal = function(x, y) {
+        let max = 0;
+        for (let i = x - 1; i <= x + 1; i++) {
+            for (let j = y - 1; j <= y + 1; j++) {
+                if (grid[i][j] > max) {
+                    max = grid[i][j];
+                }
+            }
         }
-        if (highQualityWorkers.length === k) {
-            totalCost = Math.min(totalCost, currentTotalQuality * ratio);
+        return max;
+    };
+    
+    for (let i = 1; i < width - 1; i++) {
+        let temp = [];
+        for (let j = 1; j < height - 1; j++) {
+            //analyze the 3x3 grid centered at these indices
+            temp.push(maxLocal(i, j));
         }
+        result.push(temp);
     }
-
-    console.log(totalCost);
+    console.log(result);
+    return result;
 };
 
-mincostToHireWorkers([10,20,5],[70,50,30],2);
-mincostToHireWorkers([3,1,10,10,1],[4,8,2,2,7],3);
+//largestLocal([[9,9,8,1],[5,6,2,6],[8,2,6,4],[6,2,2,2]]);
+//largestLocal([[1,1,1,1,1],[1,1,1,1,1],[1,1,2,1,1],[1,1,1,1,1],[1,1,1,1,1]]);
+largestLocal([[20,8,20,6,16,16,7,16,8,10],[12,15,13,10,20,9,6,18,17,6],[12,4,10,13,20,11,15,5,17,1],[7,10,14,14,16,5,1,7,3,11],[16,2,9,15,9,8,6,1,7,15],[18,15,18,8,12,17,19,7,7,8],[19,11,15,16,1,3,7,4,7,11],[11,6,5,14,12,18,3,20,14,6],[4,4,19,6,17,12,8,8,18,8],[19,15,14,11,11,13,12,6,16,19]]);
