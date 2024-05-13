ece54bd73e7eaa1f58cf62c84b280ea2c51daeb0
Jesse_Carter <jessetcarter@hotmail.com>
2024-05-13 15:40:20 -0600

    M2473 solved

--- main.js
+++ main.js
@@ -1,51 +1,48 @@
-class ChangeValues {
-    constructor(values) {
-        this.values = values;
-        for (let i = 0; i < values.length; i++) {
-            if (values[i] === 0) {
-                values[i] = 1;
-            } else values[i] = 0;
-        }
-        return values;
-    }
-}
+var minCost = function(n, roads, appleCost, k) {
+    // include town zero
+    n += 1;
+    const visited = new Array(n).fill(Number.MAX_SAFE_INTEGER);
+    // starting from town zero
+    visited[0] = 0;
 
-var matrixScore = function(grid) {
-    let m = grid.length;
-    let n = grid[0].length;
-    // if row starts with a zero, flip values of row
-    for (let i = 0; i < m; i++) {
-        if (grid[i][0] === 0) {
-            grid[i] = new ChangeValues(grid[i]);
-        }
-    }
-    // if column has more zeros than ones, flip values
+    // create list for the graph
+    const graph = Array.from({ length: n}, () => []);
+
+    // connect town zero to all other towns with appleCost
     for (let i = 1; i < n; i++) {
-        let columnSum = 0;
-        let columnArray = [];
-        for (let j = 0; j < m; j++) {
-            columnArray.push(grid[j][i]);
-            columnSum += grid[j][i]
+        graph[0].push([i, appleCost[i - 1]]);
+    }
+
+    // build the graph with roads and adjusted costs
+    roads.forEach(([src, dst, cost]) => {
+        const adjustedCost = (k + 1) * cost;
+        graph[src].push([dst, adjustedCost]);
+        graph[dst].push([src, adjustedCost]);
+    });
+
+    // queue for bfs
+    const queue = [[0,0]];
+
+    while (queue.length > 0) {
+        const [currentNode, currentCost] = queue.shift();
+
+        if (visited[currentNode] < currentCost) {
+            continue;
         }
-        if (columnSum/m < 0.5) {
-            let replaceColumn = new ChangeValues(columnArray);
-            for (let j = 0; j < replaceColumn.length; j++) {
-                grid[j][i] = replaceColumn[j];
+
+        // explore neighbors
+        graph[currentNode].forEach(([nextNode, travelCost]) => {
+            const newCost = currentCost + travelCost;
+            if (visited[nextNode] > newCost) {
+                visited[nextNode] = newCost;
+                queue.push([nextNode, newCost]);
             }
-        }
+        });
     }
-    let highestScore = 0;
-    for (let i = 0; i < m; i++) {
-        let score = 0;
-        // calculate the binary value of each row
-        for (let j = n - 1; j >= 0; j--) {
-            score += grid[i][j] * 2**((n - 1) - j)
-        }
-        console.log(score);
-        highestScore += score;
-    }
-    console.log(highestScore);
+
+    // prepare the result excluding town 0
+    console.log(visited.slice(1));
 };
 
-matrixScore([[0,0,1,1],[1,0,1,0],[1,1,0,0]]);
-matrixScore([[0]]);
+minCost(4,[[1,2,4],[2,3,2],[2,4,5],[3,4,1],[1,3,4]],[56,42,102,301],2);
+minCost(3,[[1,2,5],[2,3,1],[3,1,2]],[2,3,1],3);
