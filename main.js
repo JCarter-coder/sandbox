8ab1cb5639470672c9ecd1d5bceaa17f66a85dfd
Jesse_Carter <jessetcarter@hotmail.com>
2024-05-19 16:54:34 -0600

    H3068 solved

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
-var distributeCoins = function(root) {
-    let moves = 0;
-    
-    let dfs = function(current) {
-        if (current === null) {
-            return 0;
+var maximumValueSum = function(nums, k, edges) {
+    let totalSum = 0;
+    let count = 0;
+    let positiveMin = Infinity;
+    let negativeMax = -Infinity;
+
+    for (let nodeValue of nums) {
+        let nodeValAfterOperation = nodeValue ^ k;
+        totalSum += nodeValue;
+        let netChange = nodeValAfterOperation - nodeValue;
+
+        if (netChange > 0) {
+            positiveMin = Math.min(positiveMin, netChange);
+            totalSum += netChange;
+            count++;
+        } else {
+            negativeMax = Math.max(negativeMax, netChange);
         }
-
-        // calculate the coins each subtree has
-        // available to exchange
-        let leftCoins = dfs(current.left);
-        let rightCoins = dfs(current.right);
-
-        // add the total number of exchange to moves
-        moves += Math.abs(leftCoins) + Math.abs(rightCoins);
-
-        // the number of coins current has
-        // available to exchange
-        return (current.val - 1) + leftCoins + rightCoins;
     }
 
-    dfs(root);
-    return moves;
+    if (count % 2 === 0) {
+        console.log(totalSum);
+        return totalSum;
+    }
+    console.log(Math.max(totalSum - positiveMin, totalSum + negativeMax))
+    return Math.max(totalSum - positiveMin, totalSum + negativeMax);
 };
 
-distributeCoins([3,0,0]);
-distributeCoins([0,3,0]);
+maximumValueSum([1,2,1],3,[[0,1],[0,2]]);
+maximumValueSum([2,3],7,[[0,1]]);
+maximumValueSum([7,7,7,7,7,7],3,[[0,1],[0,2],[0,3],[0,4],[0,5]]);
