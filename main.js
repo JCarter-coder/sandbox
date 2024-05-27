57fc59e5e259ce5f49bd2cb8333c42024520b916
Jesse_Carter <jessetcarter@hotmail.com>
2024-05-26 22:06:18 -0600

    E1608 solved

--- main.js
+++ main.js
@@ -1,55 +1,25 @@
-var checkRecord = function(n) {
-    const MOD = 1000000007;
-    // cache to store subproblem results
-    const dp = new Array(n + 1).fill(null)
-        .map(() => new Array(2).fill(null)
-        .map(() => new Array(3).fill(0)));
+var specialArray = function(nums) {
+    let N = nums.length;
+    const freq = new Array(N + 1).fill(0);
 
-    // Base case: there is a 1 string of 
-    // length 0 with zero 'A' and zero 'L'
-    dp[0][0][0] = 1;
-
-    // Iterate on smaller sub-problems and use
-    // the current smaller sub-problem to generate
-    // results for bigger sub-problems.
-    for (let len = 0; len < n; ++len) {
-        for (let totalAbsences = 0; totalAbsences <= 1; ++totalAbsences) {
-            for (let consecutiveLates = 0; consecutiveLates <= 2; ++consecutiveLates) {
-                // Store the count when 'P' is chosen
-                dp[len + 1][totalAbsences][0] = (
-                    dp[len + 1][totalAbsences][0] + 
-                    dp[len][totalAbsences][consecutiveLates]
-                ) % MOD;
-                // Store the count when 'A' is chosen
-                if (totalAbsences < 1) {
-                    dp[len + 1][totalAbsences + 1] [0] = (
-                        dp[len + 1][totalAbsences + 1][0] +
-                        dp[len][totalAbsences][consecutiveLates]
-                    ) % MOD;
-                }
-                // Store the count when 'L' is chosen
-                if (consecutiveLates < 2) {
-                    dp[len + 1][totalAbsences][consecutiveLates + 1] = (
-                        dp[len + 1][totalAbsences][consecutiveLates + 1] +
-                        dp[len][totalAbsences][consecutiveLates]
-                    ) % MOD;
-                }
-            }
+    for (let i = 0; i < N; i++) {
+        freq[Math.min(N, nums[i])]++;
+    }
+    
+    let numGreaterThanOrEqual = 0;
+    for (let i = N; i >= 1; i--) {
+        numGreaterThanOrEqual += freq[i];
+        if (i === numGreaterThanOrEqual) {
+            console.log(i);
+            return i;
         }
     }
 
-    // Sum up the counts for all combinations of length 'n'
-    // with different absent and late counts
-    let count = 0;
-    for (let totalAbsences = 0; totalAbsences <= 1; ++totalAbsences) {
-        for (let consecutiveLates = 0; consecutiveLates <= 2; ++ consecutiveLates) {
-            count = (count + dp[n][totalAbsences][consecutiveLates]) % MOD;
-        }
-    }
-    console.log(count);
-    return count;
+    console.log(-1);
+    return -1;
 };
 
-checkRecord(2);
-checkRecord(1);
-checkRecord(10101);
+specialArray([3,5]);
+specialArray([3,5,1]);
+specialArray([0,0]);
+specialArray([0,4,3,0,4]);
