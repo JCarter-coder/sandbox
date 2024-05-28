64b5409a71a3af96756746cc3e09c6510f0e4c0a
Jesse_Carter <jessetcarter@hotmail.com>
2024-05-28 15:32:06 -0600

    M291 solved

--- main.js
+++ main.js
@@ -1,26 +1,55 @@
-var equalSubstring = function(s, t, maxCost) {
-    let N = s.length;
+var wordPatternMatch = function(pattern, s) {
+    const symbolMap = new Map();
+    const wordSet = new Set();
 
-    let maxLength = 0;
-    let start = 0;
-    let currentCost = 0;
-    
-    for (let i = 0; i < N; i++) {
-        currentCost += Math.abs(s.charCodeAt(i) - t.charCodeAt(i));
-
-        while (currentCost > maxCost) {
-            currentCost -= Math.abs(s.charCodeAt(start) - t.charCodeAt(start));
-            start++;
+    function isMatch(s, sIndex, pattern, pIndex, symbolMap, wordSet) {
+        // Base Case: reached end of pattern
+        if (pIndex === pattern.length) {
+            return sIndex === s.length // true iff reached end of s
         }
-        
-        maxLength = Math.max(maxLength, i - start + 1);
+
+        // Get current pattern character
+        let symbol = pattern.charAt(pIndex);
+
+        // This symbol already has an associated word
+        if (symbolMap.has(symbol)) {
+            let word = symbolMap.get(symbol);
+            // Check is we can use it to match
+            // s[sIndex...sIndex + word.length]
+            if (!s.startsWith(word, sIndex)) {
+                return false;
+            }
+            // If it matches continue to match the rest
+            return isMatch(s, sIndex + word.length, pattern, 
+                pIndex + 1, symbolMap, wordSet);
+        }
+
+        // This symbol does not exist in the map
+        for (let k = sIndex + 1; k <= s.length; k++) {
+            let newWord = s.substring(sIndex, k);
+            if (wordSet.has(newWord)) {
+                continue;
+            }
+            // Create or update it
+            symbolMap.set(symbol, newWord);
+            wordSet.add(newWord);
+            // Continue to match the rest
+            if (isMatch(s, k, pattern, pIndex + 1, symbolMap, wordSet)) {
+                return true;
+            }
+            // Backtracking
+            symbolMap.delete(symbol);
+            wordSet.delete(newWord);
+        }
+        // No mappings were valid
+        return false;
     }
 
-    console.log(maxLength);
-    return maxLength;
+    let result = isMatch(s, 0, pattern, 0, symbolMap, wordSet);
+    console.log(result);
+    return result;
 };
 
-equalSubstring("abcd","bcdf",3);
-equalSubstring("abcd","cdef",3);
-equalSubstring("abcd","acde",0);
-equalSubstring("pxezla","loewbi",25);
+wordPatternMatch("abab","redblueredblue");
+wordPatternMatch("aaaa","asdasdasdasd");
+wordPatternMatch("aabb","xyzabcxzyabc");
