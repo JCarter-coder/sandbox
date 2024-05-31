bbda87c8b3b804e3a1b76fe6c141c4bad5799985
Jesse_Carter <jessetcarter@hotmail.com>
2024-05-31 17:31:49 -0600

    M260 solved

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-var kthLuckyNumber = function(k) {
-    console.log((k + 1)
-        .toString(2)
-        .slice(1)
-        .replaceAll(0, 4)
-        .replaceAll(1, 7));
+var singleNumber = function(nums) {
+    // difference between two numbers (x and y)
+    // which were seen only once
+    let bitmask = 0;
+    for (let num of nums) {
+        bitmask ^= num;
+    }
+
+    // rightmost 1-bit diff between x and y
+    let diff = bitmask & (-bitmask);
+
+    let x = 0;
+    // bitmask which will contain only x
+    for (let num of nums) {
+        if ((num & diff) !== 0) {
+            x ^= num;
+        }
+    }
+
+    console.log([x, bitmask^x]);
 };
 
-kthLuckyNumber(4);
-kthLuckyNumber(10);
-kthLuckyNumber(1000);
+singleNumber([1,2,1,3,2,5]);
+singleNumber([-1,0]);
+singleNumber([0,1]);
