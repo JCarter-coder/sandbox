e7c998e2a3ad7bb4f2afcef63bc0412395c8d418
Jesse_Carter <jessetcarter@hotmail.com>
2024-06-03 15:05:42 -0600

    M1940 solved

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-var appendCharacters = function(s, t) {
-    let first = 0;
-    let longestPrefix = 0;
-    while (first < s.length && longestPrefix < t.length) {
-        if (s.charAt(first) === t.charAt(longestPrefix)) {
-            longestPrefix++;
+var longestCommonSubsequence = function(arrays) {
+    let isInAllArrays = arrays.length;
+    const seen = {};
+    let result = [];
+    for (let i = 0; i < arrays.length; i++) {
+        let index = 0;
+        while (arrays[i][index]) {
+            if (!seen[arrays[i][index]]) {
+                seen[arrays[i][index]] = 1;
+            } else seen[arrays[i][index]]++;
+            index++;
         }
-        first++;
     }
-    console.log(t.length - longestPrefix);
+    for (const [key, value] of Object.entries(seen)) {
+        if (value === isInAllArrays) {
+            result.push(parseInt(key));
+        }
+    }
+    console.log(result);
 };
 
-appendCharacters("coaching","coding");
-appendCharacters("abcde","a");
-appendCharacters("z","abcde");
+longestCommonSubsequence([[1,3,4],[1,4,7,9]]);
+longestCommonSubsequence([[2,3,6,8],[1,2,3,5,6,7,10],[2,3,4,6,9]]);
+longestCommonSubsequence([[1,2,3,4,5],[6,7,8]]);
