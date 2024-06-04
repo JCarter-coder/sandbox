83547300b3ec73994b07b535dd2951ce2040fd6c
Jesse_Carter <jessetcarter@hotmail.com>
2024-06-04 14:44:20 -0600

    E409 solved

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
-var longestCommonSubsequence = function(arrays) {
-    let isInAllArrays = arrays.length;
+var longestPalindrome = function(s) {
     const seen = {};
-    let result = [];
-    for (let i = 0; i < arrays.length; i++) {
-        let index = 0;
-        while (arrays[i][index]) {
-            if (!seen[arrays[i][index]]) {
-                seen[arrays[i][index]] = 1;
-            } else seen[arrays[i][index]]++;
-            index++;
+    for (let i = 0; i < s.length; i++) {
+        if (!seen[s[i]]) {
+            seen[s[i]] = 1;
+        } else seen[s[i]]++;
+    }
+
+    // total of each even value letter
+    let totalEvenValue = 0;
+    // greatest odd value of a letter in seen container
+    let greatestOddValue = 0;
+    
+    for (const [key, val] of Object.entries(seen)) {
+        // if even, add value to evenSeenLetters total
+        if (val % 2 === 0) {
+            totalEvenValue += val;
+        } 
+        // else if odd value, hold greatest odd value
+        else if (val > greatestOddValue) {
+            let temp = greatestOddValue - 1;
+            greatestOddValue = val;
+            // when greater odd value is found, add the
+            // adjusted temp value to the totalEvenValue
+            if (temp > 0) {
+                totalEvenValue += temp;
+            }
+        }
+        // else, adjust odd values by substracting one
+        // to make it even to increase longest palindrome
+        else {
+            totalEvenValue += val - 1;
         }
     }
-    for (const [key, value] of Object.entries(seen)) {
-        if (value === isInAllArrays) {
-            result.push(parseInt(key));
-        }
-    }
-    console.log(result);
+
+    console.log(totalEvenValue + greatestOddValue);
 };
 
-longestCommonSubsequence([[1,3,4],[1,4,7,9]]);
-longestCommonSubsequence([[2,3,6,8],[1,2,3,5,6,7,10],[2,3,4,6,9]]);
-longestCommonSubsequence([[1,2,3,4,5],[6,7,8]]);
+longestPalindrome("abccccdd");
+longestPalindrome("a");
