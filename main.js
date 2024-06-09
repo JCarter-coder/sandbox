dd63e6da7bcfb2a75f52323d9b373d6320839aba
Jesse_Carter <jessetcarter@hotmail.com>
2024-06-09 09:49:16 -0600

    M2628 solved

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
-var curry = function(fn) {
-    
-    return function curried(...args) {
-        if (args.length >= fn.length) {
-            return fn.apply(this, args);
-        }
+var areDeeplyEqual = function(o1, o2) {
+    if (o1 === o2) return true;
+    if (o1 === null || o2 === null) return false;
 
-        return curried.bind(this, ...args);
+    var objs = [[o1, o2]];
+    
+    while (objs.length) {
+        [o1, o2] = objs.pop();
+
+        if (o1 === o2) continue;
+        if (typeof o1 !== 'object' || typeof o2 !== 'object') return false;
+        if (Array.isArray(o1) !== Array.isArray(o2)) return false;
+
+        const keys1 = Object.keys(o1);
+        const keys2 = Object.keys(o2);
+
+        if (keys1.length !== keys2.length) return false;
+        for (const key of keys1) {
+            if (!(key in o2)) return false;
+            objs.push([o1[key], o2[key]]);
+        }
     }
+
+    return true;
 };
 
-curry(function sum(a, b, c) { return a + b + c; },[[1],[2],[3]]);
-curry(function sum(a, b, c) { return a + b + c; },[[1,2],[3]]);
-curry(function sum(a, b, c) { return a + b + c; },[[],[],[1,2,3]]);
-curry(function life() { return 42; },[[]]);
+areDeeplyEqual({"x":1,"y":2},{"x":1,"y":2});
+areDeeplyEqual({"y":2,"x":1},{"x":1,"y":2});
+areDeeplyEqual({"x":null,"L":[1,2,3]},{"x":null,"L":["1","2","3"]});
+areDeeplyEqual(true, false);
