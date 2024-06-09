2167ae07d701e7869fdf90115f56fe0592ce70aa
Jesse_Carter <jessetcarter@hotmail.com>
2024-06-09 09:20:11 -0600

    M2676 solved

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
-var promisePool = async function(functions, n) {
-    async function evaluateNext() {
-        if (functions.length === 0) return;
-        const fn = functions.shift();
-        await fn();
-        await evaluateNext();
+var throttle = function(fn, t) {
+    let intervalInProgress = null;
+    let argsToProcess = null;
+
+    const intervalFunction = () => {
+        if (argsToProcess === null) {
+            clearInterval(intervalInProgress);
+            // enter the waiting phase
+            intervalInProgress = null;
+        } else {
+            fn(...argsToProcess);
+            argsToProcess = null;
+        }
+    };
+    
+    return function(...args) {
+        if (intervalInProgress) {
+            argsToProcess = args;
+        } else {
+            // enter the looping phase
+            fn(...args);
+            intervalInProgress = setInterval(intervalFunction, t);
+        }
     }
-    const nPromises = new Array(n).fill().map(evaluateNext);
-    await Promise.all(nPromises);
 };
 
-promisePool([() => new Promise(res => setTimeout(res, 300)), () => new Promise(res => setTimeout(res, 400)), () => new Promise(res => setTimeout(res, 200))],2);
-promisePool([() => new Promise(res => setTimeout(res, 300)), () => new Promise(res => setTimeout(res, 400)), () => new Promise(res => setTimeout(res, 200))],5);
-promisePool([() => new Promise(res => setTimeout(res, 300)), () => new Promise(res => setTimeout(res, 400)), () => new Promise(res => setTimeout(res, 200))],1);
+throttle(100,[{"t":20,"inputs":[1]}]);
+throttle(50,[{"t":50,"inputs":[1]},{"t":75,"inputs":[2]}]);
+throttle(70,[{"t":50,"inputs":[1]},{"t":75,"inputs":[2]},{"t":90,"inputs":[8]},{"t": 140, "inputs":[5,7]},{"t": 300, "inputs": [9,4]}]);
