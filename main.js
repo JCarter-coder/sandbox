3b661c3bddbd10779c75ef094e77d8118d2512e5
Jesse_Carter <jessetcarter@hotmail.com>
2024-06-09 09:28:28 -0600

    M2632 solved

--- main.js
+++ main.js
@@ -1,29 +1,15 @@
-var throttle = function(fn, t) {
-    let intervalInProgress = null;
-    let argsToProcess = null;
-
-    const intervalFunction = () => {
-        if (argsToProcess === null) {
-            clearInterval(intervalInProgress);
-            // enter the waiting phase
-            intervalInProgress = null;
-        } else {
-            fn(...argsToProcess);
-            argsToProcess = null;
-        }
-    };
+var curry = function(fn) {
     
-    return function(...args) {
-        if (intervalInProgress) {
-            argsToProcess = args;
-        } else {
-            // enter the looping phase
-            fn(...args);
-            intervalInProgress = setInterval(intervalFunction, t);
+    return function curried(...args) {
+        if (args.length >= fn.length) {
+            return fn.apply(this, args);
         }
+
+        return curried.bind(this, ...args);
     }
 };
 
-throttle(100,[{"t":20,"inputs":[1]}]);
-throttle(50,[{"t":50,"inputs":[1]},{"t":75,"inputs":[2]}]);
-throttle(70,[{"t":50,"inputs":[1]},{"t":75,"inputs":[2]},{"t":90,"inputs":[8]},{"t": 140, "inputs":[5,7]},{"t": 300, "inputs": [9,4]}]);
+curry(function sum(a, b, c) { return a + b + c; },[[1],[2],[3]]);
+curry(function sum(a, b, c) { return a + b + c; },[[1,2],[3]]);
+curry(function sum(a, b, c) { return a + b + c; },[[],[],[1,2,3]]);
+curry(function life() { return 42; },[[]]);
