26a113ee64331d0af8058fe841f03e90cce15653
Jesse_Carter <jessetcarter@hotmail.com>
2024-06-09 22:02:53 -0600

    E1051 solved

--- main.js
+++ main.js
@@ -1,30 +1,14 @@
-var areDeeplyEqual = function(o1, o2) {
-    if (o1 === o2) return true;
-    if (o1 === null || o2 === null) return false;
-
-    var objs = [[o1, o2]];
-    
-    while (objs.length) {
-        [o1, o2] = objs.pop();
-
-        if (o1 === o2) continue;
-        if (typeof o1 !== 'object' || typeof o2 !== 'object') return false;
-        if (Array.isArray(o1) !== Array.isArray(o2)) return false;
-
-        const keys1 = Object.keys(o1);
-        const keys2 = Object.keys(o2);
-
-        if (keys1.length !== keys2.length) return false;
-        for (const key of keys1) {
-            if (!(key in o2)) return false;
-            objs.push([o1[key], o2[key]]);
+var heightChecker = function(heights) {
+    const comparedArray = heights.toSorted((a,b) => a - b);
+    let notExpected = 0;
+    for (let i = 0; i < heights.length; i++) {
+        if (heights[i] !== comparedArray[i]) {
+            notExpected++;
         }
     }
-
-    return true;
+    console.log(notExpected);
 };
 
-areDeeplyEqual({"x":1,"y":2},{"x":1,"y":2});
-areDeeplyEqual({"y":2,"x":1},{"x":1,"y":2});
-areDeeplyEqual({"x":null,"L":[1,2,3]},{"x":null,"L":["1","2","3"]});
-areDeeplyEqual(true, false);
+heightChecker([1,1,4,2,1,3]);
+heightChecker([5,1,2,3,4]);
+heightChecker([1,2,3,4,5]);
