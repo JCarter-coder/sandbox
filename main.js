c7feca58f864760816a145983461074930e0e363
Jesse_Carter <jessetcarter@hotmail.com>
2024-06-10 22:05:53 -0600

    E1122 solved

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
-var isSubsequence = function(s, t) {
-    let N = s.length;
-    let sCount = 0;
-    let index = 0;
-    let next = 0;
-    while (index < t.length) {
-        if (s[next] === t[index]) {
-            sCount++;
-            next++;
+var relativeSortArray = function(arr1, arr2) {
+    const seen = {};
+    for (let i = 0; i < arr1.length; i++) {
+        if (!seen[arr1[i]]) {
+            seen[arr1[i]] = 1;
+        } else seen[arr1[i]]++;
+    }
+    arr1 = [];
+    // rebuild arr1 with arr2 ordering
+    for (let i = 0; i < arr2.length; i++) {
+        let seenKey = arr2[i];
+        while (seen[seenKey]-- > 0) {
+            arr1.push(seenKey);
         }
-        index++;
     }
-    // if count for every s index, then true
-    if (sCount === N) {
-        console.log('true');
-        return true;
+    for (const [key, value] of Object.entries(seen)) {
+        let tempVal = value;
+        while (tempVal > 0) {
+            arr1.push(parseInt(key));
+            tempVal--;
+        }
     }
-    console.log('false');
-    return false;
+    console.log(arr1);
 };
 
-isSubsequence("abc","ahbgdc");
-isSubsequence("axc","ahbgdc");
+relativeSortArray([2,3,1,3,2,4,6,7,9,2,19],[2,1,4,3,9,6]);
+relativeSortArray([28,6,22,8,44,17],[22,28,8,6]);
