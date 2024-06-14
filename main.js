e05225dffd2b2b8f676a4379d368f9b2cfe24f87
Jesse_Carter <jessetcarter@hotmail.com>
2024-06-14 17:10:36 -0600

    M945 solved

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-var numberOfSubstrings = function(s) {
-    let answer = 0;
-    const prefixCount = new Array(26).fill(0);
-    for (let i = 0; i < s.length; i++) {
-        prefixCount[s.charCodeAt(i) - 97]++;
-        answer += prefixCount[s.charCodeAt(i) - 97];
+var minIncrementForUnique = function(nums) {
+    let N = nums.length;
+    let max = 0;
+    let minIncrements = 0;
+
+    for (let val of nums) {
+        max = Math.max(max, val);
     }
-    console.log(answer);
+
+    const freqCount = new Array(N + max).fill(0);
+
+    for (let val of nums) {
+        freqCount[val]++;
+    }
+
+    for (let i = 0; i < freqCount.length; i++) {
+        if (freqCount[i] <= 1) continue;
+        let duplicates = freqCount[i] - 1;
+        freqCount[i + 1] += duplicates;
+        freqCount[i] = 1;
+        minIncrements += duplicates;
+    }
+
+    console.log(minIncrements);
 };
 
-numberOfSubstrings("abcba");
-numberOfSubstrings("abacad");
-numberOfSubstrings("a");
+minIncrementForUnique([1,2,2]);
+minIncrementForUnique([3,2,1,2,1,7]);
