cdd892dcf66288bbe29176986dc6bfd31c5c6962
Jesse_Carter <jessetcarter@hotmail.com>
2024-06-20 14:51:04 -0600

    M1580 solved

--- main.js
+++ main.js
@@ -1,36 +1,25 @@
-var maxDistance = function(position, m) {
-    let answer = 0;
-    let N = position.length;
-    position.sort((a,b) => a - b);
+var maxBoxesInWarehouse = function(boxes, warehouse) {
+    let warehouseSize = warehouse.length;
+    boxes.sort((a,b) => a - b);
 
-    let canPlaceBalls = function(x, position, m) {
-        let prevBallPos = position[0];
-        let ballsPlaced = 1;
+    let leftIndex = 0;
+    let rightIndex = warehouseSize - 1;
+    let boxCount = 0;
+    let boxIndex = boxes.length - 1;
 
-        for (let i = 0; i < position.length && ballsPlaced < m; i++) {
-            let currentPos = position[i];
-            if (currentPos - prevBallPos >= x) {
-                ballsPlaced++;
-                prevBallPos = currentPos;
-            }
+    while (leftIndex <= rightIndex && boxIndex >= 0) {
+        if (boxes[boxIndex] <= warehouse[leftIndex]) {
+            boxCount++;
+            leftIndex++;
+        } else if (boxes[boxIndex] <= warehouse[rightIndex]) {
+            boxCount++;
+            rightIndex--;
         }
-        return ballsPlaced === m;
+        boxIndex--;
     }
 
-    // Initial search space
-    let low = 1;
-    let high = Math.ceil(position[N - 1] / (m - 1.0));
-    while (low <= high) {
-        let mid = Math.floor(low + (high - low) / 2);
-        if (canPlaceBalls(mid, position, m)) {
-            answer = mid;
-            low = mid + 1;
-        } else high = mid - 1;
-    }
-
-    console.log(answer);
+    console.log(boxCount);
 };
 
-maxDistance([1,2,3,4,7],3);
-maxDistance([5,4,3,2,1,1000000000],2);
-maxDistance([1,2,3,4,5,6,7,8,9,10],4);
+maxBoxesInWarehouse([1,2,2,3,4],[3,4,1,2]);
+maxBoxesInWarehouse([3,5,5,2],[2,1,3,4,5]);
