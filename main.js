c37db5e19009d85c4e2041b865c028f8e8080faa
Jesse_Carter <jessetcarter@hotmail.com>
2024-06-24 08:01:22 -0600

    H995 solved

--- main.js
+++ main.js
@@ -1,45 +1,25 @@
-var longestSubarray = function(nums, limit) {
-    let maxDeque = new Array();
-    let minDeque = new Array();
+var minKBitFlips = function(nums, k) {
+    let currentFlips = 0;
+    let totalFlips = 0;
 
-    let left = 0
-    let maxLength = 0;
-
-    for (let right = 0; right < nums.length; right++) {
-        // maintain maxDeque in decreasing order
-        while (maxDeque.length > 0 && maxDeque[maxDeque.length - 1] < nums[right]) {
-            maxDeque.pop();
+    for (let i = 0; i < nums.length; i++) {
+        if (i >= k && nums[i - k] === 2) {
+            currentFlips--;
         }
-        maxDeque.push(nums[right]);
-        // maintain minDeque in increasing order
-        while (minDeque.length > 0 && minDeque[minDeque.length - 1] > nums[right]) {
-            minDeque.pop();
-        }
-        minDeque.push(nums[right]);
-        //console.log(`Max: ${maxDeque}`);
-        //console.log(`Min: ${minDeque}`);
-        //console.log(`Nums[left] = ${nums[left]}`);
-
-        // check to see if window exceeds the limit
-        while (maxDeque[0] - minDeque[0] > limit) {
-            // remove the elements that are outside of the current window
-            if (maxDeque[0] === nums[left]) {
-                maxDeque.shift();
+        if (currentFlips % 2 === nums[i]) {
+            if (i + k > nums.length) {
+                console.log(-1);
+                return -1;
             }
-            if (minDeque[0] === nums[left]) {
-                minDeque.shift();
-            }
-            ++left;
+            nums[i] = 2;
+            currentFlips++;
+            totalFlips++;
         }
-        //console.log(`MaxLength: ${maxLength}`);
-        //console.log(`Compare to: ${right - left + 1}`);
-
-        maxLength = Math.max(maxLength, right - left + 1);
     }
 
-    console.log(maxLength);
+    console.log(totalFlips);
 };
 
-longestSubarray([8,2,4,7],4);
-longestSubarray([10,1,2,4,7,2],5);
-longestSubarray([4,2,2,2,4,4,2,2],0);
+minKBitFlips([0,1,0],1);
+minKBitFlips([1,1,0],2);
+minKBitFlips([0,0,0,1,0,1,1,0],3);
