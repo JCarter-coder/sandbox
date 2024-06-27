8cae0f402b59c3c6a1c88660da3d965bd458fbe5
Jesse_Carter <jessetcarter@hotmail.com>
2024-06-26 20:51:28 -0600

    E1791 solved

--- main.js
+++ main.js
@@ -1,32 +1,10 @@
-class TreeNode {
-    constructor(val, left, right) {
-        this.val = (val===undefined ? 0 : val);
-        this.left = (left===undefined ? null : left);
-        this.right = (right===undefined ? null : right);
+var findCenter = function(edges) {
+    if (edges[1][0] === edges[0][0] || edges[1][0] === edges[0][1]) {
+        console.log(edges[1][0]);
+    } else {
+        console.log(edges[1][1]);
     }
-}
-
-var inOrder = function(root, result) {
-    if (!root) return [];
-    inOrder(root.left, result);
-    result.push(root.val);
-    inOrder(root.right, result);
-    return result;
-}
-
-var createBST = function(arr, left, right) {
-    if (left > right) return null;
-    const mid = Math.floor((left + right) / 2);
-    const tree = new TreeNode(arr[mid]);
-    tree.left = createBST(arr, left, mid - 1);
-    tree.right = createBST(arr, mid + 1, right);
-    return tree;
-}
-
-var balanceBST = function(root) {
-    const nums = inOrder(root, []);
-    return createBST(nums, 0, nums.length - 1);
 };
 
-balanceBST([1,null,2,null,3,null,4]);
-balanceBST([2,1,3]);
+findCenter([[1,2],[2,3],[4,2]]);
+findCenter([[1,2],[5,1],[1,3],[1,4]]);
