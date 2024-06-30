2d4cda67404a87f956031f8ffdbc76552ddd41c8
Jesse_Carter <jessetcarter@hotmail.com>
2024-06-30 14:15:27 -0600

    M776 solved

--- main.js
+++ main.js
@@ -1,43 +1,16 @@
-var getAncestors = function(n, edges) {
-    // Initialize adjacency list for each node and ancestors list
-    const adjacencyList = new Array(n);
-    const ancestors = new Array();
+var splitBST = function(root, target) {
+    if (!root) return [null, null];
 
-    let findAncestorsDFS = function(ancestor, adjacencyList, currrentNode, ancestors) {
-        for (let childNode of adjacencyList[currrentNode]) {
-            // Check if the ancestor is already added to avoid duplicates
-            if (ancestors[childNode].length === 0 ||
-                ancestors[childNode][ancestors[childNode].length - 1] != ancestor
-            ) {
-                ancestors[childNode].push(ancestor);
-                findAncestorsDFS(ancestor, adjacencyList, childNode, ancestors);
-            }
-        }
+    if (root.val <= target) {
+        const [lesser, greater] = splitBST(root.right, target);
+        root.right = lesser;
+        return [root, greater];
+    } else {
+        const [lesser, greater] = splitBST(root.left, target);
+        root.left = greater;
+        return [lesser, root];
     }
-
-    // Populate the adjacency list with edges
-    for (let i = 0; i < n; i++) {
-        adjacencyList[i] = new Array();
-        ancestors.push(new Array());
-    }
-
-    // Populate the adjacency list with edges
-    for (let edge of edges) {
-        let from = edge[0];
-        let to = edge[1];
-        adjacencyList[from].push(to);
-    }
-
-    // Perform DFS for each node to find all its ancestors
-    for (let i = 0; i < n; i++) {
-        findAncestorsDFS(i, adjacencyList, i, ancestors);
-    }
-
-    //console.log(adjacencyList);
-
-    console.log(ancestors);
-    return ancestors;    
 };
 
-getAncestors(8,[[0,3],[0,4],[1,3],[2,4],[2,7],[3,5],[3,6],[3,7],[4,6]]);
-getAncestors(5,[[0,1],[0,2],[0,3],[0,4],[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]);
+splitBST([4,2,6,1,3,5,7],2);
+splitBST([1],1);
