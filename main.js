19d653f6541038707ef57d60d93a68071a2d94b5
Jesse_Carter <jessetcarter@hotmail.com>
2024-07-02 07:00:10 -0600

    E350 solved

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
-var threeConsecutiveOdds = function(arr) {
-    let count = 0
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 === 1) count++;
-        else count = 0;
-        if (count === 3) {
-            console.log("true");
-            return true;
-        }
+var intersect = function(nums1, nums2) {
+    nums1.sort((a,b) => a - b);
+    nums2.sort((a,b) => a - b);
+    let ptr1 = 0;
+    let ptr2 = 0;
+    let result = [];
+
+
+    while (ptr1 < nums1.length && ptr2 < nums2.length) {
+        if (nums1[ptr1] === nums2[ptr2]) {
+            result.push(nums1[ptr1]);
+            ptr1++;
+            ptr2++;
+        } else if (nums1[ptr1] > nums2[ptr2]) ptr2++;
+        else ptr1++;
     }
 
-    console.log("false");
-    return false;
+    console.log(result);
 };
 
-threeConsecutiveOdds([2,6,4,1]);
-threeConsecutiveOdds([1,2,34,3,4,5,7,23,12]);
+intersect([1,2,2,1],[2,2]);
+intersect([4,9,5],[9,4,9,8,4]);
+intersect([4,7,9,7,6,7],[5,0,0,6,1,6,2,2,4])
