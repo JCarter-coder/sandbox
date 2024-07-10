0c1b78a92f937e79fc9e8ad3462d954f2dd28dbe
Jesse_Carter <jessetcarter@hotmail.com>
2024-07-10 09:07:26 -0600

    E1598 solved

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-var averageWaitingTime = function(customers) {
-    let sum = 0;
-    let nextAvailableTime = customers[0][0];
-    for (let i = 0; i < customers.length; i++) {
-        if (customers[i][0] > nextAvailableTime) {
-            nextAvailableTime = customers[i][0] + customers[i][1];
-        } else nextAvailableTime += customers[i][1];
-        let waitTime = nextAvailableTime - customers[i][0];
-        sum += waitTime;
+var minOperations = function(logs) {
+    let directory = [];
+    for (let i = 0; i < logs.length; i++) {
+        if (logs[i] === "../") {
+            directory.pop();
+        } else if (logs[i] === "./") continue;
+        else directory.push(logs[i]);
     }
-    console.log(sum / customers.length);
+
+    console.log(directory.length);
+    return directory.length;
 };
 
-averageWaitingTime([[1,2],[2,5],[4,3]]);
-averageWaitingTime([[5,2],[5,4],[10,3],[20,1]]);
+minOperations(["d1/","d2/","../","d21/","./"]);
+minOperations(["d1/","d2/","./","d3/","../","d31/"]);
+minOperations(["d1/","../","../","../"]);
