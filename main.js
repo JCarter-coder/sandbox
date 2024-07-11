1e125cfe95616f3466a947e6f24e06077e9d5563
Jesse_Carter <jessetcarter@hotmail.com>
2024-07-10 19:40:16 -0600

    M1190 solved

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
-var minOperations = function(logs) {
-    let directory = [];
-    for (let i = 0; i < logs.length; i++) {
-        if (logs[i] === "../") {
-            directory.pop();
-        } else if (logs[i] === "./") continue;
-        else directory.push(logs[i]);
+var reverseParentheses = function(s) {
+    var reverse = function(sub) {
+        for (let i = 0; i < (sub.length / 2); i++) {
+            let temp = sub[i];
+            sub[i] = sub[sub.length - 1 - i];
+            sub[sub.length - 1 - i] = temp;
+        }
+        return sub;
+    }
+    s = s.split('');
+    
+    let left = 0;
+    let right = 1;
+
+    while (left < right && right < s.length) {
+        if (s[right] === '(') {
+            left = right;
+        } else if (s[right] === ')') {
+            let subString = reverse(s.slice(left+1, right));
+            for (let i = 0; i < subString.length; i++) {
+                s.splice(left + i, 1, subString[i]);
+            }
+            s.splice(right - 1, 2);
+            left = 0;
+            right = 1;
+            continue;
+        }
+        right++;
     }
 
-    console.log(directory.length);
-    return directory.length;
+    console.log(s.join(''));
 };
 
-minOperations(["d1/","d2/","../","d21/","./"]);
-minOperations(["d1/","d2/","./","d3/","../","d31/"]);
-minOperations(["d1/","../","../","../"]);
+reverseParentheses("(abcd)");
+reverseParentheses("(u(love)i)");
+reverseParentheses("(ed(et(oc))el)");
