8c9859f641d5b58854f4f7c829e6395c301c1584
Jesse_Carter <jessetcarter@hotmail.com>
2024-07-19 10:58:54 -0600

    E1 solved

--- main.js
+++ main.js
@@ -1,36 +1,19 @@
-var countPairs = function(root, distance) {
-    let count = 0;
-    const MAX_DISTANCE = 10;
-
-    function dfs(node) {
-        if (!node) return new Array(MAX_DISTANCE + 1).fill(0);
-        if (!node.left && !node.right) {
-            const res = new Array(MAX_DISTANCE + 1).fill(0);
-            res[1] = 1;
-            return res;
+var twoSum = function(nums, target) {
+    const seen = {};
+    let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (seen[target - nums[i]] === undefined) {
+            seen[nums[i]] = i;
+        } else {
+            result.push(seen[target - nums[i]]);
+            result.push(i);
+            break;
         }
-
-        const left = dfs(node.left);
-        const right = dfs(node.right);
-
-        for (let i = 1; i <= distance; i++) {
-            for (let j = 1; j <= distance - i; j++) {
-                count += left[i] * right[j];
-            }
-        }
-
-        const res = new Array(MAX_DISTANCE + 1).fill(0);
-        for (let i = 1; i < MAX_DISTANCE; i++) {
-            res[i + 1] = left[i] + right[i];
-        }
-
-        return res;
     }
 
-    dfs(root);
-    return count;
+    console.log(result);
 };
 
-countPairs([1,2,3,null,4],3);
-countPairs([1,2,3,4,5,6,7],3);
-countPairs([7,1,4,6,null,5,3,null,null,null,null,null,2],3);
+twoSum([2,7,11,15],9);
+twoSum([3,2,4],6);
+twoSum([3,3],6);
