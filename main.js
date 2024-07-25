2ed5aa6ab64aff4593067a6a114a2fd4ce443933
Jesse_Carter <jessetcarter@hotmail.com>
2024-07-25 14:15:14 -0600

    M912 solved

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
-var convert = function(s, numRows) {
-    if (numRows === 1) return s;
+var sortArray = function(nums) {
+    const countingSort = () => {
+        // Create the counting hash map
+        const counts = {};
+        // Find the min and max values in the array
+        let minVal = Math.min(...nums);
+        let maxVal = Math.max(...nums);
 
-    let result = "";
-    let n = s.length;
-    let zigzag = 2 * (numRows - 1);
+        // Update element's count in the hash map
+        nums.forEach(val => {
+            if (counts[val] === undefined) counts[val] = 0;
+            counts[val]++;
+        });
 
-    for (let currRow = 0; currRow < numRows; ++currRow) {
-        let index = currRow;
-        
-        while (index < n) {
-            result += s[index];
-            // If current row is not first or last,
-            // then we have to add one more character
-            // to current section
-            if (currRow != 0 && currRow != numRows - 1) {
-                let charsInBetween = zigzag - 2 * currRow;
-                let secondIndex = index + charsInBetween;
-
-                if (secondIndex < n) result += s[secondIndex];
+        let i = 0;
+        // Place each element in its correct
+        // position within the array
+        for (let val = minVal; val <= maxVal; val++) {
+            // Append all vals together if they exist
+            while (counts[val] > 0) {
+                nums[i] = val;
+                i++;
+                counts[val]--;
             }
-            // Jump to same row's first char of next zigzag
-            index += zigzag;
         }
     }
-    
-    console.log(result);
+
+    countingSort(nums);
+    console.log(nums);
 };
 
-convert("PAYPALISHIRING",3);
-convert("PAYPALISHIRING",4);
-convert("A",1);
+sortArray([5,2,3,1]);
+sortArray([5,1,1,2,0,0]);
