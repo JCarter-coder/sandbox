94d8e5ba5d2e3e96d70a4756fbb46100a2372e08
Jesse_Carter <jessetcarter@hotmail.com>
2024-07-27 14:51:19 -0600

    M2976 solved

--- main.js
+++ main.js
@@ -1,34 +1,68 @@
-var myPow = function(x, n) {
-    const binaryExp = (x, n) => {
-        if (n === 0) return 1;
-
-        // Handle case where, n < 0
-        if (n < 0) {
-            n = -1 * n;
-            x = 1.0 / x;
-        }
-
-        // Perform Binary Exponentiation
-        let result = 1;
-        while (n !== 0) {
-            // If 'n' is odd we multiply result with 'x'
-            // and reduce 'n' by '1'
-            if (n % 2 === 1) {
-                result *= x;
-                n -= 1;
-            }
-            // We square 'x' and reduce 'n' by half,
-            // x^n => (x^2)^(n/2)
-            x *= x;
-            n = Math.floor(n / 2);
-        }
-        return result;
+var minimumCost = function(source, target, original, changed, cost) {
+    // Initialize result to store min cost
+    let totalCost = 0;
+    // Initialize 2D array to store min conversion
+    // cost between any two characters
+    const minCost = [];
+    for (let i = 0; i < 26; i++) {
+        minCost.push(new Array(26).fill(Number.MAX_SAFE_INTEGER))
     }
-    
-    console.log(binaryExp(x, n));
+    // Fill the initial conversion costs from the 
+    // given original, changed, and cost arrays
+    for (let i = 0; i < original.length; ++i) {
+        let startChar = original[i].charCodeAt(0) - 97;
+        let endChar = changed[i].charCodeAt(0) - 97;
+        minCost[startChar][endChar] = Math.min(
+            minCost[startChar][endChar],
+            cost[i]
+        );
+    }
+    // Use Floyd-Warshall algorithm to find the
+    // shortest path between any two characters
+    for (let k = 0; k < 26; k++) {
+        for (let i = 0; i < 26; i++) {
+            for (let j = 0; j < 26; j++) {
+                minCost[i][j] = Math.min(
+                    minCost[i][j],
+                    minCost[i][k] + minCost[k][j]
+                );
+            }
+        }
+    }
+    // Calculate the total min cost to transform
+    // the source string to the target string
+    for (let i = 0; i < source.length; i++) {
+        if (source.charCodeAt(i) === target.charCodeAt(i)) {
+            continue;
+        }
+        let sourceChar = source.charCodeAt(i) - 97;
+        let targetChar = target.charCodeAt(i) - 97;
+        // If the transformation is not possible, return -1
+        if (minCost[sourceChar][targetChar] >= Number.MAX_SAFE_INTEGER) {
+            console.log(-1);
+            return -1;
+        }
+        totalCost += minCost[sourceChar][targetChar];
+    }
+    console.log(totalCost);
+    return totalCost;
 };
 
-myPow(2.00000,10);
-myPow(2.10000,3);
-myPow(2.00000,-2);
-myPow(5.00000, 0);
+minimumCost("abcd",
+            "acbe",
+            ["a","b","c","c","e","d"],
+            ["b","c","b","e","b","e"],
+            [2,5,5,1,2,20]
+        );
+minimumCost("aaaa",
+            "bbbb",
+            ["a","c"],
+            ["c","b"],
+            [1,2]
+        );
+minimumCost("abcd",
+            "abce",
+            ["a"],
+            ["e"],
+            [10000]
+        );
