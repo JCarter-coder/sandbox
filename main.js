77311035af6ff207c4af93c860abca62ca45dc2e
Jesse_Carter <jessetcarter@hotmail.com>
2024-07-28 22:19:52 -0600

    M1395 solved

--- main.js
+++ main.js
@@ -1,45 +1,30 @@
-var secondMinimum = function(n, edges, time, change) {
-    const adj = Array.from({ length: n + 1 }, () => []);
+var numTeams = function(rating) {
+    let N = rating.length;
+    let teams = 0;
+    
+    for (let mid = 0; mid < N; mid++) {
+        let leftSmaller = 0;
+        let rightLarger = 0;
 
-    for (const [v1, v2] of edges) {
-        adj[v1].push(v2);
-        adj[v2].push(v1);
-    }
-
-    const queue = [1];
-    let curTime = 0;
-    let res = -1;
-    const visitTimes = Array.from({ length: n + 1 }, () => []);
-
-    while (queue.length > 0) {
-        const levelSize = queue.length;
-        for (let i = 0; i < levelSize; i++) {
-            const node = queue.shift();
-
-            if (node === n) {
-                if (res !== -1) return curTime;
-                res = curTime;
-            }
-
-            for (const nei of adj[node]) {
-                const neiTimes = visitTimes[nei];
-
-                if (neiTimes.length === 0 || (neiTimes.length === 1 && neiTimes[0] !== curTime)) {
-                    queue.push(nei);
-                    visitTimes[nei].push(curTime);
-                }
-            }
+        for (let left = mid - 1; left >= 0; left--) {
+            if (rating[left] < rating[mid]) leftSmaller++;
         }
 
-        if (Math.floor(curTime / change) % 2 === 1) {
-            curTime += change - (curTime % change);
+        for (let right = mid + 1; right < N; right++) {
+            if (rating[right] > rating[mid]) rightLarger++;
         }
 
-        curTime += time;
+        teams += leftSmaller * rightLarger;
+
+        let leftLarger = mid - leftSmaller;
+        let rightSmaller = N - mid - 1 - rightLarger;
+
+        teams += leftLarger * rightSmaller;
     }
 
-    return -1;
+    console.log(teams);
 };
 
-secondMinimum(5,[[1,2],[1,3],[1,4],[3,4],[4,5]],3,5);
-secondMinimum(2,[[1,2]],3,2);
+numTeams([2,5,3,4,1]);
+numTeams([2,1,3]);
+numTeams([1,2,3,4]);
