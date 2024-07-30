0e812cf944e8439cad2ea685e882498f1fc74018
Jesse_Carter <jessetcarter@hotmail.com>
2024-07-30 13:23:21 -0600

    M1653 solved

--- main.js
+++ main.js
@@ -1,30 +1,15 @@
-var numTeams = function(rating) {
-    let N = rating.length;
-    let teams = 0;
-    
-    for (let mid = 0; mid < N; mid++) {
-        let leftSmaller = 0;
-        let rightLarger = 0;
+var minimumDeletions = function(s) {
+    let N = s.length;
+    let minDeletions = 0;
+    let bCount = 0;
 
-        for (let left = mid - 1; left >= 0; left--) {
-            if (rating[left] < rating[mid]) leftSmaller++;
-        }
-
-        for (let right = mid + 1; right < N; right++) {
-            if (rating[right] > rating[mid]) rightLarger++;
-        }
-
-        teams += leftSmaller * rightLarger;
-
-        let leftLarger = mid - leftSmaller;
-        let rightSmaller = N - mid - 1 - rightLarger;
-
-        teams += leftLarger * rightSmaller;
+    for (let i = 0; i < N; i++) {
+        if (s.charAt(i) === 'b') bCount++;
+        else minDeletions = Math.min(minDeletions + 1, bCount);
     }
 
-    console.log(teams);
+    console.log(minDeletions);
 };
 
-numTeams([2,5,3,4,1]);
-numTeams([2,1,3]);
-numTeams([1,2,3,4]);
+minimumDeletions("aababbab");
+minimumDeletions("bbaaaaabb");
