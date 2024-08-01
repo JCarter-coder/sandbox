734547d42963ac8e95b9927146fb6bc401fc3f55
Jesse_Carter <jessetcarter@hotmail.com>
2024-08-01 16:18:41 -0600

    E2678 solved

--- main.js
+++ main.js
@@ -1,30 +1,12 @@
-var minHeightShelves = function(books, shelfWidth) {
-    let N = books.length;
-    // dp[i] = min height of bookcase containing all books
-    // up to and excluding book i
-    const dp = new Array(N + 1);
-    // base cases
-    dp[0] = 0;
-    dp[1] = books[0][1];
-
-    for (let i = 2; i <= N; i++) {
-        // new shelf built to hold current books
-        let remainingShelfWidth = shelfWidth - books[i - 1][0];
-        let maxHeight = books[i - 1][1];
-        dp[i] = books[i - 1][1] + dp[i - 1];
-
-        let j = i - 1;
-        // calculate height when previous books
-        // are added onto a new shelf
-        while (j > 0 && remainingShelfWidth - books[j - 1][0] >= 0) {
-            maxHeight = Math.max(maxHeight, books[j - 1][1]);
-            remainingShelfWidth -= books[j - 1][0];
-            dp[i] = Math.min(dp[i], maxHeight + dp[j - 1]);
-            j--;
-        }
+var countSeniors = function(details) {
+    let result = 0;
+    for (let i = 0; i < details.length; i++) {
+        let age = details[i].slice(11,13);
+        if (parseInt(age) > 60) result++;
     }
-    console.log(dp[N]);
+    console.log(result);
 };
 
-minHeightShelves([[1,1],[2,3],[2,3],[1,1],[1,1],[1,1],[1,2]],4);
-minHeightShelves([[1,3],[2,4],[3,2]],6);
+countSeniors(["7868190130M7522","5303914400F9211","9273338290F4010"]);
+countSeniors(["1313579440F2036","2921522980M5644"]);
+countSeniors(["9751302862F0693","3888560693F7262","5485983835F0649","2580974299F6042","9976672161M6561","0234451011F8013","4294552179O6482"]);
