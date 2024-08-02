00ffeeb615c8ad864350e897b88e595acdda004e
Jesse_Carter <jessetcarter@hotmail.com>
2024-08-02 17:23:25 -0600

    M2134 solved

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
-var countSeniors = function(details) {
-    let result = 0;
-    for (let i = 0; i < details.length; i++) {
-        let age = details[i].slice(11,13);
-        if (parseInt(age) > 60) result++;
+var minSwaps = function(nums) {
+    // Initialize minimum swaps to a large value
+    let minimumSwaps = Number.MAX_SAFE_INTEGER;
+    let totalOnes = 0;
+    for (let num of nums) totalOnes += num;
+    // Initialize the count of 1s in the current window
+    let onesCount = nums[0];
+    let end = 0;
+
+    for (let start = 0; start < nums.length; start++) {
+        // Adjust onesCount by removing the element that
+        // is sliding out of the window
+        if (start !== 0) onesCount -= nums[start - 1];
+        // Expand the window to the right until
+        // it reaches the desired length
+        while (end - start + 1 < totalOnes) {
+            end++;
+            onesCount += nums[end % nums.length];
+        }
+        // Update the minimum number of swaps needed
+        minimumSwaps = Math.min(minimumSwaps, totalOnes - onesCount);
     }
-    console.log(result);
+
+    console.log(minimumSwaps);
 };
 
-countSeniors(["7868190130M7522","5303914400F9211","9273338290F4010"]);
-countSeniors(["1313579440F2036","2921522980M5644"]);
-countSeniors(["9751302862F0693","3888560693F7262","5485983835F0649","2580974299F6042","9976672161M6561","0234451011F8013","4294552179O6482"]);
+minSwaps([0,1,0,1,1,0,0]);
+minSwaps([0,1,1,1,0,0,1,1,0]);
+minSwaps([1,1,0,0,1]);
