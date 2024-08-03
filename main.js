a6ff3336458cef876083c35977e89abcc39f419c
Jesse_Carter <jessetcarter@hotmail.com>
2024-08-03 14:14:34 -0600

    E1460 solved

--- main.js
+++ main.js
@@ -1,29 +1,16 @@
-var minSwaps = function(nums) {
-    // Initialize minimum swaps to a large value
-    let minimumSwaps = Number.MAX_SAFE_INTEGER;
-    let totalOnes = 0;
-    for (let num of nums) totalOnes += num;
-    // Initialize the count of 1s in the current window
-    let onesCount = nums[0];
-    let end = 0;
-
-    for (let start = 0; start < nums.length; start++) {
-        // Adjust onesCount by removing the element that
-        // is sliding out of the window
-        if (start !== 0) onesCount -= nums[start - 1];
-        // Expand the window to the right until
-        // it reaches the desired length
-        while (end - start + 1 < totalOnes) {
-            end++;
-            onesCount += nums[end % nums.length];
+var canBeEqual = function(target, arr) {
+    let result = true;
+    target.sort((a,b) => a - b);
+    arr.sort((a,b) => a - b);
+    for (let i = 0; i < target.length; i++) {
+        if (target[i] !== arr[i]) {
+            result = false;
+            break;
         }
-        // Update the minimum number of swaps needed
-        minimumSwaps = Math.min(minimumSwaps, totalOnes - onesCount);
     }
-
-    console.log(minimumSwaps);
+    console.log(result);
 };
 
-minSwaps([0,1,0,1,1,0,0]);
-minSwaps([0,1,1,1,0,0,1,1,0]);
-minSwaps([1,1,0,0,1]);
+canBeEqual([1,2,3,4],[2,4,1,3]);
+canBeEqual([7],[7]);
+canBeEqual([3,7,9],[3,7,11]);
