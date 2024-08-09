a72534be5096a1d3d1145e8d4545e62f8cf65301
Jesse_Carter <jessetcarter@hotmail.com>
2024-08-09 08:47:14 -0600

    M840 solved

--- main.js
+++ main.js
@@ -1,27 +1,47 @@
-var spiralMatrixIII = function(rows, cols, rStart, cStart) {
-    const dir = [[0,1],[1,0],[0,-1],[-1,0]];
-    const traversed = new Array();
-    let step = 1;
-    let direction = 0;
+var numMagicSquaresInside = function(grid) {
+    let ans = 0;
+    let m = grid.length;
+    let n = grid[0].length;
 
-    while (traversed.length < rows * cols) {
-        for (let i = 0; i < 2; ++i) {
-            for (let j = 0; j < step; ++j) {
-                if (
-                    rStart >= 0 &&
-                    rStart < rows &&
-                    cStart >= 0 &&
-                    cStart < cols
-                ) traversed.push([rStart,cStart]);
-                rStart += dir[direction][0];
-                cStart += dir[direction][1];
+    let isMagicSquare = (grid, row, col) => {
+        const seen = new Array(9).fill(false);
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                let num = grid[row + i][col + j];
+                if (num < 1 || num > 9) return false;
+                if (seen[num]) return false;
+                seen[num] = true;
             }
-            direction = (direction + 1) % 4;
         }
-        ++step;
+
+        let diagonal1 = grid[row][col] + grid[row+1][col+1] + grid[row+2][col+2];
+        let diagonal2 = grid[row+2][col] + grid[row+1][col+1] + grid[row][col+2];
+
+        if (diagonal1 !== diagonal2) return false;
+
+        let row1 = grid[row][col] + grid[row][col+1] + grid[row][col+2];
+        let row2 = grid[row+1][col] + grid[row+1][col+1] + grid[row+1][col+2];
+        let row3 = grid[row+2][col] + grid[row+2][col+1] + grid[row+2][col+2];
+
+        if (!(row1 === diagonal1 && row2 === diagonal1 && row3 === diagonal1)) return false;
+
+        let col1 = grid[row][col] + grid[row+1][col] + grid[row+2][col];
+        let col2 = grid[row][col+1] + grid[row+1][col+1] + grid[row+2][col+1];
+        let col3 = grid[row][col+2] + grid[row+1][col+2] + grid[row+2][col+2];
+
+        if (!(col1 === diagonal1 && col2 === diagonal1 && col3 === diagonal1)) return false;
+
+        return true;
     }
-    console.log(traversed);
+
+    for (let row = 0; row + 2 < m; row++) {
+        for (let col = 0; col + 2 < n; col++) {
+            if (isMagicSquare(grid, row, col)) ans++;
+        }
+    }
+
+    console.log(ans);
 };
 
-spiralMatrixIII(1,4,0,0);
-spiralMatrixIII(5,6,1,4);
+numMagicSquaresInside([[4,3,8,4],[9,5,1,9],[2,7,6,2]]);
+numMagicSquaresInside([[8]]);
