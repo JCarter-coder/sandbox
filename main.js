ceb3bec4fbe8f0f622cdf402a52045df2ee199c1
Jesse_Carter <jessetcarter@hotmail.com>
2024-08-09 10:34:39 -0600

    M1456 solved

--- main.js
+++ main.js
@@ -1,47 +1,21 @@
-var numMagicSquaresInside = function(grid) {
-    let ans = 0;
-    let m = grid.length;
-    let n = grid[0].length;
+var maxVowels = function(s, k) {
+    const VOWELS = new Set(['a', 'e', 'i', 'o', 'u']);
+    let answer = 0;
+    let numVowels = 0;
+    let right = 0;
 
-    let isMagicSquare = (grid, row, col) => {
-        const seen = new Array(9).fill(false);
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                let num = grid[row + i][col + j];
-                if (num < 1 || num > 9) return false;
-                if (seen[num]) return false;
-                seen[num] = true;
-            }
+    for (let left = 0; left <= s.length - k; left++) {
+        while (right - left < k) {
+            numVowels += VOWELS.has(s[right++]) ? 1 : 0;
         }
-
-        let diagonal1 = grid[row][col] + grid[row+1][col+1] + grid[row+2][col+2];
-        let diagonal2 = grid[row+2][col] + grid[row+1][col+1] + grid[row][col+2];
-
-        if (diagonal1 !== diagonal2) return false;
-
-        let row1 = grid[row][col] + grid[row][col+1] + grid[row][col+2];
-        let row2 = grid[row+1][col] + grid[row+1][col+1] + grid[row+1][col+2];
-        let row3 = grid[row+2][col] + grid[row+2][col+1] + grid[row+2][col+2];
-
-        if (!(row1 === diagonal1 && row2 === diagonal1 && row3 === diagonal1)) return false;
-
-        let col1 = grid[row][col] + grid[row+1][col] + grid[row+2][col];
-        let col2 = grid[row][col+1] + grid[row+1][col+1] + grid[row+2][col+1];
-        let col3 = grid[row][col+2] + grid[row+1][col+2] + grid[row+2][col+2];
-
-        if (!(col1 === diagonal1 && col2 === diagonal1 && col3 === diagonal1)) return false;
-
-        return true;
+        answer = Math.max(answer, numVowels);
+        numVowels -= VOWELS.has(s[left]) ? 1 : 0;
     }
 
-    for (let row = 0; row + 2 < m; row++) {
-        for (let col = 0; col + 2 < n; col++) {
-            if (isMagicSquare(grid, row, col)) ans++;
-        }
-    }
-
-    console.log(ans);
+    console.log(answer);
 };
 
-numMagicSquaresInside([[4,3,8,4],[9,5,1,9],[2,7,6,2]]);
-numMagicSquaresInside([[8]]);
+maxVowels("abciiidef",3);
+maxVowels("aeiou",2);
+maxVowels("leetcode",3);
+maxVowels("weallloveyou",7) // 4
