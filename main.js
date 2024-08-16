e2d3f848b257b5d6ff56345b9a308590c7cafeb4
Jesse_Carter <jessetcarter@hotmail.com>
2024-08-16 10:44:33 -0600

    E1732 solved

--- main.js
+++ main.js
@@ -1,26 +1,20 @@
-var maxDistance = function(arrays) {
-    let answer = 0;
-    let N = arrays[0].length;
-    let min_val = arrays[0][0];
-    let max_val = arrays[0][arrays[0].length - 1];
-
-    for (let i = 1; i < arrays.length; i++) {
-        N = arrays[i].length;
-        answer = Math.max(
-            answer,
-            Math.max(
-                Math.abs(arrays[i][N - 1] - min_val),
-                Math.abs(max_val - arrays[i][0])
-            )
-        );
-        min_val = Math.min(min_val, arrays[i][0]);
-        max_val = Math.max(max_val, arrays[i][N - 1]);
+var largestAltitude = function(gain) {
+    const altitudes = new Array(gain + 1).fill(0);
+    
+    for (let i = 0; i < gain.length; i++) {
+        altitudes[i + 1] = altitudes[i] + gain[i];
     }
 
-    console.log(answer);
+    let highestAltitude = Number.MIN_SAFE_INTEGER;
+
+    for (let i = 0; i < altitudes.length; i++) {
+        highestAltitude = Math.max(
+            highestAltitude,
+            altitudes[i]
+        );
+    }
+    console.log(highestAltitude);
 };
 
-maxDistance([[1,2,3],[4,5],[1,2,3]]);
-maxDistance([[1],[1]]);
-maxDistance([[-2],[-3,-2,1]]);
-maxDistance([[1,5],[3,4]]);
+largestAltitude([-5,1,5,0,-7]);
+largestAltitude([-4,-3,-2,-1,4,3,2]);
