62f82ccd8be2384af2fca962b51d3d17b1498d2e
Jesse_Carter <jessetcarter@hotmail.com>
2024-08-15 20:50:46 -0600

    M624 solved

--- main.js
+++ main.js
@@ -1,35 +1,26 @@
-var lemonadeChange = function(bills) {
-    let fives = 0;
-    let tens = 0;
-    let twenties = 0;
-    let result = true;
+var maxDistance = function(arrays) {
+    let answer = 0;
+    let N = arrays[0].length;
+    let min_val = arrays[0][0];
+    let max_val = arrays[0][arrays[0].length - 1];
 
-    for (let i = 0; i < bills.length; i++) {
-        if (bills[i] === 5) fives++;
-        else if (bills[i] === 10) {
-            if (fives > 0) {
-                tens++;
-                fives--;
-            } else {
-                result = false;
-                break;
-            }
-        } else if (bills[i] === 20) {
-            if (tens > 0 && fives > 0) {
-                twenties++;
-                tens--;
-                fives--;
-            } else if (fives >= 3) {
-                twenties++;
-                fives -= 3;
-            } else {
-                result = false;
-                break;
-            }
-        }
+    for (let i = 1; i < arrays.length; i++) {
+        N = arrays[i].length;
+        answer = Math.max(
+            answer,
+            Math.max(
+                Math.abs(arrays[i][N - 1] - min_val),
+                Math.abs(max_val - arrays[i][0])
+            )
+        );
+        min_val = Math.min(min_val, arrays[i][0]);
+        max_val = Math.max(max_val, arrays[i][N - 1]);
     }
-    console.log(result);
+
+    console.log(answer);
 };
 
-lemonadeChange([5,5,5,10,20]);
-lemonadeChange([5,5,10,10,20]);
+maxDistance([[1,2,3],[4,5],[1,2,3]]);
+maxDistance([[1],[1]]);
+maxDistance([[-2],[-3,-2,1]]);
+maxDistance([[1,5],[3,4]]);
