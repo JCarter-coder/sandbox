95a75d5b55e0a283a8c414026a925721d28d38a2
Jesse_Carter <jessetcarter@hotmail.com>
2024-08-18 13:10:26 -0600

    M264 solved

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
-var uniqueOccurrences = function(arr) {
-    const freq = new Map();
-    for (let num of arr) {
-        freq.set(num, freq.get(num) + 1 || 1);
+var nthUglyNumber = function(n) {
+    const uglyNumbers = [1];
+    
+    // Pointers
+    let indexMultiple2 = 0;
+    let indexMultiple3 = 0;
+    let indexMultiple5 = 0;
+    let nextMultiple2 = 2;
+    let nextMultiple3 = 3;
+    let nextMultiple5 = 5;
+
+    for (let i = 1; i < n; i++) {
+        let nextUglyNumber = Math.min(
+            nextMultiple2,
+            Math.min(nextMultiple3, nextMultiple5)
+        );
+        uglyNumbers.push(nextUglyNumber);
+
+        if (nextUglyNumber === nextMultiple2) {
+            indexMultiple2++;
+            nextMultiple2 = uglyNumbers[indexMultiple2] * 2;
+        }
+
+        if (nextUglyNumber === nextMultiple3) {
+            indexMultiple3++;
+            nextMultiple3 = uglyNumbers[indexMultiple3] * 3;
+        }
+
+        if (nextUglyNumber === nextMultiple5) {
+            indexMultiple5++;
+            nextMultiple5 = uglyNumbers[indexMultiple5] * 5;
+        }
     }
 
-    const freqSet = new Set(freq.values())
-
-    // If set size equals map size, then
-    // frequency counts are unique
-    console.log(freq.size == freqSet.size);
+    console.log(uglyNumbers[n - 1]);
 };
 
-uniqueOccurrences([1,2,2,1,1,3]);
-uniqueOccurrences([1,2]);
-uniqueOccurrences([-3,0,1,-3,1,1,1,-3,10,0]);
+nthUglyNumber(10);
+nthUglyNumber(1);
