6729d5aec79155ec2467380b18642b400b11a3a3
Jesse_Carter <jessetcarter@hotmail.com>
2024-08-19 16:44:08 -0600

    M650 solved

--- main.js
+++ main.js
@@ -1,39 +1,18 @@
-var nthUglyNumber = function(n) {
-    const uglyNumbers = [1];
+var minSteps = function(n) {
+    // operations 'copy' or 'paste'
+    let operations = 0;
+    let d = 2;
     
-    // Pointers
-    let indexMultiple2 = 0;
-    let indexMultiple3 = 0;
-    let indexMultiple5 = 0;
-    let nextMultiple2 = 2;
-    let nextMultiple3 = 3;
-    let nextMultiple5 = 5;
-
-    for (let i = 1; i < n; i++) {
-        let nextUglyNumber = Math.min(
-            nextMultiple2,
-            Math.min(nextMultiple3, nextMultiple5)
-        );
-        uglyNumbers.push(nextUglyNumber);
-
-        if (nextUglyNumber === nextMultiple2) {
-            indexMultiple2++;
-            nextMultiple2 = uglyNumbers[indexMultiple2] * 2;
+    while (n > 1) {
+        while (n % d === 0) {
+            operations += d;
+            n /= d;
         }
-
-        if (nextUglyNumber === nextMultiple3) {
-            indexMultiple3++;
-            nextMultiple3 = uglyNumbers[indexMultiple3] * 3;
-        }
-
-        if (nextUglyNumber === nextMultiple5) {
-            indexMultiple5++;
-            nextMultiple5 = uglyNumbers[indexMultiple5] * 5;
-        }
+        d++;
     }
 
-    console.log(uglyNumbers[n - 1]);
+    console.log(operations);
 };
 
-nthUglyNumber(10);
-nthUglyNumber(1);
+minSteps(3); // 3
+minSteps(1); // 0
