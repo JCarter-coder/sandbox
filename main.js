f161633d111b6794bf4f91eb6868c5bf1aa8a68c
Jesse_Carter <jessetcarter@hotmail.com>
2024-08-21 15:19:48 -0600

    M1493 solved

--- main.js
+++ main.js
@@ -1,59 +1,21 @@
-var strangePrinter = function(s) {
-    let removeDuplicates = (s) => {
-        let uniqueChars = [];
-        let i = 0;
-        while (i < s.length) {
-            let currentChar = s.charAt(i);
-            uniqueChars.push(currentChar);
-            while (i < s.length && s.charAt(i) === currentChar) {
-                i++;
-            }
+var longestSubarray = function(nums) {
+    let zeros = 0;
+    let longestWindow = 0;
+    // Use a sliding window
+    let start = 0;
+    for (let i = 0; i < nums.length; i++) {
+        zeros += (nums[i] === 0 ? 1 : 0);
+
+        while (zeros > 1) {
+            zeros -= (nums[start] === 0 ? 1 : 0);
+            start++;
         }
-        return uniqueChars.join('');
+        longestWindow = Math.max(longestWindow, i - start + 1);
     }
 
-    // Remove consecutive duplicate letters
-    s = removeDuplicates(s);
-
-    let N = s.length;
-
-    // dp[i][j] represents the minimum number
-    // of turns to print s[i] to s[j]
-    let minTurns = [];
-    for (let i = 0; i < N; i++) {
-        minTurns.push(new Array(N).fill(0));
-        minTurns[i][i] = 1;
-    }
-
-    // Fill the dp table
-    for (let len = 2; len <= N; len++) {
-        for (let start = 0; start + len - 1 < N; start++) {
-            let end = start + len - 1;
-
-            // Initialize with worst case: print each character separately
-            minTurns[start][end] = len;
-
-            // Try all possible splits and find the minimum
-            for (let split = 0; split < len - 1; split++) {
-                let totalTurns = minTurns[start][start + split] +
-                    minTurns[start + split + 1][end];
-                
-                if (s.charAt(start + split) === s.charAt(end)) {
-                    totalTurns--;
-                }
-
-                minTurns[start][end] = Math.min(
-                    minTurns[start][end],
-                    totalTurns
-                );
-            }
-        }
-    }
-
-    // Return the minimum turns needed to print the entire string
-
-    console.log(minTurns[0][[N - 1]]);
+    console.log(longestWindow);
 };
 
-strangePrinter("aaabbb");
-strangePrinter("aba");
+longestSubarray([1,1,0,1]); // 4
+longestSubarray([0,1,1,1,0,1,1,0,1]); // 6
+longestSubarray([1,1,1]); // 3
