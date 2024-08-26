9c682756ee58beb2360fb5db367a6e2687d42a22
Jesse_Carter <jessetcarter@hotmail.com>
2024-08-26 06:27:59 -0600

    E590 solved

--- main.js
+++ main.js
@@ -12,6 +12,24 @@ var postorderTraversal = function(root) {
     return result;
 };
 
-postorderTraversal([1,null,2,3]);
-postorderTraversal([]);
-postorderTraversal([1]);
+var postorder = function(root) {
+    const result = [];
+
+    const traverse = (root) => {
+        if (root) {
+            if (root.children) {
+                for (let node of root.children) {
+                    traverse(node);
+                }
+            }
+            result.push(root.val);
+        }
+    };
+    
+    traverse(root);
+    return result;
+};
+
+postorder([1,null,3,2,4,null,5,6]);
+postorder([1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]);
+
