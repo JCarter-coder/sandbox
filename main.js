5e9351f022a24b8a1357ef460e4993c4ec802434
Jesse_Carter <jessetcarter@hotmail.com>
2024-08-26 20:02:45 -0600

    M320 solved

--- main.js
+++ main.js
@@ -1,35 +1,29 @@
-var postorderTraversal = function(root) {
-    const result = [];
-    const traverse = (node) => {
-        if (node === null) return;
-
-        traverse(node.left);
-        traverse(node.right);
-        result.push(node.val);
-    };
-
-    traverse(root);
-    return result;
-};
-
-var postorder = function(root) {
-    const result = [];
-
-    const traverse = (root) => {
-        if (root) {
-            if (root.children) {
-                for (let node of root.children) {
-                    traverse(node);
-                }
-            }
-            result.push(root.val);
-        }
-    };
+var generateAbbreviations = function(word) {
+    let N = word.length;
+    const abbreviations = [];
     
-    traverse(root);
-    return result;
+    for (let mask = 0; mask < (1 << N); mask++) {
+        let currWord = "";
+        let abbreviatedCount = 0;
+
+        for (let index = 0; index < N; index++) {
+            if ((mask & (1 << index)) != 0) abbreviatedCount++;
+            else {
+                if (abbreviatedCount > 0) {
+                    currWord += abbreviatedCount;
+                    abbreviatedCount = 0;
+                }
+                currWord += word.charAt(index);
+            }
+        }
+
+        if (abbreviatedCount > 0) currWord += abbreviatedCount;
+
+        abbreviations.push(currWord);
+    }
+
+    console.log(abbreviations);
 };
 
-postorder([1,null,3,2,4,null,5,6]);
-postorder([1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]);
-
+generateAbbreviations("word");
+generateAbbreviations("a");
