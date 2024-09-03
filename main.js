192290f88b3b17c162975f5e95ec7206b29db3b1
Jesse_Carter <jessetcarter@hotmail.com>
2024-09-03 14:15:25 -0600

    M1634 solved

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
-var getLucky = function(s, k) {
-    let result = "";
-    for (let i = 0; i < s.length; i++) {
-        let charValue = (s.charCodeAt(i) - 96).toString();
-        result += charValue;
+class PolyNode {
+    constructor(x=0, y=0, next=null) {
+        this.coefficient = x;
+        this.power = y;
+        this.next = next;
+    }
+}
+
+/** 
+ * @param {PolyNode} poly1 
+ * @param {PolyNode} poly2 
+ * @return {PolyNode}
+ */
+
+var addPoly = function(poly1, poly2) {
+    const root = new PolyNode(1, Infinity);
+    let tail = root;
+
+    while (poly1 != null && poly2 != null) {
+        if (poly1.power > poly2.power) {
+            tail.next = poly1;
+            tail = tail.next;
+            poly1 = poly1.next;
+        } else if (poly1.power < poly2.power) {
+            tail.next = poly2;
+            tail = tail.next;
+            poly2 = poly2.next;
+        } else {
+            poly1.coefficient += poly2.coefficient;
+            tail.next = poly1;
+            tail = (poly1.coefficient == 0) ? tail : tail.next;
+            poly1 = poly1.next;
+            poly2 = poly2.next;
+        }
     }
 
-    while (k > 0) {
-        let tempResult = 0;
-        result = result.toString();
-        for (let i = 0; i < result.length; i++) {
-            let charValue = result.charCodeAt(i) - 48;
-            tempResult += charValue;
-        }
-        result = tempResult;
-        k--;
-    }
-    
-    console.log(result);
+    tail.next = poly1 ?? poly2;
+    console.log(...root.next);
 };
 
-getLucky("iiii",1);
-getLucky("leetcode",2);
-getLucky("zbax",2);
-getLucky("hvmhoasabaymnmsd",1);
+addPoly([[1,1]],[[1,0]]);
+addPoly([[2,2],[4,1],[3,0]],[[3,2],[-4,1],[-1,0]]);
+addPoly([[1,2]],[[-1,2]]);
