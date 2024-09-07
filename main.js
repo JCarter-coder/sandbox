f5842af2bc5120288190a327ebb13fd2e8862b58
Jesse_Carter <jessetcarter@hotmail.com>
2024-09-07 14:59:14 -0600

    M1367 solved

--- main.js
+++ main.js
@@ -1,36 +1,41 @@
-class ListNode {
-    constructor(val, next) {
-        this.val = (val===undefined ? 0 : val)
-        this.next = (next===undefined ? null : next)
-    }
-}
-
 /**
- * @param {number[]} nums
- * @param {ListNode} head
- * @return {ListNode}
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
  */
-var modifiedList = function(nums, head) {
-    nums = new Set(nums);
-
-    let dummy = new ListNode(-1, head);
-    let probe = dummy;
-
-    console.log(dummy);
-
-    while (probe.next) {
-        if (nums.has(probe.next.val)) {
-            // Skip the next node by updating the pointer
-            probe.next = probe.next.next;
-        } else {
-            // Move to the next node
-            probe = probe.next;
-        }
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSubPath = function(head, root) {
+    let checkPath = (node, head) => {
+        if (node == null) return false;
+        if (dfs(node, head)) return true;
+        return checkPath(node.left, head) || checkPath(node.right, head);
     }
 
-    console.log(dummy.next);
+    let dfs = (node, head) => {
+        if (head == null) return true;
+        if (node == null) return false;
+        if (node.val != head.val) return false;
+        return dfs(node.left, head.next) || dfs(node.right, head.next);
+    }
+    
+    if (root == null) return false;
+    return checkPath(root, head);
 };
 
-modifiedList([1,2,3],[1,2,3,4,5]);
-modifiedList([1],[1,2,1,2,1,2]);
-modifiedList([5],[1,2,3,4]);
+isSubPath([4,2,8],[1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]);
+isSubPath([1,4,2,6],[1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]);
+isSubPath([1,4,2,6,8],[1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]);
