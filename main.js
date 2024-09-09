1af803c1e77241559f49729c53fd29e3581e4e54
Jesse_Carter <jessetcarter@hotmail.com>
2024-09-09 16:26:14 -0600

    M2326 solved

--- main.js
+++ main.js
@@ -1,38 +1,46 @@
 /**
- * @param {ListNode} head
- * @param {number} k
- * @return {ListNode[]}
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
  */
-var splitListToParts = function(head, k) {
-    const answer = new Array(k).fill(null);
-    let size = 0;
-    let current = head;
-    while (current) {
-        size++;
-        current = current.next;
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {ListNode} head
+ * @return {number[][]}
+ */
+var spiralMatrix = function(m, n, head) {
+    const DIRECTIONS = [[0,1],[1,0],[0,-1],[-1,0]];
+    const result = [];
+    for (let row = 0; row < m; row++) {
+        result.push(new Array(n).fill(-1));
     }
 
-    let splitSize = Math.floor(size / k);
-    let remainder = size % k;
+    let i = 0;
+    let j = 0;
+    let currentDirection = 0;
 
-    current = head;
-    let prev = null;
-    for (let i = 0; i < k; i++) {
-        // Create i-th part
-        answer[i] = current;
-        let currentSize = splitSize + (remainder > 0 ? 1 : 0);
-        remainder--;
+    while (head != null) {
+        result[i][j] = head.val;
+        let newi = i + DIRECTIONS[currentDirection][0];
+        let newj = j + DIRECTIONS[currentDirection][1];
 
-        for (let j = 0; j < currentSize; j++) {
-            prev = current;
-            current = current.next;
-        }
+        if (
+            Math.min(newi, newj) < 0 ||
+            newi >= m ||
+            newj >= n ||
+            result[newi][newj] != -1
+        ) currentDirection = (currentDirection + 1) % 4;
+        i += DIRECTIONS[currentDirection][0];
+        j += DIRECTIONS[currentDirection][1];
 
-        if (prev) prev.next = null;
+        head = head.next;
     }
-
-    console.log(answer);
+    
+    console.log(result);
 };
 
-splitListToParts([1,2,3],5);
-splitListToParts([1,2,3,4,5,6,7,8,9,10],3);
+spiralMatrix(3,5,[3,0,2,6,8,1,7,9,4,2,5,5,0]);
+spiralMatrix(1,4,[0,1,2]);
