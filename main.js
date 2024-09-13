8fae5f89f4ce2ef99962e0037651a6e788c60e01
Jesse_Carter <jessetcarter@hotmail.com>
2024-09-12 18:00:14 -0600

    M1229 solved

--- main.js
+++ main.js
@@ -1,39 +1,36 @@
 /**
- * @param {string} allowed
- * @param {string[]} words
- * @return {number}
+ * @param {number[][]} slots1
+ * @param {number[][]} slots2
+ * @param {number} duration
+ * @return {number[]}
  */
-var countConsistentStrings = function(allowed, words) {
-    let consistentCount = 0;
+var minAvailableDuration = function(slots1, slots2, duration) {
+    slots1.sort((a,b) => a[0] - b[0]);
+    slots2.sort((a,b) => a[0] - b[0]);
+    
+    let pointer1 = 0;
+    let pointer2 = 0;
 
-    for (let word of words) {
-        let isWordConsistent = true;
-
-        for (let i = 0; i < word.length; i++) {
-            let currentChar = word.charAt(i);
-            let isCharAllowed = false;
-
-            for (let j = 0; j < allowed.length; j++) {
-                if (allowed.charAt(j) === currentChar) {
-                    isCharAllowed = true;
-                    break;
-                }
-            }
-
-            if (!isCharAllowed) {
-                isWordConsistent = false;
-                break;
-            }
+    while (pointer1 < slots1.length && pointer2 < slots2.length) {
+        let intersectLeft = Math.max(
+            slots1[pointer1][0],
+            slots2[pointer2][0]
+        )
+        let intersectRight = Math.min(
+            slots1[pointer1][1],
+            slots2[pointer2][1]
+        )
+        if (intersectRight - intersectLeft >= duration) {
+            console.log([intersectLeft, intersectLeft + duration]);
+            return [intersectLeft, intersectLeft + duration];
         }
-
-        if (isWordConsistent) {
-            consistentCount++;
-        }
+        if (slots1[pointer1][1] < slots2[pointer2][1]) {
+            pointer1++;
+        } else pointer2++;
     }
-
-    console.log(consistentCount);
+    console.log([]);
+    return [];
 };
 
-countConsistentStrings("ab",["ad","bd","aaab","baa","badab"]);
-countConsistentStrings("abc",["a","b","c","ab","ac","bc","abc"]);
-countConsistentStrings("cad",["cc","acd","b","ba","bac","bad","ac","d"]);
+minAvailableDuration([[10,50],[60,120],[140,210]],[[0,15],[60,70]],8);
+minAvailableDuration([[10,50],[60,120],[140,210]],[[0,15],[60,70]],12);
