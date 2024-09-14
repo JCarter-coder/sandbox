28edeedae1c0effb12ebf64912615cb8688b2fc6
Jesse_Carter <jessetcarter@hotmail.com>
2024-09-14 15:56:53 -0600

    M2419 solved

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-/**
- * @param {number[]} arr
- * @param {number[][]} queries
- * @return {number[]}
- */
-var xorQueries = function(arr, queries) {
-    const result = [];
-    // Convert arr into an in-place prefix XOR array
-    for (let i = 1; i < arr.length; i++) {
-        arr[i] ^= arr[i - 1];
-    }
-    // Resolve each query using the prefix XOR array
-    for (let q of queries) {
-        if (q[0] > 0) result.push(arr[q[0] - 1] ^ arr[q[1]]);
-        else result.push(arr[q[1]]);
+var longestSubarray = function(nums) {
+    let maxVal = 0;
+    let ans = 0;
+    let currentStreak = 0;
+
+    for (let num of nums) {
+        if (maxVal < num) {
+            maxVal = num;
+            ans = currentStreak = 0;
+        }
+
+        if (maxVal === num) currentStreak++;
+        else currentStreak = 0;
+
+        ans = Math.max(ans, currentStreak);
     }
 
-    console.log(result);
+    console.log(ans);
 };
 
-xorQueries([1,3,4,8],[[0,1],[1,2],[0,3],[3,3]]);
-xorQueries([4,8,2,10],[[2,3],[1,3],[0,0],[0,3]]);
+longestSubarray([1,2,3,3,2,2]);
+longestSubarray([1,2,3,4]);
