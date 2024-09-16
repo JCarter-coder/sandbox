d4b68708a40ce8675ea231309fa7e6535bb9227a
Jesse_Carter <jessetcarter@hotmail.com>
2024-09-16 17:32:20 -0600

    M539 solved

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 /**
- * @param {string} s
+ * @param {string[]} timePoints
  * @return {number}
  */
-var findTheLongestSubstring = function(s) {
-    let prefixXOR = 0;
-    const characterMap = new Array(26).fill(0);
-    // Set a, e, i, o, u (i.e. 0, 4, 8, 14, 20)
-    characterMap[0] = 1;
-    characterMap[4] = 2;
-    characterMap[8] = 4;
-    characterMap[14] = 8;
-    characterMap[20] = 16;
-    const mp = new Array(32).fill(-1);
-    let longestSubstring = 0;
-    for (let i = 0; i < s.length; i++) {
-        prefixXOR ^= characterMap[s.charCodeAt(i) - 97];
-        if (mp[prefixXOR] === -1 && prefixXOR !== 0) mp[prefixXOR] = i;
-        longestSubstring = Math.max(longestSubstring, i - mp[prefixXOR]);
+var findMinDifference = function(timePoints) {
+    // Convert to minutes and store in list
+    const vec = timePoints.map(timePoint => {
+        const [h, m] = timePoint.split(':').map(Number);
+        return h * 60 + m;
+    });
+
+    // Sort the time points
+    vec.sort((a,b) => a - b);
+
+    // Calculate the minimum difference
+    let res = Infinity;
+    for (let i = 1; i < vec.length; i++) {
+        res = Math.min(vec[i] - vec[i - 1], res);
     }
-    console.log(longestSubstring);
+
+    // Handle the circular case
+    console.log(Math.min(res, 1440 + vec[0] - vec[vec.length - 1]));
+    return Math.min(res, 1440 + vec[0] - vec[vec.length - 1]);
 };
 
-findTheLongestSubstring("eleetminicoworoep");
-findTheLongestSubstring("leetcodeisgreat");
-findTheLongestSubstring("bcbcbc");
+findMinDifference(["23:59","00:00"]);
+findMinDifference(["00:00","23:59","00:00"]);
