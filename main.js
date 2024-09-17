4edbd9335de26652a2a9f071c388b11a6108a60c
Jesse_Carter <jessetcarter@hotmail.com>
2024-09-17 14:58:00 -0600

    E884 solved

--- main.js
+++ main.js
@@ -1,27 +1,39 @@
 /**
- * @param {string[]} timePoints
- * @return {number}
+ * @param {string} s1
+ * @param {string} s2
+ * @return {string[]}
  */
-var findMinDifference = function(timePoints) {
-    // Convert to minutes and store in list
-    const vec = timePoints.map(timePoint => {
-        const [h, m] = timePoint.split(':').map(Number);
-        return h * 60 + m;
-    });
-
-    // Sort the time points
-    vec.sort((a,b) => a - b);
-
-    // Calculate the minimum difference
-    let res = Infinity;
-    for (let i = 1; i < vec.length; i++) {
-        res = Math.min(vec[i] - vec[i - 1], res);
+var uncommonFromSentences = function(s1, s2) {
+    s1 = s1.split(' ');
+    s2 = s2.split(' ');
+    let index = 0;
+    const result = {};
+    
+    // swap list designations if s2 is longer than s1
+    if (s2.length > s1.length) {
+        let temp = s2;
+        s2 = s1;
+        s1 = temp;
     }
 
-    // Handle the circular case
-    console.log(Math.min(res, 1440 + vec[0] - vec[vec.length - 1]));
-    return Math.min(res, 1440 + vec[0] - vec[vec.length - 1]);
+    while (index < s1.length) {
+        if (!result[s1[index]]) {
+            result[s1[index]] = 1;
+        } else result[s1[index]]++;
+        // check if s2[index] is not undefined and proceed
+        if (s2[index] && !result[s2[index]]) {
+            result[s2[index]] = 1;
+        } else if (s2[index]) result[s2[index]]++;
+        index++;
+    }
+
+    console.log(Object.entries(result)
+        .filter(([key, value]) => value === 1)
+        .map(([key]) => key)
+    );
 };
 
-findMinDifference(["23:59","00:00"]);
-findMinDifference(["00:00","23:59","00:00"]);
+uncommonFromSentences("this apple is sweet","this apple is sour");
+uncommonFromSentences("apple apple","banana");
+uncommonFromSentences("s z z z s","s z ejt");
+
