07bb83571ce6716a0f38913578792e95a05c5888
Jesse_Carter <jessetcarter@hotmail.com>
2024-09-20 15:36:57 -0600

    E2220 solved

--- main.js
+++ main.js
@@ -1,38 +1,19 @@
 /**
- * @param {string} s
- * @return {string}
+ * @param {number} start
+ * @param {number} goal
+ * @return {number}
  */
-var shortestPalindrome = function(s) {
-    let hashBase = 29;
-    let modValue = 1e9 + 7;
-    let forwardHash = 0;
-    let reverseHash = 0;
-    let powerValue = 1;
-    let palindromeEndIndex = -1;
+var minBitFlips = function(start, goal) {
+    let xorResult = start ^ goal;
+    let count = 0;
 
-    for (let i = 0; i < s.length; i++) {
-        let currentChar = s.charCodeAt(i);
-        
-        // Update forward hash
-        forwardHash = (forwardHash * hashBase + (currentChar - 97 + 1)) % modValue;
-
-        // Update reverse hash
-        reverseHash = (reverseHash + (currentChar - 97 + 1) * powerValue) % modValue;
-        powerValue = (powerValue * hashBase) % modValue;
-
-        // If forward and reverse hashes match, update end index
-        if (forwardHash === reverseHash) palindromeEndIndex = i;
+    while (xorResult != 0) {
+        xorResult &= (xorResult - 1);
+        count++;
     }
 
-    let suffix = s.substring(palindromeEndIndex + 1);
-    let reversedSuffix = "";
-
-    for (let i = suffix.length - 1; i >= 0; i--) {
-        reversedSuffix += suffix.charAt(i);
-    }
-
-    console.log(reversedSuffix.concat(s));
+    console.log(count);
 };
 
-shortestPalindrome("aacecaaa");
-shortestPalindrome("abcd");
+minBitFlips(10,7);
+minBitFlips(3,4);
