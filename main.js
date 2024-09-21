f891299a3c1144230ab06bfbefd3c02487dcb004
Jesse_Carter <jessetcarter@hotmail.com>
2024-09-21 14:43:32 -0600

    M386 solved

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 /**
- * @param {number} start
- * @param {number} goal
- * @return {number}
+ * @param {number} n
+ * @return {number[]}
  */
-var minBitFlips = function(start, goal) {
-    let xorResult = start ^ goal;
-    let count = 0;
+var lexicalOrder = function(n) {
+    const result = [];
+    let currentNumber = 1;
+    
+    for (let i = 0; i < n; i++) {
+        result.push(currentNumber);
 
-    while (xorResult != 0) {
-        xorResult &= (xorResult - 1);
-        count++;
+        // if multiplying the current number
+        // by 10 is within the limit, do it
+        if (currentNumber * 10 <= n) currentNumber *= 10;
+        else {
+            // adjust the current number by moving up one digit
+            while (currentNumber % 10 === 9 || currentNumber >= n) {
+                // remove the last digit
+                currentNumber = Math.floor(currentNumber / 10);
+            }
+            currentNumber++;
+        }
     }
-
-    console.log(count);
+    console.log(result);
 };
 
-minBitFlips(10,7);
-minBitFlips(3,4);
+lexicalOrder(13);
+lexicalOrder(2);
