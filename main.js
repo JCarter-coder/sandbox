e3f30af410f1718655cebefa6eab9f9548a5f19e
Jesse_Carter <jessetcarter@hotmail.com>
2024-09-26 14:19:03 -0600

    M1858 solved

--- main.js
+++ main.js
@@ -1,29 +1,65 @@
-
-var MyCalendar = function() {
-    this.bookings = [];
-};
-
-/** 
- * @param {number} start 
- * @param {number} end
- * @return {boolean}
+/**
+ * @param {string[]} words
+ * @return {string}
  */
-MyCalendar.prototype.book = function(start, end) {
-    for (let event of this.bookings) {
-        let [existingStart, existingEnd] = event;
-        if (start < existingEnd && end > existingStart) {
-            return false;
-        }
+
+class Trie {
+    constructor() {
+        this.root = {};
     }
-    this.bookings.push([start, end]);
-    return true;
+
+    insert(word) {
+        let node = this.root;
+        for (let x of word) {
+            if (node[x] == null) node[x] = {};
+            node = node[x];
+        }
+        node.isWord = true;
+    }
+
+    traverse(word) {
+        let node = this.root;
+        for (let x of word) {
+            if (node[x] == null) return null;
+            node = node[x];
+        }
+        return node;
+    }
+
+    search(word) {
+        let node = this.traverse(word);
+        return node != null && node.isWord === true;
+    }
+
+    startsWith(prefix) {
+        let node = this.traverse(prefix);
+        return node != null;
+    }
+}
+
+var longestWord = function(words) {
+    let dfs = (node, word) => {
+        let maxWord = word;
+        for (let x in node) {
+            if (node[x].isWord) {
+                let res = dfs(node[x], word + x);
+                if (res.length > maxWord.length ||
+                    res.length === maxWord.length &&
+                    res < maxWord
+                ) maxWord = res;
+            }
+        }
+        return maxWord;
+    }
+
+    let t = new Trie();
+    for (let word of words) {
+        t.insert(word);
+    }
+
+    console.log(dfs(t.root, ""));
 };
 
-/** 
- * Your MyCalendar object will be instantiated and called as such:
- * var obj = new MyCalendar()
- * var param_1 = obj.book(start,end)
- */
-
-["MyCalendar","book","book","book"]
-[[],[10,20],[15,25],[20,30]]
+longestWord(["k","ki","kir","kira","kiran"]);
+longestWord(["a","banana","app","appl","ap","apply","apple"]);
+longestWord(["abc","bc","ab","qwe"]);
