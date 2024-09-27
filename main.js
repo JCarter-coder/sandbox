824ce242ebbda20e0362da63958b45a04afb204e
Jesse_Carter <jessetcarter@hotmail.com>
2024-09-26 21:56:58 -0600

    M731 solved

--- main.js
+++ main.js
@@ -1,65 +1,37 @@
-/**
- * @param {string[]} words
- * @return {string}
- */
-
-class Trie {
-    constructor() {
-        this.root = {};
-    }
-
-    insert(word) {
-        let node = this.root;
-        for (let x of word) {
-            if (node[x] == null) node[x] = {};
-            node = node[x];
-        }
-        node.isWord = true;
-    }
-
-    traverse(word) {
-        let node = this.root;
-        for (let x of word) {
-            if (node[x] == null) return null;
-            node = node[x];
-        }
-        return node;
-    }
-
-    search(word) {
-        let node = this.traverse(word);
-        return node != null && node.isWord === true;
-    }
-
-    startsWith(prefix) {
-        let node = this.traverse(prefix);
-        return node != null;
-    }
-}
-
-var longestWord = function(words) {
-    let dfs = (node, word) => {
-        let maxWord = word;
-        for (let x in node) {
-            if (node[x].isWord) {
-                let res = dfs(node[x], word + x);
-                if (res.length > maxWord.length ||
-                    res.length === maxWord.length &&
-                    res < maxWord
-                ) maxWord = res;
-            }
-        }
-        return maxWord;
-    }
-
-    let t = new Trie();
-    for (let word of words) {
-        t.insert(word);
-    }
-
-    console.log(dfs(t.root, ""));
+var MyCalendarTwo = function() {
+    this.bookings = [];
+    this.overlaps = [];
 };
 
-longestWord(["k","ki","kir","kira","kiran"]);
-longestWord(["a","banana","app","appl","ap","apply","apple"]);
-longestWord(["abc","bc","ab","qwe"]);
+/** 
+ * @param {number} start 
+ * @param {number} end
+ * @return {boolean}
+ */
+MyCalendarTwo.prototype.book = function(start, end) {
+    for (let event of this.overlaps) {
+        let [existingStart, existingEnd] = event;
+        if (start < existingEnd && end > existingStart) {
+            return false;
+        }
+    }
+    for (let event of this.bookings) {
+        let [existingStart, existingEnd] = event;
+        if (start < existingEnd && end > existingStart) {
+            this.overlaps.push(
+                [
+                    Math.max(existingStart, start),
+                    Math.min(existingEnd, end)
+                ]
+            );
+        }
+    }
+    this.bookings.push([start, end]);
+    return true;
+};
+
+/** 
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * var obj = new MyCalendarTwo()
+ * var param_1 = obj.book(start,end)
+ */
