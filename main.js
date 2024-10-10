406406628620f3cc9f6e58d62d14b491a0cfc615
Jesse_Carter <jessetcarter@hotmail.com>
2024-10-10 10:07:09 -0600

    M962 solved

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
 /**
- * @param {string} s
+ * @param {number[]} nums
  * @return {number}
  */
-var minAddToMakeValid = function(s) {
-    let openBrackets = 0;
-    let minAddsRequired = 0;
-
-    for (let char of s.split('')) {
-        if (char === '(') openBrackets++;
-        else {
-            if (openBrackets > 0) openBrackets--;
-            else minAddsRequired++;
-        }
+var maxWidthRamp = function(nums) {
+    let N = nums.length;
+    let rightMax = new Array(N);
+    
+    rightMax[N - 1] = nums[N - 1];
+    for (let i = N - 2; i >= 0; i--) {
+        rightMax[i] = Math.max(rightMax[i + 1], nums[i]);
     }
 
-    console.log(minAddsRequired + openBrackets);
+    let left = 0, right = 0;
+    let maxWidth = 0;
+
+    while (right < N) {
+        while (left < right && nums[left] > rightMax[right]) left ++;
+
+        maxWidth = Math.max(maxWidth, right - left);
+        right++;
+    }
+    console.log(maxWidth);
 };
 
-minAddToMakeValid("())");
-minAddToMakeValid("(((");
+maxWidthRamp([6,0,8,2,1,5]);
+maxWidthRamp([9,8,1,0,1,9,4,0,4,1]);
