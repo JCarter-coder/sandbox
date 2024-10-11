91bbe2499f09b5113332001f44d48f9adb753dcc
Jesse_Carter <jessetcarter@hotmail.com>
2024-10-11 17:08:09 -0600

    M1942 solved

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
 /**
- * @param {number[]} nums
+ * @param {number[][]} times
+ * @param {number} targetFriend
  * @return {number}
  */
-var maxWidthRamp = function(nums) {
-    let N = nums.length;
-    let rightMax = new Array(N);
-    
-    rightMax[N - 1] = nums[N - 1];
-    for (let i = N - 2; i >= 0; i--) {
-        rightMax[i] = Math.max(rightMax[i + 1], nums[i]);
+var smallestChair = function(times, targetFriend) {
+    const leavingTime = new Array(times.length).fill(-1);
+    const target = times[targetFriend][0];
+    times.sort((a,b) => a[0] - b[0]);
+
+    for (let [arrival, leave] of times) {
+        let i = 0;
+        while (leavingTime[i] > arrival && leavingTime[i] !== -1) i++;
+        if (arrival === target) {
+            console.log(i);
+            return i;
+        }
+        leavingTime[i] = leave;
     }
 
-    let left = 0, right = 0;
-    let maxWidth = 0;
-
-    while (right < N) {
-        while (left < right && nums[left] > rightMax[right]) left ++;
-
-        maxWidth = Math.max(maxWidth, right - left);
-        right++;
-    }
-    console.log(maxWidth);
+    console.log(-1);
 };
 
-maxWidthRamp([6,0,8,2,1,5]);
-maxWidthRamp([9,8,1,0,1,9,4,0,4,1]);
+smallestChair([[1,4],[2,3],[4,6]],1);
+smallestChair([[3,10],[1,5],[2,6]],0);
