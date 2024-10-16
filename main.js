9864e8e9ced2559714952598a2e3342a81212a69
Jesse_Carter <jessetcarter@hotmail.com>
2024-10-16 13:22:52 -0600

    M1405 solved

--- main.js
+++ main.js
@@ -1,22 +1,48 @@
 /**
- * @param {string} s
- * @return {number}
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {string}
  */
-var minimumSteps = function(s) {
-    s = s.split('');
-    let whitePosition = 0;
-    let totalSwaps = 0;
+var longestDiverseString = function(a, b, c) {
+    let currA = 0;
+    let currB = 0;
+    let currC = 0;
+    let totalIterations = a + b + c;
+    let ans = "";
 
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === '0') {
-            totalSwaps += i - whitePosition;
-            whitePosition++;
+    for (let i = 0; i < totalIterations; i++) {
+        if (
+            (a >= b && a >= c && currA !== 2) ||
+            (a > 0 && (currB === 2 || currC === 2))
+        ) {
+            ans += 'a';
+            a--;
+            currA++;
+            currB = 0;
+            currC = 0;
+        } else if (
+            (b >= a && b >= c && currB !== 2) ||
+            (b > 0 && (currC === 2 || currA === 2))
+        ) {
+            ans += 'b';
+            b--;
+            currB++;
+            currA = 0;
+            currC = 0;
+        } else if (
+            (c >= a && c >= b && currC !== 2) ||
+            (c > 0 && (currA === 2 || currB === 2))
+        ) {
+            ans += 'c';
+            c--;
+            currC++;
+            currA = 0;
+            currB = 0;
         }
     }
-
-    console.log(totalSwaps);
+    console.log(ans);
 };
 
-minimumSteps("101");
-minimumSteps("100");
-minimumSteps("0111");
+longestDiverseString(1,1,7);
+longestDiverseString(7,1,0);
