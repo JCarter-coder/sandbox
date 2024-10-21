01d6b64d91ad56a75081bafd166405d259f0a345
Jesse_Carter <jessetcarter@hotmail.com>
2024-10-20 23:08:28 -0600

    M1593 solved

--- main.js
+++ main.js
@@ -1,42 +1,31 @@
 /**
- * @param {string} expression
- * @return {boolean}
+ * @param {string} s
+ * @return {number}
  */
-var parseBoolExpr = function(expression) {
-    const st = [];
-    for (let currChar of expression.split('')) {
-        if (currChar === ',' || currChar === '(') continue;
+var maxUniqueSplit = function(s) {
+    let res = 1;
+    let strings = new Set();
+    
+    let search = (s) => {
+        if (!strings.has(s)) {
+            strings.add(s);
+            res = Math.max(res, strings.size);
+            strings.delete(s);
+        }
 
-        if (
-            currChar === 't' ||
-            currChar === 'f' ||
-            currChar === '!' ||
-            currChar === '&' ||
-            currChar === '|'
-        ) st.push(currChar);
-
-        else if (currChar === ')') {
-            let hasTrue = false;
-            let hasFalse = false;
-
-            while (
-                st[st.length - 1] !== '!' && st[st.length - 1] !== '&' && st[st.length - 1] !== '|'
-            ) {
-                let topValue = st.pop();
-                if (topValue === 't') hasTrue = true;
-                if (topValue === 'f') hasFalse = true;
-            }
-
-            let op = st.pop();
-            if (op === '!') st.push(hasTrue ? 'f' : 't');
-            else if (op === '&') st.push(hasFalse ? 'f' : 't');
-            else st.push(hasTrue ? 't' : 'f');
+        for (let i = 1; i < s.length; i++) {
+            const sub = s.substring(0, i);
+            if (strings.has(sub)) continue;
+            strings.add(sub);
+            search(s.substring(i));
+            strings.delete(sub);
         }
     }
 
-    console.log(st[st.length - 1] === 't');
+    search(s);
+    console.log(res);
 };
 
-parseBoolExpr("&(|(f))");
-parseBoolExpr("|(f,f,f,t)");
-parseBoolExpr("!(&(f,t))");
+maxUniqueSplit("ababccc");
+maxUniqueSplit("aba");
+maxUniqueSplit("aa");
