3c2aaef15a3ad9a08514b26c6ffd290549c0f645
Jesse_Carter <jessetcarter@hotmail.com>
2024-10-26 14:44:21 -0600

    H2458 solved

--- main.js
+++ main.js
@@ -1,22 +1,53 @@
 /**
- * @param {string[]} folder
- * @return {string[]}
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
  */
-var removeSubfolders = function(folder) {
-    folder.sort();
-    const result = new Array();
-    result.push(folder[0]);
+/**
+ * @param {TreeNode} root
+ * @param {number[]} queries
+ * @return {number[]}
+ */
+var treeQueries = function(root, queries) {
+    const maxHeightAfterRemoval = [];
+    let currentMaxHeight = 0;
 
-    for (let i = 1; i < folder.length; i++) {
-        let lastFolder = result[result.length - 1];
-        lastFolder += '/';
-        if (!folder[i].startsWith(lastFolder)) result.push(folder[i]);
+    let traverseLeftToRight = (node, currentHeight) => {
+        if (node === null) return;
+
+        maxHeightAfterRemoval[node.val] = currentMaxHeight;
+        currentMaxHeight = Math.max(currentMaxHeight, currentHeight);
+        traverseLeftToRight(node.left, currentHeight + 1);
+        traverseLeftToRight(node.right, currentHeight + 1);
     }
 
-    console.log(result);
+    let traverseRightToLeft = (node, currentHeight) => {
+        if (node === null) return;
+
+        maxHeightAfterRemoval[node.val] = Math.max(
+            maxHeightAfterRemoval[node.val],
+            currentMaxHeight
+        );
+        currentMaxHeight = Math.max(currentMaxHeight, currentHeight);
+        traverseRightToLeft(node.right, currentHeight + 1);
+        traverseRightToLeft(node.left, currentHeight + 1);
+    }
+
+    traverseLeftToRight(root, 0);
+    currentMaxHeight = 0;
+    traverseRightToLeft(root, 0);
+
+    let queryCount = queries.length;
+    let queryResults = [];
+    for (let i = 0; i < queryCount; i++) {
+        queryResults.push(maxHeightAfterRemoval[queries[i]]);
+    }
+
+    return queryResults;
 };
 
-removeSubfolders(["/a","/a/b","/c/d","/c/d/e","/c/f"]);
-removeSubfolders(["/a","/a/b/c","/a/b/d"]);
-removeSubfolders(["/a/b/c","/a/b/ca","/a/b/d"]);
-removeSubfolders(["/ah/al/am","/ah/al"]);
+treeQueries([1,3,4,2,null,6,5,null,null,null,null,null,7],[4]);
+treeQueries([5,8,9,2,1,3,7,4,6],[3,2,4,8]);
