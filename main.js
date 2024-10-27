dbe30da451b1f26d1d9fe9c3d6c93228c72c4099
Jesse_Carter <jessetcarter@hotmail.com>
2024-10-27 13:00:29 -0600

    M2664 solved

--- main.js
+++ main.js
@@ -1,53 +1,110 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
+ * @param {number} m
+ * @param {number} n
+ * @param {number} r
+ * @param {number} c
+ * @return {number[][]}
  */
-/**
- * @param {TreeNode} root
- * @param {number[]} queries
- * @return {number[]}
- */
-var treeQueries = function(root, queries) {
-    const maxHeightAfterRemoval = [];
-    let currentMaxHeight = 0;
+var tourOfKnight = function(m, n, r, c) {
+    const knightMoves = [
+        [-1,-2],
+        [-2,-1],
+        [-1,2],
+        [-2,1],
+        [1,-2],
+        [2,-1],
+        [1,2],
+        [2,1],
+    ]
 
-    let traverseLeftToRight = (node, currentHeight) => {
-        if (node === null) return;
-
-        maxHeightAfterRemoval[node.val] = currentMaxHeight;
-        currentMaxHeight = Math.max(currentMaxHeight, currentHeight);
-        traverseLeftToRight(node.left, currentHeight + 1);
-        traverseLeftToRight(node.right, currentHeight + 1);
+    let chessboard = [];
+    for (let i = 0; i < m; i++) {
+        chessboard.push(new Array(n).fill(0));
     }
+    chessboard[r][c] = -1;
 
-    let traverseRightToLeft = (node, currentHeight) => {
-        if (node === null) return;
-
-        maxHeightAfterRemoval[node.val] = Math.max(
-            maxHeightAfterRemoval[node.val],
-            currentMaxHeight
+    let isValidMove = (chessboard, row, col) => {
+        return (
+            row >= 0 &&
+            col >= 0 &&
+            row < chessboard.length &&
+            col < chessboard[0].length &&
+            chessboard[row][col] === 0
         );
-        currentMaxHeight = Math.max(currentMaxHeight, currentHeight);
-        traverseRightToLeft(node.right, currentHeight + 1);
-        traverseRightToLeft(node.left, currentHeight + 1);
     }
 
-    traverseLeftToRight(root, 0);
-    currentMaxHeight = 0;
-    traverseRightToLeft(root, 0);
-
-    let queryCount = queries.length;
-    let queryResults = [];
-    for (let i = 0; i < queryCount; i++) {
-        queryResults.push(maxHeightAfterRemoval[queries[i]]);
+    let countAccessibleMoves = (chessboard, row, col) => {
+        let count = 0;
+        for (let move of knightMoves) {
+            let nextRow = row + move[0];
+            let nextCol = col + move[1];
+            if (isValidMove(chessboard, nextRow, nextCol)) count++;
+        }
+        return count;
     }
 
-    return queryResults;
+    let getNextMovesWarnsdorff = (chessboard, row, col) => {
+        let nextMoves = [];
+        for (let moveIndex = 0; moveIndex < 8; moveIndex++) {
+            let nextRow = row + knightMoves[moveIndex][0];
+            let nextCol = col + knightMoves[moveIndex][1];
+            let accessibilityScore = countAccessibleMoves(
+                chessboard,
+                nextRow,
+                nextCol
+            );
+            nextMoves.push([accessibilityScore, moveIndex]);
+        }
+        nextMoves.sort((a,b) => a[0] - b[0]);
+        return nextMoves;
+    }
+
+    let solveKnightsTour = (
+        rows,
+        cols,
+        currentRow,
+        currentCol,
+        chessboard,
+        moveCount
+    ) => {
+        if (moveCount === rows * cols) return true;
+
+        let nextMoves = getNextMovesWarnsdorff(
+            chessboard,
+            currentRow,
+            currentCol
+        );
+
+        for (let move of nextMoves) {
+            let nextRow = currentRow + knightMoves[move[1]][0];
+            let nextCol = currentCol + knightMoves[move[1]][1];
+
+            if (!isValidMove(chessboard, nextRow, nextCol)) continue;
+
+            chessboard[nextRow][nextCol] = moveCount;
+
+            if (
+                solveKnightsTour(
+                    rows,
+                    cols,
+                    nextRow,
+                    nextCol,
+                    chessboard,
+                    moveCount + 1
+                )
+            ) return true;
+
+            chessboard[nextRow][nextCol] = 0;
+        }
+        return false;
+    }
+
+    solveKnightsTour(m, n, r, c, chessboard, 1);
+
+    chessboard[r][c] = 0;
+    console.log(chessboard);
 };
 
-treeQueries([1,3,4,2,null,6,5,null,null,null,null,null,7],[4]);
-treeQueries([5,8,9,2,1,3,7,4,6],[3,2,4,8]);
+tourOfKnight(1,1,0,0);
+tourOfKnight(3,4,0,0);
+
