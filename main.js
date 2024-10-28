3d65dbc014ad6d4467d6b39096b08dbe9b866dc5
Jesse_Carter <jessetcarter@hotmail.com>
2024-10-28 16:29:06 -0600

    M2501 solved

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
 /**
- * @param {number[][]} matrix
+ * @param {number[]} nums
  * @return {number}
  */
-var countSquares = function(matrix) {
-    let row = matrix.length;
-    let col = matrix[0].length;
-    let dp = new Array(row + 1);
-    for (let i = 0; i < dp.length; i++) {
-        dp[i] = new Array(col + 1).fill(0);
+var longestSquareStreak = function(nums) {
+    let longestStreak = 0;
+    const uniqueNumbers = new Set();
+    for (let num of nums) {
+        uniqueNumbers.add(num);
     }
-    let ans = 0;
-    for (let i = 0; i < row; i++) {
-        for (let j = 0; j < col; j++) {
-            if (matrix[i][j] === 1) {
-                dp[i + 1][j + 1] = Math.min(
-                    Math.min(dp[i][j + 1], dp[i + 1][j]), dp[i][j]
-                ) + 1;
-                ans += dp[i + 1][j + 1];
-            }
+
+    for (let startNumber of nums) {
+        let currentStreak = 0;
+        let current = startNumber;
+
+        while (uniqueNumbers.has(current)) {
+            currentStreak++;
+            if (current * current > 1e5) break;
+            current *= current;
         }
+
+        longestStreak = Math.max(longestStreak, currentStreak);
     }
-    console.log(ans);
+
+    console.log(longestStreak < 2 ? -1 : longestStreak);
 };
 
-countSquares([[0,1,1,1],[1,1,1,1],[0,1,1,1]]);
-countSquares([[1,0,1],[1,1,0],[1,1,0]]);
+longestSquareStreak([4,3,6,16,8,2]);
+longestSquareStreak([2,3,5,6,7]);
