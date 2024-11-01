cc63fb12e8b657ec512e8f870a87918153b3d990
Jesse_Carter <jessetcarter@hotmail.com>
2024-11-01 09:43:52 -0600

    E1957 solved

--- main.js
+++ main.js
@@ -1,45 +1,29 @@
 /**
- * @param {number[]} robot
- * @param {number[][]} factory
- * @return {number}
+ * @param {string} s
+ * @return {string}
  */
-var minimumTotalDistance = function(robot, factory) {
-    robot.sort((a,b) => a - b);
-    factory.sort((a,b) => a[0] - b[0]);
+var makeFancyString = function(s) {
+    if (s.length < 3) {
+        console.log(s);
+        return s;
+    }
+    let stringArray = [];
+    stringArray.push(s[0]);
+    stringArray.push(s[1]);
 
-    let factoryPositions = [];
-    for (let f of factory) {
-        for (let i = 0; i < f[1]; i++) {
-            factoryPositions.push(f[0]);
+    for (let i = 2; i < s.length; i++) {
+        if (
+            s[i] !== stringArray[stringArray.length - 1] ||
+            s[i] !== stringArray[stringArray.length - 2]
+        ) {
+            stringArray.push(s[i]);
         }
     }
-
-    let robotCount = robot.length;
-    let factoryCount = factoryPositions.length;
-
-    let dp = new Array(robotCount + 1);
-    for (let i = 0; i < dp.length; i++) {
-        dp[i] = (new Array(factoryCount + 1).fill(0));
-    }
-
-    for (let i = 0; i < robotCount; i++) {
-        dp[i][factoryCount] = 1e12;
-    }
-
-    for (let i = robotCount - 1; i >= 0; i--) {
-        for (let j = factoryCount - 1; j >= 0; j--) {
-            let assign = Math.abs(
-                robot[i] - factoryPositions[j]
-            ) + dp[i + 1][j + 1];
-
-            let skip = dp[i][j + 1];
-
-            dp[i][j] = Math.min(assign, skip);
-        }
-    }
-
-    console.log(dp[0][0]);
+    
+    console.log(stringArray.join(''));
 };
 
-minimumTotalDistance([0,4,6],[[2,2],[6,2]]);
-minimumTotalDistance([1,-1],[[-2,1],[2,1]]);
+makeFancyString("leeetcode");
+makeFancyString("aaabaaaa");
+makeFancyString("aab");
+makeFancyString("aaa");
