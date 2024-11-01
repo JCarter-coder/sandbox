e2920ed1b8db22edbf2349ab03e3595fc7270f11
Jesse_Carter <jessetcarter@hotmail.com>
2024-11-01 17:19:06 -0600

    M2955 solved TS

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
-"use strict";
-function sameEndSubstringCount(s, queries) {
-    let N = s.length;
-    let charFreqPrefixSum = new Array();
+function sameEndSubstringCount(s: string, queries: number[][]): number[] {
+    let N: number = s.length;
+    let charFreqPrefixSum: number[][] = new Array();
     // create array
     for (let i = 0; i < 26; i++) {
         charFreqPrefixSum.push(new Array(N).fill(0));
@@ -16,11 +15,14 @@ function sameEndSubstringCount(s, queries) {
             charFreqPrefixSum[i][j] += charFreqPrefixSum[i][j - 1];
         }
     }
-    let results = new Array(queries.length);
+
+    let results: number[] = new Array(queries.length);
+
     for (let i = 0; i < queries.length; i++) {
         let leftIndex = queries[i][0];
         let rightIndex = queries[i][1];
         let countSameEndSubstrings = 0;
+
         for (let charIndex = 0; charIndex < 26; charIndex++) {
             // calculate freq within query range
             let leftFreq = (leftIndex === 0)
@@ -28,14 +30,18 @@ function sameEndSubstringCount(s, queries) {
                 : charFreqPrefixSum[charIndex][leftIndex - 1];
             let rightFreq = charFreqPrefixSum[charIndex][rightIndex];
             let frequencyInRange = rightFreq - leftFreq;
+
             // calculate number of same-end substrings for this character
-            countSameEndSubstrings += Math.floor((frequencyInRange * (frequencyInRange + 1)) / 2);
+            countSameEndSubstrings += Math.floor((
+                frequencyInRange * (frequencyInRange + 1)
+            ) / 2);
         }
+
         results[i] = countSameEndSubstrings;
     }
     console.log(results);
     return results;
-}
-;
-sameEndSubstringCount("abcaab", [[0, 0], [1, 4], [2, 5], [0, 5]]);
-sameEndSubstringCount("abcd", [[0, 3]]);
+};
+
+sameEndSubstringCount("abcaab",[[0,0],[1,4],[2,5],[0,5]]);
+sameEndSubstringCount("abcd",[[0,3]]);
