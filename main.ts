b0141c60f6f2b7dfd1f643729a814408ef243c2b
Jesse_Carter <jessetcarter@hotmail.com>
2024-11-02 13:16:07 -0600

    E2490 solved TS

--- main.ts
+++ main.ts
@@ -1,47 +1,27 @@
-function sameEndSubstringCount(s: string, queries: number[][]): number[] {
-    let N: number = s.length;
-    let charFreqPrefixSum: number[][] = new Array();
-    // create array
-    for (let i = 0; i < 26; i++) {
-        charFreqPrefixSum.push(new Array(N).fill(0));
+function isCircularSentence(sentence: string): boolean {
+    let ans: boolean = false;
+    const sentenceArray: string[] = sentence.split(' ');
+    if (
+        sentenceArray[0].at(0) !== 
+        sentenceArray[sentenceArray.length - 1].at(-1)
+    ) {
+        console.log(ans);
+        return ans
+    };
+    for (let i = 1; i < sentenceArray.length; i++) {
+        if (
+            sentenceArray[i].at(0) !== 
+            sentenceArray[i - 1].at(-1)
+        ) {
+            console.log(ans);
+            return ans
+        };
     }
-    // fill frequency array
-    for (let i = 0; i < N; i++) {
-        charFreqPrefixSum[s.charCodeAt(i) - 97][i]++;
-    }
-    // convert to a prefix sum
-    for (let i = 0; i < 26; i++) {
-        for (let j = 1; j < N; j++) {
-            charFreqPrefixSum[i][j] += charFreqPrefixSum[i][j - 1];
-        }
-    }
-
-    let results: number[] = new Array(queries.length);
-
-    for (let i = 0; i < queries.length; i++) {
-        let leftIndex = queries[i][0];
-        let rightIndex = queries[i][1];
-        let countSameEndSubstrings = 0;
-
-        for (let charIndex = 0; charIndex < 26; charIndex++) {
-            // calculate freq within query range
-            let leftFreq = (leftIndex === 0)
-                ? 0
-                : charFreqPrefixSum[charIndex][leftIndex - 1];
-            let rightFreq = charFreqPrefixSum[charIndex][rightIndex];
-            let frequencyInRange = rightFreq - leftFreq;
-
-            // calculate number of same-end substrings for this character
-            countSameEndSubstrings += Math.floor((
-                frequencyInRange * (frequencyInRange + 1)
-            ) / 2);
-        }
-
-        results[i] = countSameEndSubstrings;
-    }
-    console.log(results);
-    return results;
+    ans = true;
+    console.log(ans);
+    return ans;
 };
 
-sameEndSubstringCount("abcaab",[[0,0],[1,4],[2,5],[0,5]]);
-sameEndSubstringCount("abcd",[[0,3]]);
+isCircularSentence("leetcode exercises sound delightful");
+isCircularSentence("eetcode");
+isCircularSentence("Leetcode is cool");
