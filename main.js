ef8e05494c0b86c890b156148401e4c7575e42f2
Jesse_Carter <jessetcarter@hotmail.com>
2024-11-10 11:40:44 -0700

    M3097 solved TS

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
 "use strict";
-function lengthOfLastWord(s) {
-    let index = s.length - 1;
-    let lastStringLength = 0;
-    while (index >= 0 && s.charAt(index) === ' ') {
-        index--;
+function minimumSubarrayLength(nums, k) {
+    let minLength = Number.MAX_SAFE_INTEGER;
+    let windowStart = 0;
+    let windowEnd = 0;
+    let bitCounts = new Array(32).fill(0);
+    function updateBitCounts(bitCounts, num, delta) {
+        for (let bitPosition = 0; bitPosition < 32; bitPosition++) {
+            if (((num >> bitPosition) & 1) != 0) {
+                bitCounts[bitPosition] += delta;
+            }
+        }
     }
-    while (index >= 0 && s.charAt(index) !== ' ') {
-        lastStringLength++;
-        index--;
+    function convertBitCountsToNumber(bitCounts) {
+        let result = 0;
+        for (let bitPosition = 0; bitPosition < 32; bitPosition++) {
+            if (bitCounts[bitPosition] != 0) {
+                result |= 1 << bitPosition;
+            }
+        }
+        return result;
     }
-    console.log(lastStringLength);
-    return lastStringLength;
+    while (windowEnd < nums.length) {
+        updateBitCounts(bitCounts, nums[windowEnd], 1);
+        while (windowStart <= windowEnd &&
+            convertBitCountsToNumber(bitCounts) >= k) {
+            minLength = Math.min(minLength, windowEnd - windowStart + 1);
+            updateBitCounts(bitCounts, nums[windowStart], -1);
+            windowStart++;
+        }
+        windowEnd++;
+    }
+    console.log(minLength === Number.MAX_SAFE_INTEGER ? -1 : minLength);
+    return minLength === Number.MAX_SAFE_INTEGER ? -1 : minLength;
 }
 ;
-lengthOfLastWord("Hello World");
-lengthOfLastWord("   fly me   to   the moon  ");
-lengthOfLastWord("luffy is still joyboy");
+minimumSubarrayLength([1, 2, 3], 2);
+minimumSubarrayLength([2, 1, 8], 10);
+minimumSubarrayLength([1, 2], 0);
--- main.ts
+++ main.ts
@@ -1,17 +1,50 @@
-function lengthOfLastWord(s: string): number {
-    let index = s.length - 1;
-    let lastStringLength = 0;
-    while (index >= 0 && s.charAt(index) === ' ') {
-        index--;
+function minimumSubarrayLength(nums: number[], k: number): number {
+    let minLength: number = Number.MAX_SAFE_INTEGER;
+    let windowStart: number = 0;
+    let windowEnd: number = 0;
+    let bitCounts: number[] = new Array(32).fill(0);
+
+    function updateBitCounts(
+        bitCounts: number[],
+        num: number,
+        delta: number
+    ): void {
+        for (let bitPosition = 0; bitPosition < 32; bitPosition++) {
+            if (((num >> bitPosition) & 1) != 0) {
+                bitCounts[bitPosition] += delta;
+            }
+        }
     }
-    while (index >= 0 && s.charAt(index) !== ' ') {
-        lastStringLength++;
-        index--;
+
+    function convertBitCountsToNumber(bitCounts: number[]): number {
+        let result: number = 0;
+        for (let bitPosition = 0; bitPosition < 32; bitPosition++) {
+            if (bitCounts[bitPosition] != 0) {
+                result |= 1 << bitPosition;
+            }
+        }
+        return result;
     }
-    console.log(lastStringLength);
-    return lastStringLength;
+    
+    while (windowEnd < nums.length) {
+        updateBitCounts(bitCounts, nums[windowEnd], 1);
+
+        while (
+            windowStart <= windowEnd &&
+            convertBitCountsToNumber(bitCounts) >= k
+        ) {
+            minLength = Math.min(minLength, windowEnd - windowStart + 1);
+            updateBitCounts(bitCounts, nums[windowStart], -1);
+            windowStart++;
+        }
+
+        windowEnd++;
+    }
+
+    console.log(minLength === Number.MAX_SAFE_INTEGER ? -1 : minLength);
+    return minLength === Number.MAX_SAFE_INTEGER ? -1 : minLength;
 };
 
-lengthOfLastWord("Hello World");
-lengthOfLastWord("   fly me   to   the moon  ");
-lengthOfLastWord("luffy is still joyboy");
+minimumSubarrayLength([1,2,3],2);
+minimumSubarrayLength([2,1,8],10);
+minimumSubarrayLength([1,2],0);
