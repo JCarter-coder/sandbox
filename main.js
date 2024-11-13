1a32385862e7053861c3725a57a906ab88cc877d
Jesse_Carter <jessetcarter@hotmail.com>
2024-11-13 14:02:09 -0700

    M274 solved TS

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
 "use strict";
-function countFairPairs(nums, lower, upper) {
-    nums.sort((a, b) => a - b);
-    function lowerBound(nums, value) {
-        let left = 0;
-        let right = nums.length - 1;
-        let result = 0;
-        while (left < right) {
-            let sum = nums[left] + nums[right];
-            if (sum < value) {
-                result += (right - left);
-                left++;
-            }
-            else
-                right--;
-        }
-        return result;
+function hIndex(citations) {
+    citations.sort((a, b) => a - b);
+    let h = 0;
+    while (h < citations.length &&
+        citations[citations.length - 1 - h] > h) {
+        h++;
     }
-    console.log(lowerBound(nums, upper + 1) - lowerBound(nums, lower));
-    return lowerBound(nums, upper + 1) - lowerBound(nums, lower);
+    console.log(h);
+    return h;
 }
 ;
-countFairPairs([0, 1, 7, 4, 4, 5], 3, 6);
-countFairPairs([1, 7, 9, 2, 5], 11, 11);
+hIndex([3, 0, 6, 1, 5]);
+hIndex([1, 3, 1]);
+hIndex([100]);
--- main.ts
+++ main.ts
@@ -1,24 +1,17 @@
-function countFairPairs(nums: number[], lower: number, upper: number): number {
-    nums.sort((a,b) => a - b);
+function hIndex(citations: number[]): number {
+    citations.sort((a,b) => a - b);
+    let h = 0;
 
-    function lowerBound(nums: number[], value: number) {
-        let left: number = 0;
-        let right: number = nums.length - 1;
-        let result = 0;
-
-        while (left < right) {
-            let sum: number = nums[left] + nums[right];
-            if (sum < value) {
-                result += (right - left);
-                left++;
-            } else right--;
-        }
-
-        return result;
+    while (
+        h < citations.length &&
+        citations[citations.length - 1 - h] > h
+    ) {
+        h++;
     }
-    console.log(lowerBound(nums, upper + 1) - lowerBound(nums, lower));
-    return lowerBound(nums, upper + 1) - lowerBound(nums, lower);
+    console.log(h);
+    return h;
 };
 
-countFairPairs([0,1,7,4,4,5],3,6);
-countFairPairs([1,7,9,2,5],11,11);
+hIndex([3,0,6,1,5]);
+hIndex([1,3,1]);
+hIndex([100]);
