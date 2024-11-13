53b334c1bb589145ddb8784aaa3a20e0dd56136d
Jesse_Carter <jessetcarter@hotmail.com>
2024-11-13 13:34:07 -0700

    M2563 solved

--- main.js
+++ main.js
@@ -1,39 +1,24 @@
 "use strict";
-function romanToInt(s) {
-    const numerals = new Map();
-    numerals.set('I', 1);
-    numerals.set('V', 5);
-    numerals.set('X', 10);
-    numerals.set('L', 50);
-    numerals.set('C', 100);
-    numerals.set('D', 500);
-    numerals.set('M', 1000);
-    numerals.set('IV', 4);
-    numerals.set('IX', 9);
-    numerals.set('XL', 40);
-    numerals.set('XC', 90);
-    numerals.set('CD', 400);
-    numerals.set('CM', 900);
-    let ans = 0;
-    let i = 0;
-    while (i < s.length) {
-        if (i < s.length - 1) {
-            let doubleSymbol = s.substring(i, i + 2);
-            if (numerals.has(doubleSymbol)) {
-                ans += numerals.get(doubleSymbol);
-                i += 2;
-                continue;
+function countFairPairs(nums, lower, upper) {
+    nums.sort((a, b) => a - b);
+    function lowerBound(nums, value) {
+        let left = 0;
+        let right = nums.length - 1;
+        let result = 0;
+        while (left < right) {
+            let sum = nums[left] + nums[right];
+            if (sum < value) {
+                result += (right - left);
+                left++;
             }
+            else
+                right--;
         }
-        let singleSymbol = s.substring(i, i + 1);
-        ans += numerals.get(singleSymbol);
-        i++;
+        return result;
     }
-    console.log(ans);
-    return ans;
+    console.log(lowerBound(nums, upper + 1) - lowerBound(nums, lower));
+    return lowerBound(nums, upper + 1) - lowerBound(nums, lower);
 }
 ;
-romanToInt("III");
-romanToInt("LVIII");
-romanToInt("MCMXCIV");
-romanToInt("MCDLXXVI");
+countFairPairs([0, 1, 7, 4, 4, 5], 3, 6);
+countFairPairs([1, 7, 9, 2, 5], 11, 11);
--- main.ts
+++ main.ts
@@ -1,42 +1,24 @@
-function romanToInt(s: string): number {
-    const numerals = new Map();
+function countFairPairs(nums: number[], lower: number, upper: number): number {
+    nums.sort((a,b) => a - b);
 
-    numerals.set('I', 1);
-    numerals.set('V', 5);
-    numerals.set('X', 10);
-    numerals.set('L', 50);
-    numerals.set('C', 100);
-    numerals.set('D', 500);
-    numerals.set('M', 1000);
-    numerals.set('IV', 4);
-    numerals.set('IX', 9);
-    numerals.set('XL', 40);
-    numerals.set('XC', 90);
-    numerals.set('CD', 400);
-    numerals.set('CM', 900);
-    
-    let ans: number = 0;
-    let i: number = 0;
-    
-    while (i < s.length) {
-        if (i < s.length - 1) {
-            let doubleSymbol = s.substring(i, i + 2);
-            if (numerals.has(doubleSymbol)) {
-                ans += numerals.get(doubleSymbol);
-                i += 2;
-                continue;
-            }
+    function lowerBound(nums: number[], value: number) {
+        let left: number = 0;
+        let right: number = nums.length - 1;
+        let result = 0;
+
+        while (left < right) {
+            let sum: number = nums[left] + nums[right];
+            if (sum < value) {
+                result += (right - left);
+                left++;
+            } else right--;
         }
-        let singleSymbol = s.substring(i, i + 1);
-        ans += numerals.get(singleSymbol);
-        i++;
+
+        return result;
     }
-    
-    console.log(ans);
-    return ans;
+    console.log(lowerBound(nums, upper + 1) - lowerBound(nums, lower));
+    return lowerBound(nums, upper + 1) - lowerBound(nums, lower);
 };
 
-romanToInt("III");
-romanToInt("LVIII");
-romanToInt("MCMXCIV");
-romanToInt("MCDLXXVI");
+countFairPairs([0,1,7,4,4,5],3,6);
+countFairPairs([1,7,9,2,5],11,11);
