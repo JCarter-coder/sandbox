d4ce966c87f20e6d40e5be5a8471e500adf667d5
Jesse_Carter <jessetcarter@hotmail.com>
2024-11-14 14:44:02 -0700

    M151 solved TS

--- main.ts
+++ main.ts
@@ -1,50 +1,28 @@
-function minimizedMaximum(n: number, quantities: number[]): number {
-    
-    function canDistribute(x: number, quantities: number[], n: number): boolean {
-        // Pointer to the first not fully distributed product type
-        let j = 0;
-        // Remaining quantity of the jth product type
-        let remaining = quantities[j];
-
-        // Loop through each store
-        for (let i = 0; i < n; i++) {
-            // check if the remaining quantity of the jth project type
-            // can be fully distributed to the ith store
-            if (remaining <= x) {
-                // if yes, move the pointer to the next product type
-                j++;
-                // check if all products have been distributed
-                if (j === quantities.length) return true;
-                else remaining = quantities[j];
-            } else remaining -= x;
-        }
-        return false;
-    }
-
-    let left = 0; 
-    let right = 0;
-
-    // Find the maximum element in quantities
-    for (let quantity of quantities) {
-        if (quantity > right) right = quantity;
-    }
-
-    // Perform binary search until the boundaries converge
-    while (left < right) {
-        let middle = Math.floor((left + right) / 2);
-        if (canDistribute(middle, quantities, n)) {
-            // try for a smaller maximum
-            right = middle;
-        } else {
-            // increase the minimum possible maximum
-            left = middle + 1;
+function reverseWords(s: string): string {
+    const strArray: string[] = [];
+    // build array of only strings without spaces
+    for (let i = 0; i < s.length; i++) {
+        if (s.charAt(i) === ' ') continue;
+        else {
+            let temp = ""
+            while (s.charAt(i) !== ' ' && i < s.length) {
+                temp += s.charAt(i++);
+            }
+            strArray.push(temp);
         }
     }
-    console.log(left);
-    return left;
+    console.log(strArray);
+
+    // reverse the string array
+    for (let i = 0; i < strArray.length / 2; i++) {
+        let temp = strArray[strArray.length - 1 - i];
+        strArray[strArray.length - 1 - i] = strArray[i];
+        strArray[i] = temp;
+    }
+    console.log(strArray.join(' '));
+    return strArray.join(' ');
 };
 
-minimizedMaximum(6,[11,6]);
-minimizedMaximum(7,[15,10,10]);
-minimizedMaximum(1,[100000]);
-minimizedMaximum(3,[1000,5]);
+reverseWords("the sky is blue");
+reverseWords("  hello world  ");
+reverseWords("a good   example");
