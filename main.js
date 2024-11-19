24248f83570c590bc2a47658b9dd081b365fc094
Jesse_Carter <jessetcarter@hotmail.com>
2024-11-19 16:40:54 -0700

    E28 solved TS

--- main.js
+++ main.js
@@ -1,33 +1,23 @@
 "use strict";
-function maximumSubarraySum(nums, k) {
-    const map = new Map();
-    let sum = 0;
-    let max = 0;
-    for (let i = 0; i < nums.length; i++) {
-        // Add the current element to the map and update the sum
-        map.set(nums[i], (map.get(nums[i]) || 0) + 1);
-        sum += nums[i];
-        // When window reaches size k, check for uniqueness and update max
-        if (i >= k - 1) {
-            if (map.size === k) {
-                max = Math.max(max, sum);
-            }
-            // Remove the element going out of the window
-            const outElem = nums[i + 1 - k];
-            if (map.get(outElem) === 1) {
-                map.delete(outElem);
-            }
-            else {
-                map.set(outElem, map.get(outElem) - 1);
-            }
-            sum -= outElem;
+function strStr(haystack, needle) {
+    let index = -1;
+    let N = needle.length;
+    const isFirst = (str, sFind) => {
+        if (str === sFind)
+            return true;
+        else
+            return false;
+    };
+    for (let i = 0; i <= haystack.length - N; i++) {
+        console.log(haystack.substring(i, i + N));
+        if (isFirst(haystack.substring(i, i + N), needle)) {
+            index = i;
+            break;
         }
     }
-    console.log(max);
-    return max;
+    console.log(index);
+    return index;
 }
 ;
-maximumSubarraySum([1, 5, 4, 2, 9, 9, 9], 3);
-maximumSubarraySum([4, 4, 4], 3);
-maximumSubarraySum([1, 5, 4, 2, 9, 9, 9], 1);
-maximumSubarraySum([1, 5, 4, 2, 9, 9, 9], 13);
+strStr("sadbutsad", "sad");
+strStr("leetcode", "leeto");
--- main.ts
+++ main.ts
@@ -1,37 +1,22 @@
-function maximumSubarraySum(nums: number[], k: number): number {
-    const map = new Map();
-    let sum = 0;
-    let max = 0;
+function strStr(haystack: string, needle: string): number {
+    let index: number = -1;
+    let N = needle.length;
 
-    for (let i = 0; i < nums.length; i++) {
-        // Add the current element to the map and update the sum
-        map.set(nums[i], (map.get(nums[i]) || 0) + 1);
-        sum += nums[i];
+    const isFirst = (str: string, sFind: string): boolean => {
+        if (str === sFind) return true;
+        else return false;
+    }
 
-        // When window reaches size k, check for uniqueness and update max
-        if (i >= k - 1) {
-            if (map.size === k) {
-                max = Math.max(max, sum);
-            }
-
-            // Remove the element going out of the window
-            const outElem = nums[i + 1 - k];
-            if (map.get(outElem) === 1) {
-                map.delete(outElem);
-            } else {
-                map.set(outElem, map.get(outElem) - 1);
-            }
-            sum -= outElem;
+    for (let i = 0; i <= haystack.length - N; i++) {
+        console.log(haystack.substring(i, i + N))
+        if (isFirst(haystack.substring(i, i + N), needle)) {
+            index = i;
+            break;
         }
     }
-    
-    console.log(max);
-    return max;
+    console.log(index)
+    return index;
 };
 
-maximumSubarraySum([1,5,4,2,9,9,9],3);
-maximumSubarraySum([4,4,4],3);
-maximumSubarraySum([1,5,4,2,9,9,9],1);
-maximumSubarraySum([1,5,4,2,9,9,9],13);
-
-
+strStr("sadbutsad","sad");
+strStr("leetcode","leeto");
