8bb25cf3c7faeedf561e29ac90d85824b02bb233
Jesse_Carter <jessetcarter@hotmail.com>
2024-12-04 11:55:37 -0700

    M2825 solved TS

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 "use strict";
-function addSpaces(s, spaces) {
-    let ans = "";
-    let currentSpaceIndex = 0;
-    for (let i = 0; i < s.length; i++) {
-        if (spaces[currentSpaceIndex] === i) {
-            ans += ' ';
-            currentSpaceIndex++;
+function canMakeSubsequence(str1, str2) {
+    let ptr1 = 0;
+    let ptr2 = 0;
+    let result = false;
+    while (ptr1 < str1.length && ptr2 < str2.length) {
+        if (str1.charCodeAt(ptr1) === str2.charCodeAt(ptr2) ||
+            (str1.charCodeAt(ptr1) + 1 - 97) % 26 + 97 === str2.charCodeAt(ptr2)) {
+            if (ptr2 === str2.length - 1) {
+                result = true;
+                break;
+            }
+            else
+                ptr2++;
         }
-        ans += s.charAt(i);
+        ptr1++;
     }
-    console.log(ans);
-    return ans;
+    console.log(result);
+    return result;
 }
 ;
-addSpaces("LeetcodeHelpsMeLearn", [8, 13, 15]);
-addSpaces("icodeinpython", [1, 5, 7, 9]);
-addSpaces("spacing", [0, 1, 2, 3, 4, 5, 6]);
+canMakeSubsequence("abc", "ad");
+canMakeSubsequence("zc", "ad");
+canMakeSubsequence("ab", "d");
--- main.ts
+++ main.ts
@@ -1,17 +1,25 @@
-function addSpaces(s: string, spaces: number[]): string {
-    let ans: string = "";
-    let currentSpaceIndex = 0;
-    for (let i = 0; i < s.length; i++) {
-        if (spaces[currentSpaceIndex] === i) {
-            ans += ' ';
-            currentSpaceIndex++;
-        }
-        ans += s.charAt(i);
+function canMakeSubsequence(str1: string, str2: string): boolean {
+    let ptr1: number = 0;
+    let ptr2: number = 0;
+    let result: boolean = false;
+
+    while (ptr1 < str1.length && ptr2 < str2.length) {
+        if (
+            str1.charCodeAt(ptr1) === str2.charCodeAt(ptr2) ||
+            (str1.charCodeAt(ptr1) + 1 - 97) % 26 + 97 === str2.charCodeAt(ptr2)
+        ) {
+            if (ptr2 === str2.length - 1) {
+                result = true;
+                break;
+            } else ptr2++;
+        } 
+        ptr1++;
     }
-    console.log(ans);
-    return ans;
+
+    console.log(result);
+    return result;
 };
 
-addSpaces("LeetcodeHelpsMeLearn",[8,13,15]);
-addSpaces("icodeinpython",[1,5,7,9]);
-addSpaces("spacing",[0,1,2,3,4,5,6]);
+canMakeSubsequence("abc","ad");
+canMakeSubsequence("zc","ad");
+canMakeSubsequence("ab","d");
