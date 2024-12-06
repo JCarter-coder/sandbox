706308150f2cda7dd20c9e4f2132e44829ffcb7f
Jesse_Carter <jessetcarter@hotmail.com>
2024-12-05 18:10:47 -0700

    E876 solved TS

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
 "use strict";
-function numberOfSteps(num) {
-    let count = 0;
-    while (num > 0) {
-        if (num % 2 === 0) {
-            num /= 2;
-        }
-        else
-            num--;
-        //console.log(num);
-        count++;
+//Definition for singly-linked list.
+class ListNode {
+    constructor(val, next) {
+        this.val = (val === undefined ? 0 : val);
+        this.next = (next === undefined ? null : next);
     }
-    console.log(count);
-    return count;
+}
+class LinkedList {
+    constructor(array) {
+        this.head = new ListNode(array[0]);
+        let currentNode = this.head;
+        for (let i = 1; i < array.length; i++) {
+            currentNode.next = new ListNode(array[i]);
+            currentNode = currentNode.next;
+        }
+    }
+}
+function middleNode(head) {
+    let steps = 0;
+    let currentNode = head;
+    while (currentNode !== null) {
+        steps++;
+        currentNode = currentNode.next;
+    }
+    let nodeNumber = (steps % 2 === 0 ? steps / 2 + 1 : Math.ceil(steps / 2));
+    //console.log(nodeNumber);
+    currentNode = head;
+    while (--nodeNumber > 0 && currentNode !== null) {
+        currentNode = currentNode.next;
+    }
+    console.log(currentNode);
+    return currentNode;
 }
 ;
-numberOfSteps(14);
-numberOfSteps(8);
-numberOfSteps(123);
+let list1 = new LinkedList([1, 2, 3, 4, 5]);
+let list2 = new LinkedList([1, 2, 3, 4, 5, 6]);
+middleNode(list1.head);
+middleNode(list2.head);
--- main.ts
+++ main.ts
@@ -1,17 +1,48 @@
-function numberOfSteps(num: number): number {
-    let count: number = 0;
-    while (num > 0) {
-        if (num % 2 === 0) {
-            num /= 2;
-        } else num--;
-        //console.log(num);
-        count++;
+//Definition for singly-linked list.
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+class LinkedList {
+    head: ListNode;
+    constructor(array: number[]) {
+        this.head = new ListNode(array[0])
+        let currentNode = this.head;
+        for (let i = 1; i < array.length; i++) {
+            currentNode.next = new ListNode(array[i]);
+            currentNode = currentNode.next;
+        }
+    }
+}
+
+
+function middleNode(head: ListNode | null): ListNode | null {
+    let steps: number = 0;
+    let currentNode = head;
+    while (currentNode !== null) {
+        steps++;
+        currentNode = currentNode.next;
     }
 
-    console.log(count);
-    return count;
+    let nodeNumber: number = (
+        steps % 2 === 0 ? steps / 2 + 1 : Math.ceil(steps/2)
+    );
+
+    currentNode = head;
+    while (--nodeNumber > 0 && currentNode !== null) {
+        currentNode = currentNode.next;
+    }
+
+    console.log(currentNode);
+    return currentNode;
 };
 
-numberOfSteps(14);
-numberOfSteps(8);
-numberOfSteps(123);
+let list1 = new LinkedList([1,2,3,4,5]);
+let list2 = new LinkedList([1,2,3,4,5,6]);
+middleNode(list1.head);
+middleNode(list2.head);
