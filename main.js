7fc7e249ea93ac90e7e21430ae2c3709a1850317
Jesse_Carter <jessetcarter@hotmail.com>
2024-12-07 23:20:41 -0700

    M2054 solved TS

--- main.js
+++ main.js
@@ -1,32 +1,38 @@
 "use strict";
-function minimumSize(nums, maxOperations) {
-    let left = 1;
-    let right = 0;
-    const isPossible = (maxBallsInBag, nums, maxOperations) => {
-        let totalOperations = 0;
-        for (let num of nums) {
-            let operations = Math.ceil(num / maxBallsInBag) - 1;
-            totalOperations += operations;
-            if (totalOperations > maxOperations)
-                return false;
+function maxTwoEvents(events) {
+    events.sort((a, b) => a[0] - b[0]);
+    const dp = new Array();
+    for (let i = 0; i < events.length; i++) {
+        dp[i] = new Array(3).fill(-1);
+    }
+    const findEvents = (events, idx, cnt, dp) => {
+        if (cnt === 2 || idx >= events.length)
+            return 0;
+        if (dp[idx][cnt] === -1) {
+            let end = events[idx][1];
+            let lo = idx + 1;
+            let hi = events.length - 1;
+            while (lo < hi) {
+                let mid = lo + ((hi - lo) >> 1);
+                if (events[mid][0] > end)
+                    hi = mid;
+                else
+                    lo = mid + 1;
+            }
+            let include = events[idx][2] + (lo < events.length && events[lo][0] > end
+                ? findEvents(events, lo, cnt + 1, dp)
+                : 0);
+            let exclude = findEvents(events, idx + 1, cnt, dp);
+            dp[idx][cnt] = Math.max(include, exclude);
         }
-        return true;
+        return dp[idx][cnt];
     };
-    for (let num of nums) {
-        right = Math.max(right, num);
-    }
-    while (left < right) {
-        let middle = Math.floor((left + right) / 2);
-        if (isPossible(middle, nums, maxOperations)) {
-            right = middle;
-        }
-        else {
-            left = middle + 1;
-        }
-    }
-    console.log(left);
-    return left;
+    console.log(dp);
+    let ans = findEvents(events, 0, 0, dp);
+    console.log(ans);
+    return ans;
 }
 ;
-minimumSize([9], 2);
-minimumSize([2, 4, 8, 2], 4);
+maxTwoEvents([[1, 3, 2], [4, 5, 2], [2, 4, 3]]);
+maxTwoEvents([[1, 3, 2], [4, 5, 2], [1, 5, 5]]);
+maxTwoEvents([[1, 5, 3], [1, 5, 1], [6, 6, 5]]);
--- main.ts
+++ main.ts
@@ -1,39 +1,42 @@
-function minimumSize(nums: number[], maxOperations: number): number {
-    let left: number = 1;
-    let right: number = 0;
+function maxTwoEvents(events: number[][]): number {
+    events.sort((a,b) => a[0] - b[0]);
+    const dp: number[][] = new Array();
+    for (let i = 0; i < events.length; i++) {
+        dp[i] = new Array(3).fill(-1);
+    }
 
-    const isPossible = (
-        maxBallsInBag: number,
-        nums: number[],
-        maxOperations: number
-    ): boolean => {
-        let totalOperations: number = 0;
-
-        for (let num of nums) {
-            let operations = Math.ceil(num / maxBallsInBag) - 1;
-            totalOperations += operations;
-            if (totalOperations > maxOperations) return false;
+    const findEvents = (
+        events: number[][],
+        idx: number,
+        cnt: number,
+        dp: number[][]
+    ): number => {
+        if (cnt === 2 || idx >= events.length) return 0;
+        if (dp[idx][cnt] === -1) {
+            let end: number = events[idx][1];
+            let lo: number = idx + 1;
+            let hi: number = events.length - 1;
+            while(lo < hi) {
+                let mid: number = lo + ((hi - lo) >> 1);
+                if (events[mid][0] > end) hi = mid;
+                else lo = mid + 1;
+            }
+            let include: number = events[idx][2] + (
+                lo < events.length && events[lo][0] > end 
+                ? findEvents(events, lo, cnt + 1, dp) 
+                : 0
+            );
+            let exclude: number = findEvents(events, idx + 1, cnt, dp);
+            dp[idx][cnt] = Math.max(include, exclude);
         }
-
-        return true;
+        return dp[idx][cnt];
     }
 
-    for (let num of nums) {
-        right = Math.max(right, num);
-    }
-
-    while (left < right) {
-        let middle = Math.floor((left + right) / 2);
-        if (isPossible(middle, nums, maxOperations)) {
-            right = middle;
-        } else {
-            left = middle + 1;
-        }
-    }
-
-    console.log(left);
-    return left;
+    let ans = findEvents(events, 0, 0, dp)
+    console.log(ans);
+    return ans;
 };
 
-minimumSize([9],2);
-minimumSize([2,4,8,2],4);
+maxTwoEvents([[1,3,2],[4,5,2],[2,4,3]]);
+maxTwoEvents([[1,3,2],[4,5,2],[1,5,5]]);
+maxTwoEvents([[1,5,3],[1,5,1],[6,6,5]]);
