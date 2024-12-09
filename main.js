b1c8ce3495864773b01aba3e65dbe960fd7f9d31
Jesse_Carter <jessetcarter@hotmail.com>
2024-12-09 14:00:51 -0700

    M167 solved TS

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 "use strict";
-function isArraySpecial(nums, queries) {
-    const answer = new Array();
-    const prefix = new Array(nums.length);
-    prefix[0] = 0;
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] % 2 === nums[i - 1] % 2)
-            prefix[i] = prefix[i - 1] + 1;
+function twoSum(numbers, target) {
+    let leftPtr = 0;
+    let rightPtr = numbers.length - 1;
+    const nextNumber = (ptr) => {
+        while (numbers[ptr] === numbers[ptr + 1]) {
+            ptr++;
+        }
+        return ptr + 1;
+    };
+    while (leftPtr < rightPtr) {
+        let evaluate = numbers[leftPtr] + numbers[rightPtr];
+        if (evaluate === target)
+            break;
+        else if (evaluate < target)
+            leftPtr++;
         else
-            prefix[i] = prefix[i - 1];
+            rightPtr--;
     }
-    console.log(prefix);
-    for (let query of queries) {
-        answer.push(prefix[query[1]] - prefix[query[0]] === 0);
-    }
-    console.log(answer);
-    return answer;
+    // adjust for 1-index reference
+    console.log([leftPtr + 1, rightPtr + 1]);
+    return [leftPtr + 1, rightPtr + 1];
 }
 ;
-isArraySpecial([3, 4, 1, 2, 6], [[0, 4]]);
-isArraySpecial([4, 3, 1, 6], [[0, 2], [2, 3]]);
+twoSum([2, 2, 2, 2, 2, 7, 11, 15], 9);
+twoSum([2, 7, 11, 15], 9);
+twoSum([2, 3, 4], 6);
+twoSum([-1, 0], -1);
--- main.ts
+++ main.ts
@@ -1,22 +1,20 @@
-function isArraySpecial(nums: number[], queries: number[][]): boolean[] {
-    const answer: boolean[] = new Array();
-    const prefix: number[] = new Array(nums.length);
-    prefix[0] = 0;
+function twoSum(numbers: number[], target: number): number[] {
+    let leftPtr: number = 0;
+    let rightPtr: number = numbers.length - 1;
 
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] % 2 === nums[i - 1] % 2) prefix[i] = prefix[i - 1] + 1;
-        else prefix[i] = prefix[i - 1];
+    while (leftPtr < rightPtr) {
+        let evaluate: number = numbers[leftPtr] + numbers[rightPtr];
+
+        if (evaluate === target) break;
+        else if (evaluate < target) leftPtr++;
+        else rightPtr--;
     }
 
-    console.log(prefix);
-
-    for (let query of queries) {
-        answer.push(prefix[query[1]] - prefix[query[0]] === 0);
-    }
-
-    console.log(answer);
-    return answer;
+    // adjust for 1-index reference
+    console.log([leftPtr + 1, rightPtr + 1]);
+    return [leftPtr + 1, rightPtr + 1];
 };
 
-isArraySpecial([3,4,1,2,6],[[0,4]]);
-isArraySpecial([4,3,1,6],[[0,2],[2,3]]);
+twoSum([2,7,11,15],9);
+twoSum([2,3,4],6);
+twoSum([-1,0],-1);
