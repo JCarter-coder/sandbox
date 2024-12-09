b8779f7489d0740efd6b007fa74b31f1b14b6073
Jesse_Carter <jessetcarter@hotmail.com>
2024-12-09 11:01:29 -0700

    M3152 solved TS

--- main.js
+++ main.js
@@ -1,38 +1,21 @@
 "use strict";
-function maxTwoEvents(events) {
-    events.sort((a, b) => a[0] - b[0]);
-    const dp = new Array();
-    for (let i = 0; i < events.length; i++) {
-        dp[i] = new Array(3).fill(-1);
+function isArraySpecial(nums, queries) {
+    const answer = new Array();
+    const prefix = new Array(nums.length);
+    prefix[0] = 0;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] % 2 === nums[i - 1] % 2)
+            prefix[i] = prefix[i - 1] + 1;
+        else
+            prefix[i] = prefix[i - 1];
     }
-    const findEvents = (events, idx, cnt, dp) => {
-        if (cnt === 2 || idx >= events.length)
-            return 0;
-        if (dp[idx][cnt] === -1) {
-            let end = events[idx][1];
-            let lo = idx + 1;
-            let hi = events.length - 1;
-            while (lo < hi) {
-                let mid = lo + ((hi - lo) >> 1);
-                if (events[mid][0] > end)
-                    hi = mid;
-                else
-                    lo = mid + 1;
-            }
-            let include = events[idx][2] + (lo < events.length && events[lo][0] > end
-                ? findEvents(events, lo, cnt + 1, dp)
-                : 0);
-            let exclude = findEvents(events, idx + 1, cnt, dp);
-            dp[idx][cnt] = Math.max(include, exclude);
-        }
-        return dp[idx][cnt];
-    };
-    console.log(dp);
-    let ans = findEvents(events, 0, 0, dp);
-    console.log(ans);
-    return ans;
+    console.log(prefix);
+    for (let query of queries) {
+        answer.push(prefix[query[1]] - prefix[query[0]] === 0);
+    }
+    console.log(answer);
+    return answer;
 }
 ;
-maxTwoEvents([[1, 3, 2], [4, 5, 2], [2, 4, 3]]);
-maxTwoEvents([[1, 3, 2], [4, 5, 2], [1, 5, 5]]);
-maxTwoEvents([[1, 5, 3], [1, 5, 1], [6, 6, 5]]);
+isArraySpecial([3, 4, 1, 2, 6], [[0, 4]]);
+isArraySpecial([4, 3, 1, 6], [[0, 2], [2, 3]]);
--- main.ts
+++ main.ts
@@ -1,42 +1,22 @@
-function maxTwoEvents(events: number[][]): number {
-    events.sort((a,b) => a[0] - b[0]);
-    const dp: number[][] = new Array();
-    for (let i = 0; i < events.length; i++) {
-        dp[i] = new Array(3).fill(-1);
+function isArraySpecial(nums: number[], queries: number[][]): boolean[] {
+    const answer: boolean[] = new Array();
+    const prefix: number[] = new Array(nums.length);
+    prefix[0] = 0;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] % 2 === nums[i - 1] % 2) prefix[i] = prefix[i - 1] + 1;
+        else prefix[i] = prefix[i - 1];
     }
 
-    const findEvents = (
-        events: number[][],
-        idx: number,
-        cnt: number,
-        dp: number[][]
-    ): number => {
-        if (cnt === 2 || idx >= events.length) return 0;
-        if (dp[idx][cnt] === -1) {
-            let end: number = events[idx][1];
-            let lo: number = idx + 1;
-            let hi: number = events.length - 1;
-            while(lo < hi) {
-                let mid: number = lo + ((hi - lo) >> 1);
-                if (events[mid][0] > end) hi = mid;
-                else lo = mid + 1;
-            }
-            let include: number = events[idx][2] + (
-                lo < events.length && events[lo][0] > end 
-                ? findEvents(events, lo, cnt + 1, dp) 
-                : 0
-            );
-            let exclude: number = findEvents(events, idx + 1, cnt, dp);
-            dp[idx][cnt] = Math.max(include, exclude);
-        }
-        return dp[idx][cnt];
+    console.log(prefix);
+
+    for (let query of queries) {
+        answer.push(prefix[query[1]] - prefix[query[0]] === 0);
     }
 
-    let ans = findEvents(events, 0, 0, dp)
-    console.log(ans);
-    return ans;
+    console.log(answer);
+    return answer;
 };
 
-maxTwoEvents([[1,3,2],[4,5,2],[2,4,3]]);
-maxTwoEvents([[1,3,2],[4,5,2],[1,5,5]]);
-maxTwoEvents([[1,5,3],[1,5,1],[6,6,5]]);
+isArraySpecial([3,4,1,2,6],[[0,4]]);
+isArraySpecial([4,3,1,6],[[0,2],[2,3]]);
