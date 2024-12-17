9ce21caa837ccbc1819fca36231441bff8accf11
Jesse_Carter <jessetcarter@hotmail.com>
2024-12-17 09:20:31 -0700

    M2182 solved TS

--- main.ts
+++ main.ts
@@ -1,36 +1,51 @@
-function wordPattern(pattern: string, s: string): boolean {
-    let result: boolean = true;
-    const sArray: string[] = s.split(' ');
+function repeatLimitedString(s: string, repeatLimit: number): string {
+    let N = s.length;
+    const lettersUsed: number[] = new Array(26).fill(0);
+    let result: string = "";
 
-    if (pattern.length !== sArray.length) {
-        result = false;
-        console.log(result);
-        return result;
-    }
-    
-    const mapped: (string | boolean)[] = new Array(26);
-
-    for (let index = 0; index < 26; index++) {
-        mapped[index] = false;
+    for (let i = 0; i < N; i++) {
+        lettersUsed[s.charCodeAt(i) - 97] = (
+            lettersUsed[s.charCodeAt(i) - 97] === 0 ?
+            1 : 
+            lettersUsed[s.charCodeAt(i) - 97] + 1
+        );
     }
 
-    for (let i = 0; i < pattern.length; i++) {
-        if (mapped[pattern.charCodeAt(i) - 97] === false) {
-            if (mapped.find((word) => word === sArray[i])) {
-                result = false;
-                break;
-            } else mapped[pattern.charCodeAt(i) - 97] = sArray[i];
-        } else if (mapped[pattern.charCodeAt(i) - 97] !== sArray[i]) {
-            result = false;
-            break;
+    // build string from lettersUsed, start from end
+    // and track the remaining letters of largest index
+    let currentCharIndex = 25
+
+    while (currentCharIndex >= 0) {
+        if (lettersUsed[currentCharIndex] === 0) {
+            currentCharIndex--;
+            continue;
+        }
+
+        let use = Math.min(lettersUsed[currentCharIndex], repeatLimit);
+
+        for (let k = 0; k < use; k++) {
+            result += String.fromCharCode(currentCharIndex + 97);
+        }
+
+        lettersUsed[currentCharIndex] -= use;
+
+        if (lettersUsed[currentCharIndex] > 0) {
+            let smallerCharIndex = currentCharIndex - 1;
+            while (
+                smallerCharIndex >= 0 &&
+                lettersUsed[smallerCharIndex] === 0
+            ) {
+                smallerCharIndex--;
+            }
+            if (smallerCharIndex < 0) break;
+            result += String.fromCharCode(smallerCharIndex + 97);
+            lettersUsed[smallerCharIndex]--;
         }
     }
 
     console.log(result);
-    return result;
+    return result
 };
 
-wordPattern("abba","dog cat cat dog");
-wordPattern("abba","dog cat cat fish");
-wordPattern("aaaa","dog cat cat dog");
-wordPattern("abba","dog dog dog dog");
+repeatLimitedString("cczazcc",3);
+repeatLimitedString("aababab",2);
