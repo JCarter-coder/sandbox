d1a0b75c12799b8ce868a4eee6f34b058b0c2b48
Jesse_Carter <jessetcarter@hotmail.com>
2024-12-19 14:49:17 -0700

    M769 solved TS

--- main.js
+++ main.js
@@ -1,37 +1,16 @@
 "use strict";
-function maximumLengthOfRanges(nums) {
-    const ans = new Array();
-    let N = nums.length;
-    const left = new Array(N);
-    const right = new Array(N);
-    let idxStack = new Array();
+function maxChunksToSorted(arr) {
+    let N = arr.length;
+    let chunks = 0;
+    let maxElement = 0;
     for (let i = 0; i < N; i++) {
-        while (idxStack.length > 0 &&
-            nums[idxStack[idxStack.length - 1]] < nums[i]) {
-            idxStack.pop();
-        }
-        left[i] = idxStack.length === 0 ? -1 : idxStack[idxStack.length - 1];
-        idxStack.push(i);
+        maxElement = Math.max(maxElement, arr[i]);
+        if (maxElement === i)
+            chunks++;
     }
-    //console.log(idxStack);
-    //console.log(`Left: ${left}`);
-    idxStack = [];
-    for (let i = N - 1; i >= 0; i--) {
-        while (idxStack.length > 0 &&
-            nums[idxStack[idxStack.length - 1]] < nums[i]) {
-            idxStack.pop();
-        }
-        right[i] = idxStack.length === 0 ? N : idxStack[idxStack.length - 1];
-        idxStack.push(i);
-    }
-    for (let i = 0; i < N; i++) {
-        ans[i] = right[i] - left[i] - 1;
-    }
-    //console.log(idxStack);
-    //console.log(`Right: ${right}`);
-    console.log(`Answer: ${ans}`);
-    return ans;
+    console.log(chunks);
+    return chunks;
 }
 ;
-maximumLengthOfRanges([1, 5, 4, 3, 6]);
-maximumLengthOfRanges([1, 2, 3, 4, 5]);
+maxChunksToSorted([4, 3, 2, 1, 0]);
+maxChunksToSorted([1, 0, 2, 3, 4]);
--- main.ts
+++ main.ts
@@ -1,47 +1,17 @@
-function maximumLengthOfRanges(nums: number[]): number[] {
-    const ans: number[] = new Array();
-    let N: number = nums.length;
-    const left: number[] = new Array(N);
-    const right: number[] = new Array(N);
-    let idxStack: number[] = new Array();
+function maxChunksToSorted(arr: number[]): number {
+    let N: number = arr.length;
+    let chunks: number = 0;
+    let maxElement: number = 0;
 
     for (let i = 0; i < N; i++) {
-        while (
-            idxStack.length > 0 &&
-            nums[idxStack[idxStack.length - 1]] < nums[i]
-        ) {
-            idxStack.pop();
-        }
-        left[i] = idxStack.length === 0 ? -1 : idxStack[idxStack.length - 1];
-        idxStack.push(i);
+        maxElement = Math.max(maxElement, arr[i]);
+
+        if (maxElement === i) chunks++;
     }
 
-    //console.log(idxStack);
-    //console.log(`Left: ${left}`);
-
-    idxStack = [];
-
-    for (let i = N - 1; i >= 0; i--) {
-        while (
-            idxStack.length > 0 && 
-            nums[idxStack[idxStack.length - 1]] < nums[i]
-        ) {
-            idxStack.pop();
-        }
-        right[i] = idxStack.length === 0 ? N : idxStack[idxStack.length - 1];
-        idxStack.push(i);
-    }
-
-    for (let i = 0; i < N; i++) {
-        ans[i] = right[i] - left[i] - 1;
-    }
-
-    //console.log(idxStack);
-    //console.log(`Right: ${right}`);
-
-    console.log(`Answer: ${ans}`);
-    return ans;
+    console.log(chunks);
+    return chunks;
 };
 
-maximumLengthOfRanges([1,5,4,3,6]);
-maximumLengthOfRanges([1,2,3,4,5]);
+maxChunksToSorted([4,3,2,1,0]);
+maxChunksToSorted([1,0,2,3,4]);
