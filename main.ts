337a29a2c12704bf14ec0dd1316e6f30876d9682
Jesse_Carter <jessetcarter@hotmail.com>
2024-12-23 14:21:58 -0700

    M2471 solved TS

--- main.ts
+++ main.ts
@@ -1,69 +1,64 @@
-import { stringify } from "querystring";
 
-function leftmostBuildingQueries(heights: number[], queries: number[][]): number[] {
-    const ans: number[] = new Array;
-    const n: number = heights.length;
-    const table = Array.from({ length: n }, () => Array(20).fill(0));
-    const logValues = Array(n + 1).fill(0);
-    logValues[0] = -1;
-
-    for (let i = 1; i <= n; i++) {
-        logValues[i] = logValues[i >> 1] + 1;
+//Definition for a binary tree node.
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
     }
+}
 
-    for (let i = 0; i < n; i++) {
-        table[i][0] = heights[i];
-    }
 
-    for (let i = 1; i < 20; i++) {
-        for (let j = 0; j + (1 << i) <= n; j++) {
-            table[j][i] = Math.max(table[j][i - 1], table[j + (1 << (i - 1))][i - 1]);
-        }
-    }
+function minimumOperations(root: TreeNode | null): number {
+    if (!root) return 0;
 
-    for (let i = 0; i < queries.length; i++) {
-        let [l, r] = queries[i];
-        if (l > r) {
-            [l, r] = [r, l];
-        }
+    const calculateMinSwaps = (arr: number[]): number => {
+        const N = arr.length;
+        const sorted = [...arr].map((val, index) => 
+            [val, index]).sort((a,b) => a[0] - b[0]);
+        const visited = new Array(N).fill(false);
+        let swaps = 0;
 
-        if (l === r) {
-            ans.push(l);
-            continue;
-        }
-
-        if (heights[r] > heights[l]) {
-            ans.push(r);
-            continue;
-        }
-
-        const maxHeight = Math.max(heights[l], heights[r]);
-        let left = r + 1, right = n, mid;
-
-        while (left < right) {
-            mid = Math.floor((left + right) / 2);
-            const k = logValues[mid - r + 1];
-            const maxInRange = Math.max(table[r][k], table[mid - (1 << k) + 1][k]);
-
-            if (maxInRange > maxHeight) {
-                right = mid;
-            } else {
-                left = mid + 1;
+        for (let i = 0; i < N; i++) {
+            if (visited[i] || sorted[i][1] === 1) continue;
+            
+            let cycleSize = 0;
+            let j = i;
+            while (!visited[j]) {
+                visited[j] = true;
+                j = sorted[j][1];
+                cycleSize++;
             }
+            swaps += cycleSize - 1;
         }
 
-        ans.push(left === n ? -1 : left);
+        return swaps;
     }
 
-    console.log(ans);
-    return ans;
+    const queue: TreeNode[] = [root];
+    let totalSwaps = 0;
+
+    while (queue.length > 0) {
+        const levelSize = queue.length;
+        const levelValues: number[] = [];
+
+        for (let i = 0; i < levelSize; i++) {
+            const node = queue.shift()!;
+            levelValues.push(node.val);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+
+        totalSwaps += calculateMinSwaps(levelValues);
+    }
+
+    console.log(totalSwaps);
+    return totalSwaps;
 };
 
-leftmostBuildingQueries(
-    [6,4,8,5,2,7],
-    [[0,1],[0,3],[2,4],[3,4],[2,2]]
-);
-leftmostBuildingQueries(
-    [5,3,8,2,6,1,4,6],
-    [[0,7],[3,5],[5,2],[3,0],[1,6]]
-);
+minimumOperations([1,4,3,7,6,8,5,null,null,null,null,9,null,10]);
+minimumOperations([1,3,2,7,6,5,4]);
+minimumOperations([1,2,3,4,5,6]);
