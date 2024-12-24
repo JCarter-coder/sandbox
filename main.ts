c380d554f24ee6d21f7e3933a19e622f3b8c851e
Jesse_Carter <jessetcarter@hotmail.com>
2024-12-24 11:58:44 -0700

    E20 solved TS

--- main.ts
+++ main.ts
@@ -1,67 +1,22 @@
 function isValid(s: string): boolean {
-    let result: boolean = false;
-    let parentheses = 0;
-    let brackets = 0;
-    let curlyBrackets = 0;
-    const queue: ("(" | "[" | "{") [] = new Array();
-
-    for (let i = 0; i < s.length; i++) {
-        if (s.charAt(i) === "(") {
-            parentheses++;
-            queue.push("(");
-        } else if (s.charAt(i) === ")") {
-            if (queue[queue.length - 1] === "(") {
-                parentheses--;
-                queue.pop();
-            } else {
-                console.log(result);
-                return result;
+    const mappings: { [key: string]: string } = {
+        ")": "(",
+        "}": "{",
+        "]": "[",
+    };
+    const stack: string[] = [];
+    for (let c of s) {
+        if (mappings[c]) {
+            const topElement = stack.length ? stack.pop() : "#";
+            if (topElement !== mappings[c]) {
+                return false;
             }
+        } else {
+            stack.push(c);
         }
-        if (s.charAt(i) === "[") {
-            brackets++;
-            queue.push("[");
-        } else if (s.charAt(i) === "]") {
-            if (queue[queue.length - 1] === "[") {
-                brackets--;
-                queue.pop();
-            } else {
-                console.log(result);
-                return result;
-            }
-        }
-        if (s.charAt(i) === "{") {
-            curlyBrackets++;
-            queue.push("{");
-        } else if (s.charAt(i) === "}") {
-            if (queue[queue.length - 1] === "{") {
-                curlyBrackets--;
-                queue.pop();
-            } else {
-                console.log(result);
-                return result;
-            }
-        }
-        if (
-            parentheses < 0 ||
-            brackets < 0 ||
-            curlyBrackets < 0
-        ) break
     }
 
-    if (
-        parentheses === 0 &&
-        brackets === 0 &&
-        curlyBrackets === 0 &&
-        queue.length === 0
-    ) {
-        result = true;
-        console.log(result);
-        return result;
-    }
-
-    console.log(result);
-    return result;
+    return stack.length === 0;
 };
 
 isValid("()");
