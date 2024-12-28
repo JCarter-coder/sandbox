52bd435eab84aa57693370cfb8d95c1517886920
Jesse_Carter <jessetcarter@hotmail.com>
2024-12-28 16:20:57 -0700

    H689 solved TS

--- main.js
+++ main.js
@@ -1,27 +1,64 @@
 "use strict";
-function summaryRanges(nums) {
-    let left = 0;
-    const result = new Array();
-    while (left < nums.length) {
-        let newRange = nums[left].toString();
-        let stopRange = undefined;
-        let right = left + 1;
-        let increment = nums[left];
-        while (right < nums.length &&
-            nums[right] === increment + 1) {
-            stopRange = nums[right].toString();
-            right++;
-            increment++;
-        }
-        if (stopRange !== undefined) {
-            newRange += "->" + stopRange;
-        }
-        result.push(newRange);
-        left = right;
+function maxSumOfThreeSubarrays(nums, k) {
+    let bestSingleStart = 0;
+    const bestDoubleStart = [0, k];
+    const bestTripleStart = [0, k, k * 2];
+    // compute initial sums of first three subarrays
+    let currentWindowSumSingle = 0;
+    for (let i = 0; i < k; i++) {
+        currentWindowSumSingle += nums[i];
     }
-    console.log(result);
-    return result;
+    let currentWindowSumDouble = 0;
+    for (let i = k; i < k * 2; i++) {
+        currentWindowSumDouble += nums[i];
+    }
+    let currentWindowSumTriple = 0;
+    for (let i = k * 2; i < k * 3; i++) {
+        currentWindowSumTriple += nums[i];
+    }
+    // Track best sums so far
+    let bestSingleSum = currentWindowSumSingle;
+    let bestDoubleSum = (currentWindowSumSingle + currentWindowSumDouble);
+    let bestTripleSum = (currentWindowSumSingle +
+        currentWindowSumDouble +
+        currentWindowSumTriple);
+    // pointers for the subarrays
+    let singleStartIndex = 1;
+    let doubleStartIndex = k + 1;
+    let tripleStartIndex = k * 2 + 1;
+    while (tripleStartIndex <= nums.length - k) {
+        // update the sums using the sliding window technique
+        currentWindowSumSingle = (currentWindowSumSingle -
+            nums[singleStartIndex - 1] +
+            nums[singleStartIndex + k - 1]);
+        currentWindowSumDouble = (currentWindowSumDouble -
+            nums[doubleStartIndex - 1] +
+            nums[doubleStartIndex + k - 1]);
+        currentWindowSumTriple = (currentWindowSumTriple -
+            nums[tripleStartIndex - 1] +
+            nums[tripleStartIndex + k - 1]);
+        if (currentWindowSumSingle > bestSingleSum) {
+            bestSingleStart = singleStartIndex;
+            bestSingleSum = currentWindowSumSingle;
+        }
+        if (currentWindowSumDouble + bestSingleSum > bestDoubleSum) {
+            bestDoubleStart[0] = bestSingleStart;
+            bestDoubleStart[1] = doubleStartIndex;
+            bestDoubleSum = currentWindowSumDouble + bestSingleSum;
+        }
+        if (currentWindowSumTriple + bestDoubleSum > bestTripleSum) {
+            bestTripleStart[0] = bestDoubleStart[0];
+            bestTripleStart[1] = bestDoubleStart[1];
+            bestTripleStart[2] = tripleStartIndex;
+            bestTripleSum = currentWindowSumTriple + bestDoubleSum;
+        }
+        singleStartIndex++;
+        doubleStartIndex++;
+        tripleStartIndex++;
+    }
+    console.log(bestTripleStart);
+    return bestTripleStart;
 }
 ;
-summaryRanges([0, 1, 2, 4, 5, 7]);
-summaryRanges([0, 2, 3, 4, 6, 8, 9]);
+maxSumOfThreeSubarrays([1, 2, 1, 2, 6, 7, 5, 1], 2);
+maxSumOfThreeSubarrays([1, 2, 1, 2, 1, 2, 1, 2, 1], 2);
--- main.ts
+++ main.ts
@@ -1,33 +1,84 @@
-function summaryRanges(nums: number[]): string[] {
-    let left: number = 0;
-    const result: string[] = new Array();
+function maxSumOfThreeSubarrays(nums: number[], k: number): number[] {
+    let bestSingleStart: number = 0;
+    const bestDoubleStart: number[] = [0, k];
+    const bestTripleStart: number[] = [0, k, k*2];
 
-    while (left < nums.length) {
-        let newRange: string = nums[left].toString();
-        let stopRange: string | undefined = undefined;
-        let right: number = left + 1;
-        let increment: number = nums[left];
-
-        while (
-            right < nums.length &&
-            nums[right] === increment + 1
-        ) {
-            stopRange = nums[right].toString();
-            right++;
-            increment++;
-        }
-
-        if (stopRange !== undefined) {
-            newRange += "->" + stopRange;
-        }
-
-        result.push(newRange);
-        left = right;
+    // compute initial sums of first three subarrays
+    let currentWindowSumSingle: number = 0;
+    for (let i = 0; i < k; i++) {
+        currentWindowSumSingle += nums[i];
     }
 
-    console.log(result);
-    return result;
+    let currentWindowSumDouble: number = 0;
+    for (let i = k; i < k * 2; i++) {
+        currentWindowSumDouble += nums[i];
+    }
+
+    let currentWindowSumTriple: number = 0;
+    for (let i = k * 2; i < k * 3; i++) {
+        currentWindowSumTriple += nums[i];
+    }
+
+    // Track best sums so far
+    let bestSingleSum = currentWindowSumSingle;
+    let bestDoubleSum = (
+        currentWindowSumSingle + currentWindowSumDouble
+    );
+    let bestTripleSum = (
+        currentWindowSumSingle + 
+        currentWindowSumDouble +
+        currentWindowSumTriple
+    );
+
+    // pointers for the subarrays
+    let singleStartIndex: number = 1;
+    let doubleStartIndex: number = k + 1;
+    let tripleStartIndex: number = k * 2 + 1;
+
+    while (tripleStartIndex <= nums.length - k) {
+        // update the sums using the sliding window technique
+        currentWindowSumSingle = (
+            currentWindowSumSingle - 
+            nums[singleStartIndex - 1] +
+            nums[singleStartIndex + k - 1]
+        );
+        currentWindowSumDouble = (
+            currentWindowSumDouble -
+            nums[doubleStartIndex - 1] +
+            nums[doubleStartIndex + k - 1]
+        );
+        currentWindowSumTriple = (
+            currentWindowSumTriple - 
+            nums[tripleStartIndex - 1] +
+            nums[tripleStartIndex + k - 1]
+        );
+
+        if (currentWindowSumSingle > bestSingleSum) {
+            bestSingleStart = singleStartIndex;
+            bestSingleSum = currentWindowSumSingle;
+        }
+
+        if (currentWindowSumDouble + bestSingleSum > bestDoubleSum) {
+            bestDoubleStart[0] = bestSingleStart;
+            bestDoubleStart[1] = doubleStartIndex;
+            bestDoubleSum = currentWindowSumDouble + bestSingleSum;
+        }
+
+        if (currentWindowSumTriple + bestDoubleSum > bestTripleSum) {
+            bestTripleStart[0] = bestDoubleStart[0];
+            bestTripleStart[1] = bestDoubleStart[1];
+            bestTripleStart[2] = tripleStartIndex;
+            bestTripleSum = currentWindowSumTriple + bestDoubleSum;
+        }
+
+        singleStartIndex++;
+        doubleStartIndex++;
+        tripleStartIndex++;
+    }
+
+    console.log(bestTripleStart);
+    return bestTripleStart;
 };
 
-summaryRanges([0,1,2,4,5,7]);
-summaryRanges([0,2,3,4,6,8,9]);
+maxSumOfThreeSubarrays([1,2,1,2,6,7,5,1],2);
+maxSumOfThreeSubarrays([1,2,1,2,1,2,1,2,1],2);
