73a3ad90d9b30f1c3448db7d0bd3d1deae62f2a0
Jesse_Carter <jessetcarter@hotmail.com>
2024-12-30 15:20:29 -0700

    M2466 solved TS

--- main.ts
+++ main.ts
@@ -1,48 +1,27 @@
-function numWays(words: string[], target: string): number {
-    let wordLength: number = words[0].length;
-    let targetLength: number = target.length;
+function countGoodStrings(low: number, high: number, zero: number, one: number): number {
     const MOD: number = 1000000007;
+    const dp = new Array(high + 1).fill(0);
+    dp[0] = 1;
 
-    // Step 1: Calculate freq of each char at every "words" index
-    const charFrequency: number[][] = new Array(wordLength);
-    for (let i = 0; i < charFrequency.length; i++) {
-        charFrequency[i] = new Array(26).fill(0);
-    }
-    for (let word of words) {
-        for (let j = 0; j < wordLength; ++j) {
-            charFrequency[j][word.charCodeAt(j) - 97]++;
+    for (let end = 1; end <= high; ++end) {
+        if (end >= zero) {
+            dp[end] += dp[end - zero];
         }
-    }
-
-    // Step 2: Initialize a dp table
-    const dp = new Array(wordLength + 1);
-    for (let i = 0; i < dp.length; i++) {
-        dp[i] = new Array(targetLength + 1).fill(0);
-    }
-
-    // Base case: one way to form an empty target string
-    for (let currWord = 0; currWord <= wordLength; ++currWord) {
-        dp[currWord][0] = 1;
-    }
-
-    // Step 3: Fill the dp table
-    for (let currWord = 1; currWord <= wordLength; ++currWord) {
-        for (let currTarget = 1; currTarget <= targetLength; ++currTarget) {
-            dp[currWord][currTarget] = dp[currWord - 1][currTarget];
-
-            let currPos: number = target.charCodeAt(currTarget - 1) - 97;
-            dp[currWord][currTarget] += (
-                charFrequency[currWord - 1][currPos] *
-                dp[currWord - 1][currTarget - 1]
-            ) % MOD;
-            dp[currWord][currTarget] %= MOD;
+        if (end >= one) {
+            dp[end] += dp[end - one];
         }
+        dp[end] %= MOD;
     }
 
-    // Step 4: The result is in dp[wordLength][targetLength]
-    console.log(dp[wordLength][targetLength]);
-    return dp[wordLength][targetLength];
+    let result = 0;
+    for (let i = low; i <= high; ++i) {
+        result += dp[i];
+        result %= MOD;
+    }
+
+    console.log(result);
+    return result;
 };
 
-numWays(["acca","bbbb","caca"],"aba");
-numWays(["abba","baab"],"bab");
+countGoodStrings(3,3,1,1);
+countGoodStrings(2,3,1,2);
