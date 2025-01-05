a7aeb4fe1adac810753976748f0b6c99225b3725
Jesse_Carter <jessetcarter@hotmail.com>
2025-01-05 16:18:58 -0700

    M2381 solved TS

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
 "use strict";
-function countPalindromicSubsequence(s) {
-    const first = new Array(26).fill(-1);
-    const last = new Array(26);
-    for (let i = 0; i < s.length; i++) {
-        let curr = s.charCodeAt(i) - 97;
-        if (first[curr] === -1)
-            first[curr] = i;
-        last[curr] = i;
-    }
-    let ans = 0;
-    for (let i = 0; i < 26; i++) {
-        if (first[i] === -1)
-            continue;
-        const between = new Set();
-        for (let j = first[i] + 1; j < last[i]; j++) {
-            between.add(s.charAt(j));
+function shiftingLetters(s, shifts) {
+    const N = s.length;
+    let finalShifts = new Array(N).fill(0);
+    for (let shift of shifts) {
+        if (shift[2] === 1) {
+            finalShifts[shift[0]]++;
+            if (shift[1] + 1 < N)
+                finalShifts[shift[1] + 1]--;
         }
-        ans += between.size;
+        else {
+            finalShifts[shift[0]]--;
+            if (shift[1] + 1 < N)
+                finalShifts[shift[1] + 1]++;
+        }
     }
-    console.log(ans);
-    return ans;
+    //console.log(finalShifts);
+    let result = "";
+    let numberOfShifts = 0;
+    for (let i = 0; i < N; i++) {
+        numberOfShifts = (numberOfShifts + finalShifts[i]) % 26;
+        if (numberOfShifts < 0)
+            numberOfShifts += 26;
+        let shiftedCharacter = String.fromCharCode(97 + (s.charCodeAt(i) - 97 + numberOfShifts) % 26);
+        result += shiftedCharacter;
+    }
+    console.log(result);
+    return result;
 }
 ;
-countPalindromicSubsequence("aabca");
-countPalindromicSubsequence("adc");
-countPalindromicSubsequence("bbcbaba");
+shiftingLetters("abc", [[0, 1, 0], [1, 2, 1], [0, 2, 1]]);
+shiftingLetters("dztz", [[0, 0, 0], [1, 1, 1]]);
--- main.ts
+++ main.ts
@@ -1,29 +1,34 @@
-function countPalindromicSubsequence(s: string): number {
-    const first: number[] = new Array(26).fill(-1);
-    const last: number[] = new Array(26);
+function shiftingLetters(s: string, shifts: number[][]): string {
+    const N: number = s.length;
+    let finalShifts: number[] = new Array(N).fill(0);
 
-    for (let i = 0; i < s.length; i++) {
-        let curr = s.charCodeAt(i) - 97;
-        if (first[curr] === -1) first[curr] = i;
-
-        last[curr] = i;
-    }
-
-    let ans: number = 0;
-    for (let i = 0; i < 26; i++) {
-        if (first[i] === -1) continue;
-
-        const between = new Set();
-        for (let j = first[i] + 1; j < last[i]; j++) {
-            between.add(s.charAt(j));
+    for (let shift of shifts) {
+        if (shift[2] === 1) {
+            finalShifts[shift[0]]++;
+            if (shift[1] + 1 < N) finalShifts[shift[1] + 1]--;
+        } else {
+            finalShifts[shift[0]]--;
+            if (shift[1] + 1 < N) finalShifts[shift[1] + 1]++;
         }
-        ans += between.size;
     }
-    
-    console.log(ans);
-    return ans;
+
+    //console.log(finalShifts);
+    let result: string = "";
+    let numberOfShifts = 0;
+
+    for (let i = 0; i < N; i++) {
+        numberOfShifts = (numberOfShifts + finalShifts[i]) % 26;
+        if (numberOfShifts < 0) numberOfShifts += 26;
+
+        let shiftedCharacter = String.fromCharCode(
+            97 + (s.charCodeAt(i) - 97 + numberOfShifts) % 26
+        );
+        result += shiftedCharacter;
+    }
+
+    console.log(result);
+    return result;
 };
 
-countPalindromicSubsequence("aabca");
-countPalindromicSubsequence("adc");
-countPalindromicSubsequence("bbcbaba");
+shiftingLetters("abc",[[0,1,0],[1,2,1],[0,2,1]]);
+shiftingLetters("dztz",[[0,0,0],[1,1,1]]);
