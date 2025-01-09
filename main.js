1200e43cbf8c98be7b1e3847a7aa532406f6a798
Jesse_Carter <jessetcarter@hotmail.com>
2025-01-09 12:06:48 -0700

    E2185 solved TS

--- main.js
+++ main.js
@@ -1,46 +1,14 @@
-function simplifyPath(path) {
-    const N = path.length;
-    let canonicalPath = "/";
-    let leftPtr = 1;
-    let rightPtr = 1;
-    const directoryStack = new Array();
-    while (leftPtr < N) {
-        //console.log(leftPtr);
-        if (path.charAt(leftPtr) === '/')
-            leftPtr++;
-        else {
-            rightPtr = leftPtr + 1;
-            while (path.charAt(rightPtr) !== '/' && rightPtr < N) {
-                rightPtr++;
-            }
-            //console.log(`Right: ${rightPtr}`);
-            //console.log(path.substring(leftPtr, rightPtr))
-            directoryStack.push(path.substring(leftPtr, rightPtr));
-            leftPtr = rightPtr + 1;
-            //console.log(`Left: ${leftPtr}`);
-        }
+"use strict";
+function prefixCount(words, pref) {
+    let result = 0;
+    let prefLength = pref.length;
+    for (let word of words) {
+        if (word.substring(0, prefLength) === pref)
+            result++;
     }
-    const canonicalStack = new Array();
-    ;
-    let index = 0;
-    while (index < directoryStack.length) {
-        if (directoryStack[index] === "..") {
-            canonicalStack.pop();
-        }
-        else if (directoryStack[index] !== ".") {
-            canonicalStack.push(directoryStack[index]);
-        }
-        index++;
-    }
-    //console.log(directoryStack);
-    canonicalPath += canonicalStack.join('/');
-    console.log(canonicalPath);
-    return canonicalPath;
+    console.log(result);
+    return result;
 }
 ;
-simplifyPath("/home/");
-simplifyPath("/home//foo/");
-simplifyPath("/home/user/Documents/../Pictures");
-simplifyPath("/../");
-simplifyPath("/.../a/../b/c/../d/./");
-export {};
+prefixCount(["pay", "attention", "practice", "attend"], "at");
+prefixCount(["leetcode", "win", "loops", "success"], "code");
--- main.ts
+++ main.ts
@@ -1,49 +1,14 @@
-import { dir } from "console";
+function prefixCount(words: string[], pref: string): number {
+    let result: number = 0;
+    let prefLength = pref.length;
 
-function simplifyPath(path: string): string {
-    const N: number = path.length;
-    let canonicalPath: string = "/";
-    let leftPtr: number = 1;
-    let rightPtr: number = 1;
-    const directoryStack: string[] = new Array();
-
-    while (leftPtr < N) {
-        //console.log(leftPtr);
-        if (path.charAt(leftPtr) === '/') leftPtr++;
-        else {
-            rightPtr = leftPtr + 1;
-            while (path.charAt(rightPtr) !== '/' && rightPtr < N) {
-                rightPtr++;
-            }
-            //console.log(`Right: ${rightPtr}`);
-            //console.log(path.substring(leftPtr, rightPtr))
-            directoryStack.push(path.substring(leftPtr, rightPtr));
-            leftPtr = rightPtr + 1;
-            //console.log(`Left: ${leftPtr}`);
-        }
+    for (let word of words) {
+        if (word.substring(0, prefLength) === pref) result++;
     }
 
-    const canonicalStack: string[] = new Array();;
-    let index: number = 0;
-
-    while (index < directoryStack.length) {
-        if (directoryStack[index] === "..") {
-            canonicalStack.pop();
-        } else if (directoryStack[index] !== ".") {
-            canonicalStack.push(directoryStack[index]);
-        }
-        index++;
-    }
-
-    //console.log(directoryStack);
-    canonicalPath += canonicalStack.join('/');
-
-    console.log(canonicalPath);
-    return canonicalPath;
+    console.log(result);
+    return result;
 };
 
-simplifyPath("/home/");
-simplifyPath("/home//foo/");
-simplifyPath("/home/user/Documents/../Pictures");
-simplifyPath("/../");
-simplifyPath("/.../a/../b/c/../d/./");
+prefixCount(["pay","attention","practice","attend"],"at");
+prefixCount(["leetcode","win","loops","success"],"code");
