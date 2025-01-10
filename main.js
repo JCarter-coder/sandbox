3ba4e77da07a17453e7b798c1301eb63a2da6d6f
Jesse_Carter <jessetcarter@hotmail.com>
2025-01-10 16:43:50 -0700

    M916 solved TS

--- main.js
+++ main.js
@@ -1,14 +1,59 @@
 "use strict";
-function prefixCount(words, pref) {
-    let result = 0;
-    let prefLength = pref.length;
-    for (let word of words) {
-        if (word.substring(0, prefLength) === pref)
-            result++;
+function wordSubsets(words1, words2) {
+    /* const subsetChars: number[] = new Array(26);
+    const result = new Array();
+
+    for (let word of words2) {
+        const wordCharSet: letterCount = {};
+        for (let char of word) {
+            !wordCharSet[char] ? wordCharSet[char] = 1 : wordCharSet[char]++;
+        }
+        for (const [key, value] of Object.entries(wordCharSet)) {
+            let char = key.charCodeAt(0) - 97;
+            subsetChars[char] = Math.max(subsetChars[char] || 0, value);
+        }
     }
-    console.log(result);
-    return result;
+
+    for (let word of words1) {
+        const tempSet = subsetChars.slice();
+        for (let char of word) {
+            tempSet[char.charCodeAt(0) - 97]--;
+            if (tempSet[char.charCodeAt(0) - 97] < 0) break;
+        }
+        let isSetLengthZero = tempSet.filter((element) => (
+            !Number.isNaN(element)
+        ));
+        //if (isSetLengthZero.length === 0) result.push(word);
+        console.log(`${word}: ${isSetLengthZero.length}`);
+        console.log(tempSet);
+    } */
+    //console.log(subsetChars);
+    const count = (S) => {
+        const ans = new Array(26).fill(0);
+        for (let c of S) {
+            ans[c.charCodeAt(0) - 97]++;
+        }
+        return ans;
+    };
+    const bmax = count("");
+    for (let b of words2) {
+        let bCount = count(b);
+        for (let i = 0; i < 26; i++) {
+            bmax[i] = Math.max(bmax[i], bCount[i]);
+        }
+    }
+    const ans = new Array();
+    search: for (let a of words1) {
+        const aCount = count(a);
+        for (let i = 0; i < 26; i++) {
+            if (aCount[i] < bmax[i])
+                continue search;
+        }
+        ans.push(a);
+    }
+    console.log(ans);
+    return ans;
 }
 ;
-prefixCount(["pay", "attention", "practice", "attend"], "at");
-prefixCount(["leetcode", "win", "loops", "success"], "code");
+wordSubsets(["amazon", "apple", "facebook", "google", "leetcode"], ["e", "o"]);
+wordSubsets(["amazon", "apple", "facebook", "google", "leetcode"], ["l", "e"]);
--- main.ts
+++ main.ts
@@ -1,14 +1,38 @@
-function prefixCount(words: string[], pref: string): number {
-    let result: number = 0;
-    let prefLength = pref.length;
+type letterCount = {
+    [key: string]: number;
+}
 
-    for (let word of words) {
-        if (word.substring(0, prefLength) === pref) result++;
+function wordSubsets(words1: string[], words2: string[]): string[] {
+    const count = (S: string): number[] => {
+        const ans = new Array(26).fill(0);
+        for (let c of S) {
+            ans[c.charCodeAt(0) - 97]++;
+        }
+        return ans;
     }
 
-    console.log(result);
-    return result;
+    const bmax = count("");
+    
+    for (let b of words2) {
+        let bCount = count(b);
+        for (let i = 0; i < 26; i++) {
+            bmax[i] = Math.max(bmax[i], bCount[i]);
+        }
+    }
+
+    const ans = new Array();
+
+    search: for (let a of words1) {
+        const aCount = count(a);
+        for (let i = 0; i < 26; i++) {
+            if (aCount[i] < bmax[i]) continue search;
+        }
+        ans.push(a);
+    }
+
+    console.log(ans);
+    return ans;
 };
 
-prefixCount(["pay","attention","practice","attend"],"at");
-prefixCount(["leetcode","win","loops","success"],"code");
+wordSubsets(["amazon","apple","facebook","google","leetcode"],["e","o"]);
+wordSubsets(["amazon","apple","facebook","google","leetcode"],["l","e"]);
