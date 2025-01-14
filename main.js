d7589666aef7b7b14ee38310adffbdc5bc94fc55
Jesse_Carter <jessetcarter@hotmail.com>
2025-01-14 11:44:36 -0700

    M2657 solved TS

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 "use strict";
 function findThePrefixCommonArray(A, B) {
     const N = A.length;
-    const arrayA = new Array(N + 1).fill(0);
+    const freq = new Array(N + 1).fill(0);
     const arrayB = new Array(N + 1).fill(0);
     const result = new Array();
     let count = 0;
     for (let i = 0; i < N; i++) {
-        arrayA[A[i]] = 1;
-        arrayB[B[i]] = 1;
-        //console.log(arrayA);
-        //console.log(arrayB);
-        if (arrayA[A[i]] === arrayB[A[i]])
+        freq[A[i]]++;
+        freq[B[i]]++;
+        console.log(freq);
+        if (freq[A[i]] === 2)
             count++;
-        if (arrayB[B[i]] === arrayA[B[i]] &&
+        if (freq[B[i]] === 2 &&
             B[i] !== A[i])
             count++;
         result.push(count);
@@ -21,5 +20,5 @@ function findThePrefixCommonArray(A, B) {
     return result;
 }
 ;
-findThePrefixCommonArray([1, 3, 2, 4], [3, 1, 2, 4]);
-findThePrefixCommonArray([2, 3, 1], [3, 1, 2]);
+findThePrefixCommonArray([1, 3, 2, 4], [3, 1, 2, 4]); // [0,2,3,4]
+findThePrefixCommonArray([2, 3, 1], [3, 1, 2]); // [0,1,3]
--- main.ts
+++ main.ts
@@ -1,20 +1,18 @@
 function findThePrefixCommonArray(A: number[], B: number[]): number[] {
     const N: number = A.length;
-    const arrayA: number[] = new Array(N+1).fill(0);
-    const arrayB: number[] = new Array(N+1).fill(0);
+    const freq: number[] = new Array(N+1).fill(0);
 
     const result: number[] = new Array();
     let count: number = 0;
     
     for (let i = 0; i < N; i++) {
-        arrayA[A[i]] = 1;
-        arrayB[B[i]] = 1;
-        //console.log(arrayA);
-        //console.log(arrayB);
+        freq[A[i]]++;
+        freq[B[i]]++;
+        console.log(freq);
 
-        if (arrayA[A[i]] === arrayB[A[i]]) count++;
+        if (freq[A[i]] === 2) count++;
         if (
-            arrayB[B[i]] === arrayA[B[i]] &&
+            freq[B[i]] === 2 &&
             B[i] !== A[i]
         ) count++;
         result.push(count);
@@ -24,5 +22,5 @@ function findThePrefixCommonArray(A: number[], B: number[]): number[] {
     return result;
 };
 
-findThePrefixCommonArray([1,3,2,4],[3,1,2,4]);
-findThePrefixCommonArray([2,3,1],[3,1,2]);
+findThePrefixCommonArray([1,3,2,4],[3,1,2,4]); // [0,2,3,4]
+findThePrefixCommonArray([2,3,1],[3,1,2]); // [0,1,3]
