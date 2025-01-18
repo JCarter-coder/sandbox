a21fb6fa75bb31057529fc5488348f2a2d943bb5
Jesse_Carter <jessetcarter@hotmail.com>
2025-01-18 10:54:46 -0700

    H1368 solved TS

--- main.js
+++ main.js
@@ -1,13 +1,44 @@
 "use strict";
-function doesValidArrayExist(derived) {
-    let XOR = 0;
-    for (let element of derived) {
-        XOR ^= element;
+function minCost(grid) {
+    const dirs = [
+        [0, 1],
+        [0, -1],
+        [1, 0],
+        [-1, 0]
+    ];
+    let m = grid.length;
+    let n = grid[0].length;
+    const cost = Array.from({ length: m }, () => Array(n).fill(Infinity));
+    cost[0][0] = 0;
+    const deque = [];
+    deque.push({ x: 0, y: 0, c: 0 });
+    while (deque.length > 0) {
+        const { x, y, c } = deque.shift();
+        for (let i = 0; i < 4; i++) {
+            let dx = dirs[i][0];
+            let dy = dirs[i][1];
+            const nx = x + dx;
+            const ny = y + dy;
+            if (nx < 0 || nx >= m || ny < 0 || ny >= n) {
+                continue;
+            }
+            const newCost = c + (grid[x][y] === i + 1 ? 0 : 1);
+            if (newCost >= cost[nx][ny]) {
+                continue;
+            }
+            cost[nx][ny] = newCost;
+            if (grid[x][y] === i + 1) {
+                deque.unshift({ x: nx, y: ny, c: newCost });
+            }
+            else {
+                deque.push({ x: nx, y: ny, c: newCost });
+            }
+        }
     }
-    console.log(XOR === 0);
-    return XOR === 0;
+    console.log(cost[m - 1][n - 1]);
+    return cost[m - 1][n - 1];
 }
 ;
-doesValidArrayExist([1, 1, 0]);
-doesValidArrayExist([1, 1]);
-doesValidArrayExist([1, 0]);
+minCost([[1, 1, 1, 1], [2, 2, 2, 2], [1, 1, 1, 1], [2, 2, 2, 2]]);
+minCost([[1, 1, 3], [3, 2, 2], [1, 1, 4]]);
+minCost([[1, 2], [4, 3]]);
