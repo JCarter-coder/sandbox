16d598e43483795fc29badd184c31c79403eabfb
Jesse_Carter <jessetcarter@hotmail.com>
2025-01-20 12:53:34 -0700

    M1730 solved TS

--- main.js
+++ main.js
@@ -1,44 +1,62 @@
 "use strict";
-function trapRainWater(heightMap) {
-    const N = heightMap.length;
-    const M = heightMap[0].length;
-    const pq = new Array();
-    const vis = Array.from({ length: N }, () => Array(M).fill(0));
-    for (let i = 0; i < M; i++) {
-        pq.push([heightMap[0][i], [0, i]]);
-        pq.push([heightMap[N - 1][i], [N - 1, i]]);
-        vis[0][i] = 1;
-        vis[N - 1][i] = 1;
-    }
-    for (let i = 1; i < N - 1; i++) {
-        pq.push([heightMap[i][0], [i, 0]]);
-        pq.push([heightMap[i][M - 1], [i, M - 1]]);
-        vis[i][0] = 1;
-        vis[i][M - 1] = 1;
-    }
-    const delRow = [-1, 0, 1, 0];
-    const delCol = [0, 1, 0, -1];
-    let ans = 0;
-    while (pq.length > 0) {
-        pq.sort((a, b) => a[0] - b[0]);
-        const [height, [row, col]] = pq.shift();
-        for (let i = 0; i < 4; i++) {
-            const nRow = row + delRow[i];
-            const nCol = col + delCol[i];
-            if (nRow >= 0 &&
-                nCol >= 0 &&
-                nRow < N &&
-                nCol < M &&
-                !vis[nRow][nCol]) {
-                ans += Math.max(height - heightMap[nRow][nCol], 0);
-                pq.push([Math.max(height, heightMap[nRow][nCol]), [nRow, nCol]]);
-                vis[nRow][nCol] = 1;
+function getFood(grid) {
+    const DIRECTIONS = [
+        [0, 1],
+        [0, -1],
+        [-1, 0],
+        [1, 0]
+    ];
+    const ROWS = grid.length;
+    const COLS = grid[0].length;
+    const isValid = (x, y) => {
+        return (x >= 0 &&
+            x < ROWS &&
+            y >= 0 &&
+            y < COLS &&
+            grid[x][y] !== 'X');
+    };
+    const bfs = (row, col) => {
+        const queue = [[row, col, 0]];
+        while (queue.length !== 0) {
+            const [x, y, d] = queue.shift();
+            if (!isValid(x, y))
+                continue;
+            else if (grid[x][y] === '#')
+                return d;
+            grid[x][y] = 'X';
+            for (let i = 0; i < 4; i++) {
+                const new_x = x + DIRECTIONS[i][0];
+                const new_y = y + DIRECTIONS[i][1];
+                queue.push([new_x, new_y, d + 1]);
             }
         }
+        return -1;
+    };
+    for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLS; j++) {
+            if (grid[i][j] === '*')
+                return bfs(i, j);
+        }
     }
-    console.log(ans);
-    return ans;
+    return -1;
 }
 ;
-trapRainWater([[1, 4, 3, 1, 3, 2], [3, 2, 1, 3, 2, 4], [2, 3, 3, 2, 3, 1]]);
-trapRainWater([[3, 3, 3, 3, 3], [3, 2, 2, 2, 3], [3, 2, 1, 2, 3], [3, 2, 2, 2, 3], [3, 3, 3, 3, 3]]);
+getFood([
+    ["X", "X", "X", "X", "X", "X"],
+    ["X", "*", "O", "O", "O", "X"],
+    ["X", "O", "O", "#", "O", "X"],
+    ["X", "X", "X", "X", "X", "X"]
+]);
+getFood([
+    ["X", "X", "X", "X", "X"],
+    ["X", "*", "X", "O", "X"],
+    ["X", "O", "X", "#", "X"],
+    ["X", "X", "X", "X", "X"]
+]);
+getFood([
+    ["X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "*", "O", "X", "O", "#", "O", "X"],
+    ["X", "O", "O", "X", "O", "O", "X", "X"],
+    ["X", "O", "O", "O", "O", "#", "O", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X"]
+]);
