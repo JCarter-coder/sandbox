7606e51885f5cfeeb08d87a5b5184a8ce296c96f
Jesse_Carter <jessetcarter@hotmail.com>
2025-01-30 16:49:30 -0700

    M261 solved TS

--- main.js
+++ main.js
@@ -1,34 +1,58 @@
 "use strict";
-function findMaxFish(grid) {
-    const ROWS = grid.length;
-    const COLS = grid[0].length;
-    const visited = Array.from({ length: ROWS }, () => Array(COLS).fill(false));
-    const calculateFishes = (grid, visited, row, col) => {
-        if (row < 0 ||
-            row >= grid.length ||
-            col < 0 ||
-            col >= grid[0].length ||
-            grid[row][col] === 0 ||
-            visited[row][col])
-            return 0;
-        visited[row][col] = true;
-        return (grid[row][col] +
-            calculateFishes(grid, visited, row, col + 1) +
-            calculateFishes(grid, visited, row, col - 1) +
-            calculateFishes(grid, visited, row + 1, col) +
-            calculateFishes(grid, visited, row - 1, col));
-    };
-    let maxFishCount = 0;
-    for (let row = 0; row < ROWS; row++) {
-        for (let col = 0; col < COLS; col++) {
-            if (grid[row][col] > 0 && !visited[row][col]) {
-                maxFishCount = Math.max(maxFishCount, calculateFishes(grid, visited, row, col));
-            }
+class UnionFind {
+    constructor(n) {
+        this.parent = new Array(n);
+        this.size = new Array(n);
+        for (let node = 0; node < n; node++) {
+            this.parent[node] = node;
+            this.size[node] = 1;
         }
     }
-    console.log(maxFishCount);
-    return maxFishCount;
+    find(A) {
+        let root = A;
+        while (this.parent[root] !== root) {
+            root = this.parent[root];
+        }
+        while (A !== root) {
+            let oldRoot = this.parent[A];
+            this.parent[A] = root;
+            A = oldRoot;
+        }
+        return root;
+    }
+    union(A, B) {
+        let rootA = this.find(A);
+        let rootB = this.find(B);
+        if (rootA === rootB)
+            return false;
+        if (this.size[rootA] < this.size[rootB]) {
+            this.parent[rootA] = rootB;
+            this.size[rootB] += this.size[rootA];
+        }
+        else {
+            this.parent[rootB] = rootA;
+            this.size[rootA] += this.size[rootB];
+        }
+        return true;
+    }
+}
+function validTree(n, edges) {
+    if (edges.length !== n - 1) {
+        console.log(false);
+        return false;
+    }
+    const unionFind = new UnionFind(n);
+    for (let edge of edges) {
+        let A = edge[0];
+        let B = edge[1];
+        if (!unionFind.union(A, B)) {
+            console.log(false);
+            return false;
+        }
+    }
+    console.log(true);
+    return true;
 }
 ;
-findMaxFish([[0, 2, 1, 0], [4, 0, 0, 3], [1, 0, 0, 4], [0, 3, 2, 0]]);
-findMaxFish([[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1]]);
+validTree(5, [[0, 1], [0, 2], [0, 3], [1, 4]]);
+validTree(5, [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]]);
