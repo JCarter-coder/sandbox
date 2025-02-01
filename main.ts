b4df8f579277ebd044800575a705be3e64eeb9f4
Jesse_Carter <jessetcarter@hotmail.com>
2025-02-01 16:26:25 -0700

    E3151 solved TS

--- main.ts
+++ main.ts
@@ -1,114 +1,22 @@
-function largestIsland(grid: number[][]): number {
-    const exploreIsland = (
-        grid: number[][],
-        islandId: number,
-        currentRow: number,
-        currentColumn: number
-    ): number => {
-        if (
-            currentRow < 0 ||
-            currentRow >= grid.length ||
-            currentColumn < 0 ||
-            currentColumn >= grid[0].length ||
-            grid[currentRow][currentColumn] !== 1
-        ) return 0;
-
-        grid[currentRow][currentColumn] = islandId;
-        return (
-            1 +
-            exploreIsland(grid, islandId, currentRow + 1, currentColumn) +
-            exploreIsland(grid, islandId, currentRow - 1, currentColumn) +
-            exploreIsland(grid, islandId, currentRow, currentColumn + 1) +
-            exploreIsland(grid, islandId, currentRow, currentColumn - 1)
-        );
+function isArraySpecial(nums: number[]): boolean {
+    if (nums.length === 1) {
+        console.log(true);
+        return true;
     }
 
-    const islandSizes = new Map();
-    let islandId = 2;
+    let parityChanged: boolean = true;
 
-    for (let currentRow = 0; currentRow < grid.length; currentRow++) {
-        for (let currentColumn = 0; currentColumn < grid[0].length; currentColumn++) {
-            if (grid[currentRow][currentColumn] === 1) {
-                islandSizes.set(
-                    islandId,
-                    exploreIsland(grid, islandId, currentRow, currentColumn)
-                );
-                islandId++;
-            }
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] % 2 === nums[i - 1] % 2) {
+            parityChanged = false;
+            break;
         }
     }
 
-    if (islandSizes.size === 0) {
-        console.log(1);
-        return 1;
-    }
-
-    if (islandSizes.size === 1) {
-        islandId--;
-        return (
-            (islandSizes.get(islandId) === grid.length * grid[0].length) ?
-            islandSizes.get(islandId) :
-            islandSizes.get(islandId) + 1
-        );
-    }
-
-    let maxIslandSize = 1;
-
-    for (let currentRow = 0; currentRow < grid.length; currentRow++) {
-        for (let currentColumn = 0; currentColumn < grid[0].length; currentColumn++) {
-            if (grid[currentRow][currentColumn] === 0) {
-                let currentIslandSize = 1;
-                const neighboringIslands = new Set();
-
-                if (
-                    currentRow + 1 < grid.length &&
-                    grid[currentRow + 1][currentColumn] > 1
-                ) {
-                    neighboringIslands.add(
-                        grid[currentRow + 1][currentColumn]
-                    );
-                }
-
-                if (
-                    currentRow - 1 >= 0 &&
-                    grid[currentRow - 1][currentColumn] > 1
-                ) {
-                    neighboringIslands.add(
-                        grid[currentRow - 1][currentColumn]
-                    );
-                }
-
-                if (
-                    currentColumn + 1 < grid[0].length &&
-                    grid[currentRow][currentColumn + 1] > 1
-                ) {
-                    neighboringIslands.add(
-                        grid[currentRow][currentColumn + 1]
-                    );
-                }
-
-                if (
-                    currentColumn - 1 >= 0 &&
-                    grid[currentRow][currentColumn - 1]
-                ) {
-                    neighboringIslands.add(
-                        grid[currentRow][currentColumn - 1]
-                    );
-                }
-
-                for (let id of neighboringIslands) {
-                    currentIslandSize += islandSizes.get(id);
-                }
-
-                maxIslandSize = Math.max(maxIslandSize, currentIslandSize);
-            }
-        }
-    }
-
-    console.log(maxIslandSize);
-    return maxIslandSize;
+    console.log(parityChanged);
+    return parityChanged
 };
 
-largestIsland([[1,0],[0,1]]);
-largestIsland([[1,1],[1,0]]);
-largestIsland([[1,1],[1,1]]);
+isArraySpecial([1]);
+isArraySpecial([2,1,4]);
+isArraySpecial([4,3,1,6]);
