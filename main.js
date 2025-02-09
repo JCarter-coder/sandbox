8f2639d4907e704e07d01f2822b546794fd7285b
Jesse_Carter <jessetcarter@hotmail.com>
2025-02-09 13:54:49 -0700

    M2364 solved TS

--- main.js
+++ main.js
@@ -1,52 +1,18 @@
 "use strict";
-class NumberContainers {
-    constructor(numberSystem) {
-        this.numberSystem = [];
-        this.firstIndexOfNumberInContainer = new Map();
-        this.numberSystem = numberSystem ?? [];
+function countBadPairs(nums) {
+    const N = nums.length;
+    const diffCount = new Map();
+    let result = 0;
+    for (let i = 0; i < N; i++) {
+        let diff = i - nums[i];
+        let goodPairsCount = diffCount.get(diff) || 0;
+        result += i - goodPairsCount;
+        diffCount.set(diff, ++goodPairsCount);
     }
-    change(index, number) {
-        const arrayStoreListIndexOfOldNumber = this.firstIndexOfNumberInContainer.get(this.numberSystem[index]) ?? [];
-        if (arrayStoreListIndexOfOldNumber.length) {
-            const idxRemove = arrayStoreListIndexOfOldNumber.indexOf(index);
-            arrayStoreListIndexOfOldNumber.splice(idxRemove, 1);
-            this.firstIndexOfNumberInContainer.set(this.numberSystem[index], arrayStoreListIndexOfOldNumber);
-        }
-        this.numberSystem[index] = number;
-        if (!this.firstIndexOfNumberInContainer.get(number)?.length) {
-            this.firstIndexOfNumberInContainer.set(number, []);
-        }
-        const arrayStoreListIndexOfNumber = this.firstIndexOfNumberInContainer.get(number);
-        if (arrayStoreListIndexOfNumber !== undefined) {
-            let idxPush = arrayStoreListIndexOfNumber.findIndex(element => element > index);
-            if (idxPush === -1) {
-                idxPush = arrayStoreListIndexOfNumber.length;
-            }
-            arrayStoreListIndexOfNumber.splice(idxPush, 0, index);
-            this.firstIndexOfNumberInContainer.set(number, arrayStoreListIndexOfNumber);
-        }
-    }
-    find(number) {
-        return this.firstIndexOfNumberInContainer.get(number)?.[0] || -1;
-    }
+    console.log(diffCount);
+    console.log(result);
+    return result;
 }
-/**
- * Your NumberContainers object will be instantiated and called as such:
- * var obj = new NumberContainers()
- * obj.change(index,number)
- * var param_2 = obj.find(number)
- */
-var obj = new NumberContainers([]);
-console.log(obj.find(10));
-obj.change(2, 10);
-obj.change(1, 10);
-obj.change(3, 10);
-obj.change(5, 10);
-console.log(obj.find(10));
-obj.change(1, 20);
-console.log(obj.find(10));
-console.log(obj);
-var obj2 = new NumberContainers([]);
-console.log(obj2.find(10));
-obj2.change(1000000000, 10);
-console.log(obj2.find(10));
+;
+countBadPairs([4, 1, 3, 3]);
+countBadPairs([1, 2, 3, 4, 5]);
--- main.ts
+++ main.ts
@@ -1,57 +1,22 @@
-class NumberContainers {
-    numberSystem: number[] = [];
-    firstIndexOfNumberInContainer: Map<number, number[]> = new Map();
+function countBadPairs(nums: number[]): number {
+    const N: number = nums.length;
+    const diffCount = new Map()
+    let result: number = 0;
 
-    constructor(numberSystem: number[]) {
-        this.numberSystem = numberSystem ?? [];
+    for (let i = 0; i < N; i++) {
+        let diff: number = i - nums[i];
+
+        let goodPairsCount = diffCount.get(diff) || 0;
+
+        result += i - goodPairsCount;
+
+        diffCount.set(diff, ++goodPairsCount);
     }
 
-    change(index: number, number: number): void {
-        const arrayStoreListIndexOfOldNumber = this.firstIndexOfNumberInContainer.get(this.numberSystem[index]) ?? [];
-        if (arrayStoreListIndexOfOldNumber.length) {
-            const idxRemove = arrayStoreListIndexOfOldNumber.indexOf(index);
-            arrayStoreListIndexOfOldNumber.splice(idxRemove, 1);
-            this.firstIndexOfNumberInContainer.set(this.numberSystem[index], arrayStoreListIndexOfOldNumber);
-        }
-        this.numberSystem[index] = number;
-        if (!this.firstIndexOfNumberInContainer.get(number)?.length) {
-            this.firstIndexOfNumberInContainer.set(number, []);
-        }
-        const arrayStoreListIndexOfNumber = this.firstIndexOfNumberInContainer.get(number);
-        if (arrayStoreListIndexOfNumber !== undefined) {
-            let idxPush = arrayStoreListIndexOfNumber.findIndex(element => element > index);
-            if (idxPush === -1) {
-                idxPush = arrayStoreListIndexOfNumber.length;
-            }
-            arrayStoreListIndexOfNumber.splice(idxPush, 0, index);
-            this.firstIndexOfNumberInContainer.set(number, arrayStoreListIndexOfNumber);
-        }
-    }
+    console.log(diffCount);
+    console.log(result);
+    return result;
+};
 
-    find(number: number): number {
-        return this.firstIndexOfNumberInContainer.get(number)?.[0] || -1;
-    }
-}
-
-/**
- * Your NumberContainers object will be instantiated and called as such:
- * var obj = new NumberContainers()
- * obj.change(index,number)
- * var param_2 = obj.find(number)
- */
-
-var obj = new NumberContainers([])
-console.log(obj.find(10));
-obj.change(2,10)
-obj.change(1,10)
-obj.change(3,10)
-obj.change(5,10)
-console.log(obj.find(10));
-obj.change(1,20)
-console.log(obj.find(10));
-console.log(obj);
-
-var obj2 = new NumberContainers([]);
-console.log(obj2.find(10));
-obj2.change(1000000000,10)
-console.log(obj2.find(10));
+countBadPairs([4,1,3,3]);
+countBadPairs([1,2,3,4,5]);
