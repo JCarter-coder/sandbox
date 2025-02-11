553712364f4532fbb2583eb08557c589614806ed
Jesse_Carter <jessetcarter@hotmail.com>
2025-02-10 20:16:10 -0700

    M1910 solved TS

--- main.js
+++ main.js
@@ -1,17 +1,56 @@
 "use strict";
-function clearDigits(s) {
-    const resultQueue = new Array();
-    for (let i = 0; i < s.length; i++) {
-        if (!Number.isInteger(+s[i])) {
-            resultQueue.push(s[i]);
+function removeOccurrences(s, part) {
+    const computeLongestPrefixSuffix = (pattern) => {
+        const lps = new Array(pattern.length).fill(0);
+        for (let current = 1, prefixLength = 0; current < pattern.length;) {
+            if (pattern.charAt(current) === pattern.charAt(prefixLength)) {
+                lps[current] = ++prefixLength;
+                current++;
+            }
+            else if (prefixLength !== 0) {
+                prefixLength = lps[prefixLength - 1];
+            }
+            else {
+                lps[current] = 0;
+                current++;
+            }
+        }
+        return lps;
+    };
+    const kmpLPS = computeLongestPrefixSuffix(part);
+    const charStack = new Array();
+    const patternIndexes = new Array(s.length + 1);
+    for (let strIndex = 0, patternIndex = 0; strIndex < s.length; strIndex++) {
+        let currentChar = s.charAt(strIndex);
+        charStack.push(currentChar);
+        if (currentChar === part.charAt(patternIndex)) {
+            patternIndexes[charStack.length] = ++patternIndex;
+            if (patternIndex === part.length) {
+                let remainingLength = part.length;
+                while (remainingLength > 0) {
+                    charStack.pop();
+                    remainingLength--;
+                }
+                patternIndex = (charStack.length !== 0) ?
+                    patternIndexes[charStack.length] :
+                    0;
+            }
         }
         else {
-            resultQueue.pop();
+            if (patternIndex !== 0) {
+                strIndex--;
+                patternIndex = kmpLPS[patternIndex - 1];
+                charStack.pop();
+            }
+            else {
+                patternIndexes[charStack.length] = 0;
+            }
         }
     }
-    console.log(resultQueue.join(""));
-    return resultQueue.join("");
+    //console.log(startIndexForPart);
+    console.log(charStack.join(""));
+    return charStack.join("");
 }
 ;
-clearDigits("abc");
-clearDigits("cb34");
+removeOccurrences("daabcbaabcbc", "abc");
+removeOccurrences("axxxxyyyyb", "xy");
--- main.ts
+++ main.ts
@@ -1,16 +1,64 @@
-function clearDigits(s: string): string {
-    const resultQueue = new Array();
-    
-    for (let i = 0; i < s.length; i++) {
-        if (!Number.isInteger(+s[i])) {
-            resultQueue.push(s[i]);
+function removeOccurrences(s: string, part: string): string {
+
+    const computeLongestPrefixSuffix = (pattern: string): number[] => {
+        const lps: number[] = new Array(pattern.length).fill(0);
+
+        for (let current = 1, prefixLength = 0; current < pattern.length;) {
+            if (pattern.charAt(current) === pattern.charAt(prefixLength)) {
+                lps[current] = ++prefixLength;
+                current++;
+            } else if (prefixLength !== 0) {
+                prefixLength = lps[prefixLength - 1];
+            } else {
+                lps[current] = 0;
+                current++;
+            }
+        }
+
+        return lps;
+    }
+
+    const kmpLPS: number[] = computeLongestPrefixSuffix(part);
+    const charStack: string[] = new Array();
+    const patternIndexes: number[] = new Array(s.length + 1);
+
+    for (
+        let strIndex: number = 0, patternIndex: number = 0; 
+        strIndex < s.length; 
+        strIndex++
+    ) {
+        let currentChar: string = s.charAt(strIndex);
+        charStack.push(currentChar);
+
+        if (currentChar === part.charAt(patternIndex)) {
+            patternIndexes[charStack.length] = ++patternIndex;
+
+            if (patternIndex === part.length) {
+                let remainingLength = part.length;
+                while (remainingLength > 0) {
+                    charStack.pop();
+                    remainingLength--;
+                }
+
+                patternIndex = (charStack.length !== 0) ?
+                    patternIndexes[charStack.length]:
+                    0;
+            }
         } else {
-            resultQueue.pop();
+            if (patternIndex !== 0) {
+                strIndex--;
+                patternIndex = kmpLPS[patternIndex - 1];
+                charStack.pop();
+            } else {
+                patternIndexes[charStack.length] = 0;
+            }
         }
     }
-    console.log(resultQueue.join(""));
-    return resultQueue.join("");
+
+    //console.log(startIndexForPart);
+    console.log(charStack.join(""));
+    return charStack.join("");
 };
 
-clearDigits("abc");
-clearDigits("cb34");
+removeOccurrences("daabcbaabcbc","abc");
+removeOccurrences("axxxxyyyyb","xy");
