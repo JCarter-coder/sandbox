fd5df9d5c0970b58066b5653a8d6f92d3ffcf95f
Jesse_Carter <jessetcarter@hotmail.com>
2025-02-14 10:41:55 -0700

    M1352 solved TS

--- main.ts
+++ main.ts
@@ -1,25 +1,26 @@
-function maximumSum(nums: number[]): number {
-    let result: number = -1;
-    const digitMapping: number[] = new Array(82).fill(0);
+class ProductOfNumbers {
+    numArray: number[];
 
-    for (let element of nums) {
-        let digitSum = 0;
-        
-        for (let currValue = element; currValue !== 0; currValue = Math.floor(currValue / 10)) {
-            let currDigit = currValue % 10;
-            digitSum += currDigit;
-        }
-
-        if (digitMapping[digitSum] > 0) {
-            result = Math.max(result, digitMapping[digitSum] + element);
-        }
-
-        digitMapping[digitSum] = Math.max(digitMapping[digitSum], element);
+    constructor() {
+        this.numArray = new Array();
     }
 
-    console.log(result);
-    return result;
-};
+    add(num: number): void {
+        this.numArray.push(num);
+    }
 
-maximumSum([18,43,36,13,7]);
-maximumSum([10,12,19,14]);
+    getProduct(k: number): number {
+        let product = 1;
+        for (let i = this.numArray.length - k; i < this.numArray.length; i++) {
+            product *= this.numArray[i];
+        }
+        return product;
+    }
+}
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * var obj = new ProductOfNumbers()
+ * obj.add(num)
+ * var param_2 = obj.getProduct(k)
+ */
