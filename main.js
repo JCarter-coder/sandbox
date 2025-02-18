a0e9f28288c96717d190e92c29cb2a7eae4f588b
Jesse_Carter <jessetcarter@hotmail.com>
2025-02-17 21:45:23 -0700

    M2375 solved TS

--- main.js
+++ main.js
@@ -1,37 +1,18 @@
 "use strict";
-function numTilePossibilities(tiles) {
-    const freq = {};
-    for (const tile of tiles) {
-        freq[tile] = (freq[tile] || 0) + 1;
-    }
-    const letters = Object.keys(freq);
-    let poly = [1];
-    for (const letter of letters) {
-        const f = freq[letter];
-        const series = [];
-        let fact = 1;
-        for (let j = 0; j <= f; j++) {
-            series.push(1 / fact);
-            fact *= (j + 1);
-        }
-        const newPoly = new Array(poly.length + f).fill(0);
-        for (let i = 0; i < poly.length; i++) {
-            for (let j = 0; j < series.length; j++) {
-                newPoly[i + j] += poly[i] * series[j];
+function smallestNumber(pattern) {
+    const numStack = new Array();
+    let result = "";
+    for (let i = 0; i <= pattern.length; i++) {
+        numStack.push(i + 1);
+        if (i === pattern.length || pattern.charAt(i) === 'I') {
+            while (numStack.length !== 0) {
+                result += numStack.pop();
             }
         }
-        poly = newPoly;
     }
-    let result = 0;
-    let factorial = 1;
-    for (let k = 1; k < poly.length; k++) {
-        factorial *= k;
-        result += poly[k] * factorial;
-    }
-    console.log(Math.round(result));
-    return Math.round(result);
+    console.log(result);
+    return result;
 }
 ;
-numTilePossibilities("AAB");
-numTilePossibilities("AAABBC");
-numTilePossibilities("V");
+smallestNumber("IIIDIDDD");
+smallestNumber("DDD");
--- main.ts
+++ main.ts
@@ -1,41 +1,20 @@
-function numTilePossibilities(tiles: string): number {
-    const freq: { [letter: string]: number } = {};
-    for (const tile of tiles) {
-        freq[tile] = (freq[tile] || 0) + 1;
-    }
-    const letters = Object.keys(freq);
+function smallestNumber(pattern: string): string {
+    const numStack: number[] = new Array();
+    let result: string = "";
 
-    let poly: number[] = [1];
+    for (let i = 0; i <= pattern.length; i++) {
+        numStack.push(i + 1);
 
-    for (const letter of letters) {
-        const f = freq[letter];
-        const series: number[] = [];
-        let fact = 1;
-        for (let j = 0; j <= f; j++) {
-            series.push(1 / fact);
-            fact *= (j + 1);
-        }
-
-        const newPoly: number[] = new Array(poly.length + f).fill(0);
-        for (let i = 0; i < poly.length; i++) {
-            for (let j = 0; j < series.length; j++) {
-                newPoly[i + j] += poly[i] * series[j];
+        if (i === pattern.length || pattern.charAt(i) === 'I') {
+            while (numStack.length !== 0) {
+                result += numStack.pop();
             }
         }
-        poly = newPoly;
     }
 
-    let result = 0;
-    let factorial = 1;
-    for (let k = 1; k < poly.length; k++) {
-        factorial *= k;
-        result += poly[k] * factorial;
-    }
-
-    console.log(Math.round(result));
-    return Math.round(result);
+    console.log(result);
+    return result;
 };
 
-numTilePossibilities("AAB");
-numTilePossibilities("AAABBC");
-numTilePossibilities("V");
+smallestNumber("IIIDIDDD");
+smallestNumber("DDD");
