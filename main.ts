9592a8f6d777f44050f286cde807875233734c9d
Jesse_Carter <jessetcarter@hotmail.com>
2025-02-23 00:42:51 -0700

    M889 solved TS

--- main.ts
+++ main.ts
@@ -9,43 +9,30 @@ class TreeNode {
     }
 }
 
-function recoverFromPreorder(traversal: string): TreeNode | null {
-    const depths = [new TreeNode()];
+function constructFromPrePost(preorder: number[], postorder: number[]): TreeNode | null {
+    let preIndex = 0;
+    let postIndex = 0;
 
-    const attach = (
-        str: string,
-        depths: TreeNode[],
-        depthI: number,
-        valueMinI: number,
-        valueMaxI: number
-    ): void => {
-        const depth = valueMinI - depthI;
-        const value = parseInt(str.substring(valueMinI, valueMaxI));
-        depths[depth + 1] = new TreeNode(value);
-        if (depths[depth].left === null) {
-            depths[depth].left = depths[depth + 1];
-        } else {
-            depths[depth].right = depths[depth + 1];
+    const constructTree = (
+        preorder: number[],
+        postorder: number[]
+    ): TreeNode => {
+        let root: TreeNode = new TreeNode(preorder[preIndex++]);
+
+        if (root.val !== postorder[postIndex]) {
+            root.left = constructTree(preorder, postorder);
         }
-    }
 
-    let depthI = 0;
-    let valueI = -1;
-    for (let i = 0; i < traversal.length; ++i) {
-        if (traversal[i] === '-') {
-            if (valueI >= depthI) {
-                attach(traversal, depths, depthI, valueI, i);
-                depthI = i;
-            }
-        } else if (valueI < depthI) {
-            valueI = i;
+        if (root.val !== postorder[postIndex]) {
+            root.right = constructTree(preorder, postorder);
         }
-    }
 
-    attach(traversal, depths, depthI, valueI, traversal.length);
-    return depths[1];
+        postIndex++;
+        return root;
+    }
+    
+    return constructTree(preorder, postorder);
 };
 
-recoverFromPreorder("1-2--3--4-5--6--7");
-recoverFromPreorder("1-2--3---4-5--6---7");
-recoverFromPreorder("1-401--349---90--88");
+constructFromPrePost([1,2,4,5,3,6,7],[4,5,2,6,7,3,1]);
+constructFromPrePost([1],[1]);
