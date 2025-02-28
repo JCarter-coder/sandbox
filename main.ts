fdae65b4437050931a47252d48ea4d97d362d55d
Jesse_Carter <jessetcarter@hotmail.com>
2025-02-27 21:39:31 -0700

    H1092 solved TS

--- main.ts
+++ main.ts
@@ -1,26 +1,66 @@
-function findMaxConsecutiveOnes(nums: number[]): number {
-    let index: number = 0;
-    let zeroSeen: boolean = false;
-    let prevZeroIndex: number = 0;
-    let maxResult: number = 0;
+function shortestCommonSupersequence(str1: string, str2: string): string {
+    let str1Length: number = str1.length;
+    let str2Length: number = str2.length;
 
-    let currentMax: number = 0;
-    while (index < nums.length) {
-        if (nums[index] === 0 && zeroSeen === false) {
-            zeroSeen = true;
-            prevZeroIndex = index;
-        } else if (nums[index] === 0 && zeroSeen === true) {
-            currentMax = index - prevZeroIndex - 1;
-            prevZeroIndex = index;
-        }
-        currentMax++;
-        maxResult = Math.max(maxResult, currentMax);
-        index++;
+    const dp: number[][] = Array.from(
+        {length: str1Length + 1}, () => Array(str2Length + 1).fill(0)
+    );
+
+    for (let row = 0; row <= str1Length; row++) {
+        dp[row][0] = row;
+    }
+    for (let col = 0; col <= str2Length; col++) {
+        dp[0][col] = col;
     }
 
-    console.log(maxResult);
-    return maxResult;
+    for (let row = 1; row <= str1Length; row++) {
+        for (let col = 1; col <= str2Length; col++) {
+            if (str1.charAt(row - 1) === str2.charAt(col - 1)) {
+                dp[row][col] = dp[row - 1][col - 1] + 1;
+            } else {
+                dp[row][col] = Math.min(
+                    dp[row - 1][col],
+                    dp[row][col - 1]
+                ) + 1;
+            }
+        }
+    }
+
+    const supersequenceQueue = new Array();
+    let row = str1Length;
+    let col = str2Length;
+
+    while (row > 0 && col > 0) {
+        if (str1.charAt(row - 1) === str2.charAt(col - 1)) {
+            supersequenceQueue.push(str1.charAt(row - 1));
+            row--;
+            col--;
+        } else if (dp[row - 1][col] < dp[row][col - 1]) {
+            supersequenceQueue.push(str1.charAt(row - 1));
+            row--;
+        } else {
+            supersequenceQueue.push(str2.charAt(col - 1));
+            col--;
+        }
+    }
+
+    while (row > 0) {
+        supersequenceQueue.push(str1.charAt(row - 1));
+        row--;
+    }
+    while (col > 0) {
+        supersequenceQueue.push(str2.charAt(col - 1));
+        col--;
+    }
+
+    let result: string = "";
+    while (supersequenceQueue.length > 0) {
+        result += supersequenceQueue.pop();
+    }
+
+    console.log(result);
+    return result;
 };
 
-findMaxConsecutiveOnes([1,0,1,1,0]);
-findMaxConsecutiveOnes([1,0,1,1,0,1]);
+shortestCommonSupersequence("abac","cab");
+shortestCommonSupersequence("aaaaaaaa","aaaaaaaa");
