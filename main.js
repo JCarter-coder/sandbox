c0cfb8d236779d5867e39fef363d985239c831a9
Jesse_Carter <jessetcarter@hotmail.com>
2025-03-02 20:36:28 -0700

    M2161 solved TS

--- main.js
+++ main.js
@@ -1,32 +1,25 @@
 "use strict";
-function mergeArrays(nums1, nums2) {
-    let index1 = 0;
-    let index2 = 0;
-    const result = new Array();
-    while (index1 < nums1.length && index2 < nums2.length) {
-        if (nums1[index1][0] === nums2[index2][0]) {
-            result.push([
-                nums1[index1][0],
-                nums1[index1][1] + nums2[index2][1]
-            ]);
-            index1++;
-            index2++;
+function pivotArray(nums, pivot) {
+    const lessThanPivot = new Array();
+    const pivotArray = new Array();
+    const greaterThanPivot = new Array();
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < pivot) {
+            lessThanPivot.push(nums[i]);
         }
-        else if (nums1[index1][0] < nums2[index2][0]) {
-            result.push(nums1[index1++]);
+        else if (nums[i] === pivot) {
+            pivotArray.push(nums[i]);
         }
-        else
-            result.push(nums2[index2++]);
+        else {
+            greaterThanPivot.push(nums[i]);
+        }
     }
-    while (index1 < nums1.length) {
-        result.push(nums1[index1++]);
-    }
-    while (index2 < nums2.length) {
-        result.push(nums2[index2++]);
-    }
-    console.log(result);
-    return result;
+    nums = lessThanPivot;
+    nums.push(...pivotArray);
+    nums.push(...greaterThanPivot);
+    console.log(nums);
+    return nums;
 }
 ;
-mergeArrays([[1, 2], [2, 3], [4, 5]], [[1, 4], [3, 2], [4, 1]]);
-mergeArrays([[2, 4], [3, 6], [5, 5]], [[1, 3], [4, 3]]);
+pivotArray([9, 12, 5, 10, 14, 3, 10], 10);
+pivotArray([-3, 4, 3, 2], 2);
--- main.ts
+++ main.ts
@@ -1,34 +1,24 @@
-function mergeArrays(nums1: number[][], nums2: number[][]): number[][] {
-    let index1: number = 0;
-    let index2: number = 0;
-    const result: number[][] = new Array();
+function pivotArray(nums: number[], pivot: number): number[] {
+    const lessThanPivot: number[] = new Array();
+    const pivotArray: number[] = new Array();
+    const greaterThanPivot: number[] = new Array();
 
-    while (index1 < nums1.length && index2 < nums2.length) {
-        if (nums1[index1][0] === nums2[index2][0]) {
-            result.push(
-                [
-                    nums1[index1][0],
-                    nums1[index1][1] + nums2[index2][1]
-                ]
-            )
-            index1++;
-            index2++;
-        } else if (nums1[index1][0] < nums2[index2][0]) {
-            result.push(nums1[index1++]);
-        } else result.push(nums2[index2++]);
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < pivot) {
+            lessThanPivot.push(nums[i]);
+        } else if (nums[i] === pivot) {
+            pivotArray.push(nums[i]);
+        } else {
+            greaterThanPivot.push(nums[i]);
+        }
     }
 
-    while (index1 < nums1.length) {
-        result.push(nums1[index1++]);
-    }
-
-    while (index2 < nums2.length) {
-        result.push(nums2[index2++]);
-    }
-
-    console.log(result);
-    return result;
+    nums = lessThanPivot;
+    nums.push(...pivotArray);
+    nums.push(...greaterThanPivot);
+    console.log(nums);
+    return nums;
 };
 
-mergeArrays([[1,2],[2,3],[4,5]],[[1,4],[3,2],[4,1]]);
-mergeArrays([[2,4],[3,6],[5,5]],[[1,3],[4,3]]);
+pivotArray([9,12,5,10,14,3,10],10);
+pivotArray([-3,4,3,2],2);
