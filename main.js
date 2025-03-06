e792f8bb926d6f1528895e32fbfc440d0cee6853
Jesse_Carter <jessetcarter@hotmail.com>
2025-03-05 21:50:23 -0700

    E2965 solved TS

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 "use strict";
-function checkPowersOfThree(n) {
-    while (n > 0) {
-        if (n % 3 === 2) {
-            console.log(false);
-            return false;
+function findMissingAndRepeatedValues(grid) {
+    const gridSize = grid.length;
+    const highestNumber = gridSize * gridSize;
+    const seen = new Set();
+    let doubleVal = -1;
+    let missingVal = -1;
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            if (seen.has(grid[i][j])) {
+                doubleVal = grid[i][j];
+            }
+            seen.add(grid[i][j]);
         }
-        n = Math.floor(n / 3);
     }
-    console.log(true);
-    return true;
+    for (let i = 1; i <= highestNumber; i++) {
+        if (!seen.has(i)) {
+            missingVal = i;
+            break;
+        }
+    }
+    console.log([doubleVal, missingVal]);
+    return [doubleVal, missingVal];
 }
 ;
-checkPowersOfThree(12);
-checkPowersOfThree(91);
-checkPowersOfThree(21);
+findMissingAndRepeatedValues([[1, 3], [2, 2]]);
+findMissingAndRepeatedValues([[9, 1, 7], [8, 9, 2], [3, 4, 6]]);
--- main.ts
+++ main.ts
@@ -1,18 +1,29 @@
-function checkPowersOfThree(n: number): boolean {
+function findMissingAndRepeatedValues(grid: number[][]): number[] {
+    const gridSize: number = grid.length;
+    const highestNumber: number = gridSize * gridSize;
+    const seen = new Set();
+    let doubleVal: number = -1;
+    let missingVal: number = -1;
 
-    while (n > 0) {
-        if (n % 3 === 2) {
-            console.log(false);
-            return false;
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            if (seen.has(grid[i][j])) {
+                doubleVal = grid[i][j];
+            }
+            seen.add(grid[i][j]);
         }
-
-        n = Math.floor(n / 3);
     }
 
-    console.log(true);
-    return true;
+    for (let i = 1; i <= highestNumber; i++) {
+        if (!seen.has(i)) {
+            missingVal = i;
+            break;
+        }
+    }
+
+    console.log([doubleVal, missingVal]);
+    return [doubleVal, missingVal];
 };
 
-checkPowersOfThree(12);
-checkPowersOfThree(91);
-checkPowersOfThree(21);
+findMissingAndRepeatedValues([[1,3],[2,2]]);
+findMissingAndRepeatedValues([[9,1,7],[8,9,2],[3,4,6]]);
