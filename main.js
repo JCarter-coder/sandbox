f67c733ec2fcc6759ce4e7e15013e709f253924a
Jesse_Carter <jessetcarter@hotmail.com>
2025-03-23 22:15:26 -0600

    M3169 solved TS

--- main.js
+++ main.js
@@ -1,59 +1,21 @@
-import { PriorityQueue } from "./node_modules/datastructures-js/index.js";
-function countPaths(n, roads) {
-    const MOD = 1000000007;
-    const graph = new Map();
-    for (let i = 0; i < n; i++) {
-        graph.set(i, new Array());
-    }
-    for (let road of roads) {
-        let startNode = road[0];
-        let endNode = road[1];
-        let travelTime = road[2];
-        graph.get(startNode).push([endNode, travelTime]);
-        graph.get(endNode).push([startNode, travelTime]);
-    }
-    const shortestTime = new Array(n).fill(Number.MAX_SAFE_INTEGER);
-    const pathCount = new Array(n).fill(0);
-    shortestTime[0] = 0;
-    pathCount[0] = 1;
-    const minHeap = new PriorityQueue((a, b) => a[0] - b[0]);
-    minHeap.enqueue([0, 0]);
-    while (!minHeap.isEmpty()) {
-        let top = minHeap.dequeue();
-        let currTime = top[0];
-        let currNode = top[1];
-        if (currTime > shortestTime[currNode]) {
-            continue;
+"use strict";
+function countDays(days, meetings) {
+    let freeDays = 0;
+    let latestEnd = 0;
+    meetings.sort((a, b) => a[0] - b[0]);
+    for (let meeting of meetings) {
+        let start = meeting[0];
+        let end = meeting[1];
+        if (start > latestEnd + 1) {
+            freeDays += start - latestEnd - 1;
         }
-        for (let neighbor of graph.get(currNode)) {
-            let neighborNode = neighbor[0];
-            let roadTime = neighbor[1];
-            if (currTime + roadTime < shortestTime[neighborNode]) {
-                shortestTime[neighborNode] = currTime + roadTime;
-                pathCount[neighborNode] = pathCount[currNode];
-                minHeap.enqueue([shortestTime[neighborNode], neighborNode]);
-            }
-            else if (currTime + roadTime === shortestTime[neighborNode]) {
-                pathCount[neighborNode] = (pathCount[neighborNode] + pathCount[currNode]) % MOD;
-            }
-        }
+        latestEnd = Math.max(latestEnd, end);
     }
-    console.log(pathCount[n - 1]);
-    return pathCount[n - 1];
+    freeDays += days - latestEnd;
+    console.log(freeDays);
+    return freeDays;
 }
 ;
-countPaths(7, [
-    [0, 6, 7],
-    [0, 1, 2],
-    [1, 2, 3],
-    [1, 3, 3],
-    [6, 3, 3],
-    [3, 5, 1],
-    [6, 5, 1],
-    [2, 5, 1],
-    [0, 4, 5],
-    [4, 6, 2]
-]);
-countPaths(2, [
-    [1, 0, 10]
-]);
+countDays(10, [[5, 7], [1, 3], [9, 10]]);
+countDays(5, [[2, 4], [1, 3]]);
+countDays(6, [[1, 6]]);
