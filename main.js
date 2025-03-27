ad9fc2417740a3e8f9c617860f4ad5a8d0e8e339
Jesse_Carter <jessetcarter@hotmail.com>
2025-03-26 21:40:18 -0600

    M2780 solved TS

--- main.js
+++ main.js
@@ -1,22 +1,40 @@
 "use strict";
-function minOperations(grid, x) {
-    const numsArray = grid.flat().flat();
-    let result = 0;
-    numsArray.sort((a, b) => a - b);
-    let arrayLength = numsArray.length;
-    let meanIndex = Math.floor(arrayLength / 2);
-    let finalCommonNumber = numsArray[meanIndex];
-    for (let num of numsArray) {
-        if (num % x !== finalCommonNumber % x) {
-            console.log(-1);
-            return -1;
+function minimumIndex(nums) {
+    let x = nums[0];
+    let count = 0;
+    let xCount = 0;
+    let n = nums.length;
+    for (let num of nums) {
+        if (num === x) {
+            count++;
         }
-        result += Math.floor(Math.abs(finalCommonNumber - num) / x);
+        else {
+            count--;
+        }
+        if (count === 0) {
+            x = num;
+            count = 1;
+        }
     }
-    console.log(result);
-    return result;
+    for (let num of nums) {
+        if (num === x)
+            xCount++;
+    }
+    count = 0;
+    for (let i = 0; i < n; i++) {
+        if (nums[i] === x)
+            count++;
+        let remainingCount = xCount - count;
+        if (count * 2 > i + 1 &&
+            remainingCount * 2 > n - i - 1) {
+            console.log(i);
+            return i;
+        }
+    }
+    console.log(-1);
+    return -1;
 }
 ;
-minOperations([[2, 4], [6, 8]], 2);
-minOperations([[1, 5], [2, 3]], 1);
-minOperations([[1, 2], [3, 4]], 2);
+minimumIndex([1, 2, 2, 2]);
+minimumIndex([2, 1, 3, 1, 1, 1, 7, 1, 2, 1]);
+minimumIndex([3, 3, 3, 3, 7, 2, 2]);
