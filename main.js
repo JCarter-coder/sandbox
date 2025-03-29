13fb01af2f95c8c7234a52900e16e0d9960781a5
Jesse_Carter <jessetcarter@hotmail.com>
2025-03-29 00:52:53 -0600

    H2818 troubleshooting...

--- main.js
+++ main.js
@@ -1,82 +1,97 @@
 "use strict";
-class UnionFind {
-    constructor(n) {
-        this.parent = new Array(n).fill(-1);
-        this.size = new Array(n).fill(1);
-    }
-    find(node) {
-        if (this.parent[node] < 0)
-            return node;
-        return this.parent[node] = this.find(this.parent[node]);
-    }
-    union(nodeA, nodeB) {
-        let rootA = this.find(nodeA);
-        let rootB = this.find(nodeB);
-        if (rootA === rootB)
-            return false;
-        if (this.size[rootA] > this.size[rootB]) {
-            this.parent[rootB] = rootA;
-            this.size[rootA] += this.size[rootB];
-        }
-        else {
-            this.parent[rootA] = rootB;
-            this.size[rootB] += this.size[rootA];
-        }
-        return true;
-    }
-    getSize(node) {
-        return this.size[this.find(node)];
-    }
-}
-function maxPoints(grid, queries) {
-    const ROW_DIRECTIONS = [0, 0, 1, -1];
-    const COL_DIRECTIONS = [1, -1, 0, 0];
-    let rowCount = grid.length;
-    let colCount = grid[0].length;
-    let totalCells = rowCount * colCount;
-    const sortedQueries = new Array();
-    for (let i = 0; i < queries.length; i++) {
-        sortedQueries.push({ index: i, value: queries[i] });
-    }
-    sortedQueries.sort((a, b) => a.value - b.value);
-    const sortedCells = new Array(totalCells);
-    for (let row = 0; row < rowCount; row++) {
-        for (let col = 0; col < colCount; col++) {
-            sortedCells[row * colCount + col] = {
-                row: row,
-                col: col,
-                value: grid[row][col]
-            };
-        }
-    }
-    sortedCells.sort((a, b) => a.value - b.value);
-    let uf = new UnionFind(totalCells);
-    let result = new Array(queries.length);
-    let cellIndex = 0;
-    for (let query of sortedQueries) {
-        while (cellIndex < totalCells &&
-            sortedCells[cellIndex].value < query.value) {
-            let row = sortedCells[cellIndex].row;
-            let col = sortedCells[cellIndex].col;
-            let cellId = row * colCount + col;
-            for (let direction = 0; direction < 4; direction++) {
-                let newRow = row + ROW_DIRECTIONS[direction];
-                let newCol = col + COL_DIRECTIONS[direction];
-                if (newRow >= 0 &&
-                    newRow < rowCount &&
-                    newCol >= 0 &&
-                    newCol < colCount &&
-                    grid[newRow][newCol] < query.value) {
-                    uf.union(cellId, newRow * colCount + newCol);
-                }
+function maximumScore(nums, k) {
+    let MOD = 10e9 + 7;
+    const power = (base, exponent) => {
+        let res = 1;
+        while (exponent > 0) {
+            if (exponent % 2 === 1) {
+                res = (res * base) % MOD;
             }
-            cellIndex++;
+            base = (base * base) % MOD;
+            exponent = Math.floor(exponent / 2);
         }
-        result[query.index] = query.value > grid[0][0] ? uf.getSize(0) : 0;
+        return res;
+    };
+    const getPrimes = (limit) => {
+        const isPrime = new Array(limit + 1).fill(true);
+        const primes = new Set();
+        for (let num = 2; num <= limit; num++) {
+            if (!isPrime[num])
+                continue;
+            primes.add(num);
+            for (let multiple = num * num; multiple <= limit; multiple += num) {
+                isPrime[multiple] = false;
+            }
+        }
+        return primes;
+    };
+    let N = nums.length;
+    const primeScores = new Array(N).fill(0);
+    let maxElement = Math.max(...nums);
+    const primes = getPrimes(maxElement);
+    for (let i = 0; i < N; i++) {
+        let num = nums[i];
+        for (let prime of primes) {
+            if (prime * prime > num)
+                break;
+            if (num % prime !== 0)
+                continue;
+            primeScores[i]++;
+            while (num % prime === 0) {
+                num = Math.floor(num / prime);
+            }
+        }
+        if (num > 1)
+            primeScores[i]++;
     }
-    console.log(result);
-    return result;
+    let nextDominant = new Array(N).fill(N);
+    let prevDominant = new Array(N).fill(-1);
+    const decreasingPrimeScoreStack = new Array();
+    for (let i = 0; i < N; i++) {
+        while (decreasingPrimeScoreStack.length !== 0 &&
+            primeScores[decreasingPrimeScoreStack[decreasingPrimeScoreStack.length - 1]] < primeScores[i]) {
+            let topIndex = decreasingPrimeScoreStack.pop();
+            //console.log(`top Index: ${topIndex}`);
+            if (typeof topIndex === "number") {
+                nextDominant[topIndex] = i;
+            }
+        }
+        if (decreasingPrimeScoreStack.length !== 0) {
+            prevDominant[i] = decreasingPrimeScoreStack[decreasingPrimeScoreStack.length - 1];
+        }
+        decreasingPrimeScoreStack.push(i);
+    }
+    const numOfSubArrays = new Array(N);
+    for (let i = 0; i < N; i++) {
+        numOfSubArrays[i] = (nextDominant[i] - i) * (i - prevDominant[i]);
+    }
+    const sortedArray = new Array();
+    for (let i = 0; i < N; i++) {
+        sortedArray.push([nums[i], i]);
+    }
+    sortedArray.sort((a, b) => b[0] - a[0]);
+    //console.log(sortedArray);
+    let score = 1;
+    let processingIndex = 0;
+    //console.log(`numOfSubArrays: ${numOfSubArrays}`);
+    while (k > 0) {
+        let element = sortedArray[processingIndex++];
+        let num = element[0];
+        let index = element[1];
+        //console.log(`num: ${num}`);
+        //console.log(`index: ${index}`);
+        let operations = Math.min(k, numOfSubArrays[index]);
+        //console.log(`operations: ${operations}`);
+        //console.log(`score = ${score} * power(${num}, ${operations}) % MOD`);
+        score = (score * power(num, operations)) % MOD;
+        //console.log(`score: ${score}`);
+        //console.log(`operations: ${operations}`);
+        k -= operations;
+    }
+    console.log(score);
+    return score;
 }
 ;
-maxPoints([[1, 2, 3], [2, 5, 7], [3, 5, 1]], [5, 6, 2]);
-maxPoints([[5, 2, 1], [1, 1, 2]], [3]);
+maximumScore([8, 3, 9, 3, 8], 2);
+maximumScore([19, 12, 14, 6, 10, 18], 3);
+maximumScore([3289, 2832, 14858, 22011], 6);
