ead2051eb927fe888d132fa0a9563b3e829359d6
Jesse_Carter <jessetcarter@hotmail.com>
2025-03-29 15:59:34 -0600

    H2818 solved TS

--- main.ts
+++ main.ts
@@ -1,121 +1,72 @@
-function maximumScore(nums: number[], k: number): number {
-    let MOD = 10e9 + 7;
+const Z = 1e5 + 1;
+const primes: number[] = [];
+const f = Array<boolean>(Z).fill(true);
 
-    const power = (base: number, exponent: number): number => {
-        let res = 1;
-        while (exponent > 0) {
-            if (exponent % 2 === 1) {
-                res = (res * base) % MOD;
-            }
-            base = (base * base) % MOD;
-            exponent = Math.floor(exponent / 2);
-        }
-        return res;
+// Find all primes <= Z using the Sieve of Eratosthenes method.
+for (let i = 2; i < Z; i += i > 2 ? 2 : 1) {
+    if (!f[i]) continue;
+    primes.push(i);
+    for (let j = i * i; j < Z; j += i) {
+        f[j] = false;
     }
+}
 
-    const getPrimes = (limit: number): Set<number> => {
-        const isPrime: boolean[] = new Array(limit + 1).fill(true);
-        const primes: Set<number> = new Set();
-
-        for (let num = 2; num <= limit; num++) {
-            if (!isPrime[num]) continue;
-
-            primes.add(num);
-
-            for (
-                let multiple = num * num;
-                multiple <= limit;
-                multiple += num
-            ) {
-                isPrime[multiple] = false;
-            }
-        }
-
-        return primes;
+function getPrimeScore(x: number): number {
+    let score = 0;
+    for (const p of primes) {
+        if (x % p === 0) ++score;
+        else if (p > x) break;
     }
-
-    let N = nums.length;
-    const primeScores: number[] = new Array(N).fill(0);
-
-    let maxElement = Math.max(...nums);
-    const primes = getPrimes(maxElement);
-    
-    for (let i = 0; i < N; i++) {
-        let num = nums[i];
-
-        for (let prime of primes) {
-            if (prime * prime > num) break;
-            if (num % prime !== 0) continue;
-
-            primeScores[i]++;
-            while (num % prime === 0) {
-                num = Math.floor(num / prime);
-            }
-        }
-
-        if (num > 1) primeScores[i]++;
-    }
-
-    let nextDominant: number[] = new Array(N).fill(N);
-    let prevDominant: number[] = new Array(N).fill(-1);
-
-    const decreasingPrimeScoreStack: number[] = new Array();
-
-    for (let i = 0; i < N; i++) {
-        while (
-            decreasingPrimeScoreStack.length !== 0 &&
-            primeScores[decreasingPrimeScoreStack[decreasingPrimeScoreStack.length - 1]] < primeScores[i]
-        ) {
-            let topIndex = decreasingPrimeScoreStack.pop();
-            //console.log(`top Index: ${topIndex}`);
-            if (typeof topIndex === "number") {
-                nextDominant[topIndex] = i;
-            } 
-        }
-
-        if (decreasingPrimeScoreStack.length !== 0) {
-            prevDominant[i] = decreasingPrimeScoreStack[decreasingPrimeScoreStack.length - 1]
-        }
-
-        decreasingPrimeScoreStack.push(i);
-    }
-
-    const numOfSubArrays: number[] = new Array(N);
-    for (let i = 0; i < N; i++) {
-        numOfSubArrays[i] = (nextDominant[i] - i) * (i - prevDominant[i]);
-    }
-
-    const sortedArray = new Array();
-    for (let i = 0; i < N; i++) {
-        sortedArray.push([nums[i], i]);
-    }
-    sortedArray.sort((a,b) => b[0] - a[0]);
-    //console.log(sortedArray);
-
-    let score = 1;
-    let processingIndex = 0;
-
-    //console.log(`numOfSubArrays: ${numOfSubArrays}`);
-
-    while (k > 0) {
-        let element: number[] = sortedArray[processingIndex++];
-        let num = element[0];
-        let index = element[1];
-        //console.log(`num: ${num}`);
-        //console.log(`index: ${index}`);
-
-        let operations = Math.min(k, numOfSubArrays[index]);
-        //console.log(`operations: ${operations}`);
-        //console.log(`score = ${score} * power(${num}, ${operations}) % MOD`);
-        score = (score * power(num, operations)) % MOD;
-
-        //console.log(`score: ${score}`);
-        //console.log(`operations: ${operations}`);
-        k -= operations;
-    }
-
-    console.log(score);
     return score;
+}
+
+const primeScores = Array.from({ length: Z }, (_, i) => getPrimeScore(i));
+
+const BIG_MOD = BigInt(1_000_000_007);
+
+function bigPow(x: bigint, n: bigint): bigint {
+    let p = 1n;
+    while (n > 0) {
+        if (n & 1n) p = (p * x) % BIG_MOD;
+        x = (x * x) % BIG_MOD;
+        n /= 2n;
+    }
+    return p;
+}
+
+function maximumScore(nums: number[], k: number): number {
+    const n = nums.length;
+    
+    const stack = [-1];
+    
+    const cnts = Array<number>(n).fill(0);
+
+    for (let i = 0; i <= n; ++i) {
+        const primeScore = i < n ? primeScores[nums[i]] : Infinity;
+
+        let prevIdx = stack[stack.length - 1];
+        while (stack.length > 1 && primeScores[nums[prevIdx]] < primeScore) {
+            stack.pop();
+            const prevPrevIdx = stack[stack.length - 1];
+            cnts[prevIdx] = (i - prevIdx) * (prevIdx - prevPrevIdx);
+            prevIdx = prevPrevIdx;
+        }
+
+        stack.push(i);
+    }
+
+    let ans = 1n;
+
+    const indexes = nums.map((_, i) => i).sort((a, b) => nums[b] - nums[a]);
+    for (const i of indexes) {
+        const cnt = Math.min(cnts[i], k);
+        ans = (ans * bigPow(BigInt(nums[i]), BigInt(cnt))) % BIG_MOD;
+        k -= cnt;
+        if (k === 0) break;
+    }
+
+    console.log(Number(ans));
+    return Number(ans);
 };
 
 maximumScore([8,3,9,3,8],2);
