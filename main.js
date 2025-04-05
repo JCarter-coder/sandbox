494061322faff5cd2f2f2f3a53b592d6a0180b30
Jesse_Carter <jessetcarter@hotmail.com>
2025-04-04 22:12:15 -0600

    E1863 solved TS

--- main.js
+++ main.js
@@ -1,31 +1,14 @@
 "use strict";
-class TreeNode {
-    constructor(val, left, right) {
-        this.val = (val === undefined ? 0 : val);
-        this.left = (left === undefined ? null : left);
-        this.right = (right === undefined ? null : right);
+function subsetXORSum(nums) {
+    let total = 0;
+    for (let num of nums) {
+        total |= num;
     }
-}
-const dfs = (root) => {
-    if (!root) {
-        return [0, null];
-    }
-    let left = dfs(root.left);
-    let right = dfs(root.right);
-    if (left[0] > right[0]) {
-        return [left[0] + 1, left[1]];
-    }
-    if (left[0] < right[0]) {
-        return [right[0] + 1, right[1]];
-    }
-    return [left[0] + 1, root];
-};
-function lcaDeepestLeaves(root) {
-    let result = dfs(root)[1];
+    let result = total << (nums.length - 1);
     console.log(result);
     return result;
 }
 ;
-lcaDeepestLeaves([3, 5, 1, 6, 2, 0, 8, null, null, 7, 4]);
-lcaDeepestLeaves([1]);
-lcaDeepestLeaves([0, 1, 3, null, 2]);
+subsetXORSum([1, 3]);
+subsetXORSum([5, 1, 6]);
+subsetXORSum([3, 4, 5, 6, 7, 8]);
--- main.ts
+++ main.ts
@@ -1,37 +1,13 @@
-class TreeNode {
-    val: number
-    left: TreeNode | null
-    right: TreeNode | null
-    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.left = (left===undefined ? null : left)
-        this.right = (right===undefined ? null : right)
+function subsetXORSum(nums: number[]): number {
+    let total: number = 0;
+    for (let num of nums) {
+        total |= num;
     }
-}
-
-const dfs = (root: TreeNode | null): [number, TreeNode | null] => {
-    if (!root) {
-        return [0, null];
-    }
-
-    let left = dfs(root.left);
-    let right = dfs(root.right);
-
-    if (left[0] > right[0]) {
-        return [left[0] + 1, left[1]];
-    }
-    if (left[0] < right[0]) {
-        return [right[0] + 1, right[1]];
-    }
-    return [left[0] + 1, root];
-}
-
-function lcaDeepestLeaves(root: TreeNode | null): TreeNode | null {
-    let result = dfs(root)[1];
+    let result = total << (nums.length - 1);
     console.log(result);
     return result;
 };
 
-lcaDeepestLeaves([3,5,1,6,2,0,8,null,null,7,4]);
-lcaDeepestLeaves([1]);
-lcaDeepestLeaves([0,1,3,null,2]);
+subsetXORSum([1,3]);
+subsetXORSum([5,1,6]);
+subsetXORSum([3,4,5,6,7,8]);
