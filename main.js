7e95983eeb02267d3246490f949c8599d6c8b264
Jesse_Carter <jessetcarter@hotmail.com>
2025-04-21 20:37:55 -0600

    H2338 solved TS

--- main.js
+++ main.js
@@ -1,22 +1,51 @@
 "use strict";
-function numberOfArrays(differences, lower, upper) {
-    let x = 0;
-    let y = 0;
-    let current = 0;
-    for (let d of differences) {
-        current += d;
-        x = Math.min(x, current);
-        y = Math.max(y, current);
-        if (y - x > upper - lower) {
-            console.log(0);
-            return 0;
+function idealArrays(n, maxValue) {
+    let MOD = BigInt(1e9 + 7);
+    let MAX_N = 10010;
+    let MAX_P = 15;
+    const c = Array.from({ length: (MAX_N + MAX_P) }, () => Array(MAX_P + 1).fill(0));
+    const sieve = new Array(MAX_N).fill(0);
+    const ps = Array.from({ length: MAX_N }, () => []);
+    for (let i = 2; i < MAX_N; i++) {
+        if (sieve[i] === 0) {
+            for (let j = i; j < MAX_N; j += i) {
+                if (sieve[j] === 0) {
+                    sieve[j] = i;
+                }
+            }
         }
     }
-    let result = (upper - lower) - (y - x) + 1;
-    console.log(result);
-    return result;
+    for (let i = 2; i < MAX_N; i++) {
+        let x = i;
+        while (x > 1) {
+            let p = sieve[x];
+            let cnt = 0;
+            while (x % p === 0) {
+                x = Math.floor(x / p);
+                cnt++;
+            }
+            ps[i].push(cnt);
+        }
+    }
+    c[0][0] = 1;
+    for (let i = 1; i < MAX_N + MAX_P; i++) {
+        c[i][0] = 1;
+        for (let j = 1; j <= Math.min(i, MAX_P); j++) {
+            c[i][j] = (c[i - 1][j] + c[i - 1][j - 1]) % Number(MOD);
+        }
+    }
+    //solution
+    let ans = 0n;
+    for (let i = 1; i <= maxValue; i++) {
+        let mul = 1n;
+        for (let p of ps[i]) {
+            mul = (mul * BigInt(c[n + p - 1][p])) % MOD;
+        }
+        ans = (ans + mul) % MOD;
+    }
+    console.log(Number(ans));
+    return Number(ans);
 }
 ;
-numberOfArrays([1, -3, 4], 1, 6);
-numberOfArrays([3, -4, 5, 1, -2], -4, 5);
-numberOfArrays([4, -7, 2], 3, 6);
+idealArrays(2, 5);
+idealArrays(5, 3);
