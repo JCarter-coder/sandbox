a34d639dae7f6378a5f0b400f2398ff079e8fb47
Jesse_Carter <jessetcarter@hotmail.com>
2025-04-30 21:18:11 -0600

    H2071 solved TS

--- main.js
+++ main.js
@@ -1,21 +1,50 @@
-"use strict";
-function countSymmetricIntegers(low, high) {
-    let result = 0;
-    for (let i = low; i <= high; i++) {
-        if (i < 100 && i % 11 === 0) {
-            result++;
-        }
-        else if (1000 <= i && i < 10000) {
-            let left = Math.floor(i / 1000) + Math.floor((i % 1000) / 100);
-            let right = Math.floor((i % 100) / 10) + (i % 10);
-            if (left === right) {
-                result++;
+import { Deque } from "./node_modules/datastructures-js/index.js";
+function maxTaskAssign(tasks, workers, pills, strength) {
+    const check = (mid) => {
+        let p = pills;
+        let ws = new Deque();
+        let ptr = M - 1;
+        for (let i = mid - 1; i >= 0; --i) {
+            while (ptr >= M - mid && workers[ptr] + strength >= tasks[i]) {
+                ws.pushFront(workers[ptr]);
+                --ptr;
+            }
+            if (ws.isEmpty()) {
+                return false;
+            }
+            else if (ws.back() >= tasks[i]) {
+                ws.popBack();
+            }
+            else {
+                if (!p)
+                    return false;
+                --p;
+                ws.popFront();
             }
         }
+        return true;
+    };
+    tasks.sort((a, b) => a - b);
+    workers.sort((a, b) => a - b);
+    const N = tasks.length;
+    const M = workers.length;
+    let left = 1;
+    let right = Math.min(N, M);
+    let ans = 0;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (check(mid)) {
+            ans = mid;
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
     }
-    console.log(result);
-    return result;
+    console.log(ans);
+    return ans;
 }
 ;
-countSymmetricIntegers(1, 100);
-countSymmetricIntegers(1200, 1230);
+maxTaskAssign([3, 2, 1], [0, 3, 3], 1, 1);
+maxTaskAssign([5, 4], [0, 0, 0], 1, 5);
+maxTaskAssign([10, 15, 30], [0, 10, 10, 10, 10], 3, 10);
