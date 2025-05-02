d2567971624079f8601fb55f742311b5d3df9835
Jesse_Carter <jessetcarter@hotmail.com>
2025-05-02 17:55:20 -0600

    M838 solved TS

--- main.ts
+++ main.ts
@@ -1,49 +1,45 @@
-import { Deque } from "./node_modules/datastructures-js/index.js";
-
-function maxTaskAssign(tasks: number[], workers: number[], pills: number, strength: number): number {
-    const check = (mid: number): boolean => {
-        let p = pills;
-        let ws = new Deque<number>();
-        let ptr = M - 1;
-        for (let i = mid - 1; i >= 0; --i) {
-            while (ptr >= M - mid && workers[ptr] + strength >= tasks[i]) {
-                ws.pushFront(workers[ptr]);
-                --ptr;
-            }
-            if (ws.isEmpty()) {
-                return false;
-            } else if (ws.back() >= tasks[i]) {
-                ws.popBack();
-            } else {
-                if (!p) return false;
-                --p;
-                ws.popFront();
-            }
-        }
-        return true;
-    };
-
-    tasks.sort((a, b) => a - b);
-    workers.sort((a, b) => a - b);
-    const N = tasks.length;
-    const M = workers.length;
-
-    let left = 1;
-    let right = Math.min(N, M);
-    let ans = 0;
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        if (check(mid)) {
-            ans = mid;
-            left = mid + 1;
+function pushDominoes(dominoes: string): string {
+    const charArray = dominoes.split("");
+    const N = charArray.length;
+    const forces = new Array(N).fill(0);
+    let force = 0;
+    // Left to right
+    for (let i = 0; i < N; i++) {
+        if (charArray[i] === "R") {
+            force = N;
+        } else if (charArray[i] === "L") {
+            force = 0;
         } else {
-            right = mid - 1;
+            force = Math.max(force - 1, 0);
+        }
+        forces[i] += force;
+    }
+    force = 0;
+    // Right to left
+    for (let i = N - 1; i >= 0; i--) {
+        if (charArray[i] === "L") {
+            force = N;
+        } else if (charArray[i] === "R") {
+            force = 0;
+        } else {
+            force = Math.max(force - 1, 0);
+        }
+        forces[i] -= force;
+    }
+    // Build the result string
+    let result = "";
+    for (let f of forces) {
+        if (f > 0) {
+            result += "R";
+        } else if (f < 0) {
+            result += "L";
+        } else {
+            result += ".";
         }
     }
-    console.log(ans);
-    return ans;
+    console.log(result);
+    return result;
 };
 
-maxTaskAssign([3,2,1],[0,3,3],1,1);
-maxTaskAssign([5,4],[0,0,0],1,5);
-maxTaskAssign([10,15,30],[0,10,10,10,10],3,10);
+pushDominoes("RR.L");
+pushDominoes(".L.R...LR..L..");
