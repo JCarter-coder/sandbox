e33f2c0401d3ccbc5be63786e86632c52218dee9
Jesse_Carter <jessetcarter@hotmail.com>
2025-05-06 15:02:33 -0600

    E1920 solved TS

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
 "use strict";
-function numEquivDominoPairs(dominoes) {
-    let result = 0;
-    const hashSet = new Set();
-    for (let i = 0; i < dominoes.length; i++) {
-        if (!(hashSet.has(dominoes[i].toString()) ||
-            hashSet.has(dominoes[i].reverse().toString()))) {
-            hashSet.add(dominoes[i].toString());
-            hashSet.add(dominoes[i].reverse().toString());
-        }
-        else {
-            result++;
-        }
+function buildArray(nums) {
+    const result = new Array(nums.length);
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = nums[nums[i]];
     }
     console.log(result);
     return result;
 }
 ;
-numEquivDominoPairs([[1, 2], [2, 1], [3, 4], [5, 6]]);
-numEquivDominoPairs([[1, 2], [1, 2], [1, 1], [1, 2], [2, 2]]);
+buildArray([0, 2, 1, 5, 3, 4]);
+buildArray([5, 0, 1, 2, 3, 4]);
--- main.ts
+++ main.ts
@@ -1,18 +1,11 @@
-function numEquivDominoPairs(dominoes: number[][]): number {
-    let result = 0;
-    const num: number[] = new Array(100).fill(0);
-    for (let domino of dominoes) {
-        let val = (
-            domino[0] < domino[1] ? 
-            domino[0] * 10 + domino[1] :
-            domino[1] * 10 + domino[0]
-        );
-        result += num[val];
-        num[val]++;
+function buildArray(nums: number[]): number[] {
+    const result: number[] = new Array(nums.length);
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = nums[nums[i]];
     }
     console.log(result);
     return result;
 };
 
-numEquivDominoPairs([[1,2],[2,1],[3,4],[5,6]]);
-numEquivDominoPairs([[1,2],[1,2],[1,1],[1,2],[2,2]]);
+buildArray([0,2,1,5,3,4]);
+buildArray([5,0,1,2,3,4]);
