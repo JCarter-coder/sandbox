5a835420edd02b2f7047ee1dbf8760b00cfea7d3
Jesse_Carter <jessetcarter@hotmail.com>
2025-05-08 17:03:45 -0600

    M3342 solved TS

--- main.ts
+++ main.ts
@@ -17,12 +17,15 @@ function minTimeToReach(moveTime: number[][]): number {
     while (!queue.isEmpty()) {
         const s = queue.dequeue();
         if (visited[s.x][s.y]) continue;
+        if (s.x === N - 1 && s.y === M - 1) {
+            break;
+        }
         visited[s.x][s.y] = true;
         for (const [dx, dy] of directions) {
             const nx = s.x + dx;
             const ny = s.y + dy;
             if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-            const newDist = Math.max(d[s.x][s.y], moveTime[nx][ny]) + 1;
+            const newDist = Math.max(d[s.x][s.y], moveTime[nx][ny]) + ((s.x + s.y) % 2) + 1;
             if (d[nx][ny] > newDist) {
                 d[nx][ny] = newDist;
                 queue.enqueue({ x: nx, y: ny, dist: newDist });
@@ -35,5 +38,5 @@ function minTimeToReach(moveTime: number[][]): number {
 };
 
 minTimeToReach([[0,4],[4,4]]);
-minTimeToReach([[0,0,0],[0,0,0]]);
+minTimeToReach([[0,0,0,0],[0,0,0,0]]);
 minTimeToReach([[0,1],[1,2]]);
