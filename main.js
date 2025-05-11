e17f84ec0b2fec5880704dde29e8f59ee0dbaa8b
Jesse_Carter <jessetcarter@hotmail.com>
2025-05-10 19:30:37 -0600

    E1550 solved TS

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
 "use strict";
-function minSum(nums1, nums2) {
-    let sum1 = 0;
-    let nums1ZeroCount = 0;
-    for (let i = 0; i < nums1.length; i++) {
-        sum1 += nums1[i];
-        if (nums1[i] === 0) {
-            nums1ZeroCount++;
-            sum1++;
+function threeConsecutiveOdds(arr) {
+    let result = false;
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) {
+            count++;
+            if (count === 3) {
+                result = true;
+                break;
+            }
+        }
+        else {
+            count = 0;
         }
     }
-    let sum2 = 0;
-    let nums2ZeroCount = 0;
-    for (let i = 0; i < nums2.length; i++) {
-        sum2 += nums2[i];
-        if (nums2[i] === 0) {
-            nums2ZeroCount++;
-            sum2++;
-        }
-    }
-    if ((nums1ZeroCount === 0 && sum2 > sum1) || (nums2ZeroCount === 0 && sum1 > sum2)) {
-        console.log(-1);
-        return -1;
-    }
-    console.log(Math.max(sum1, sum2));
-    return Math.max(sum1, sum2);
+    console.log(result);
+    return result;
 }
 ;
-minSum([3, 2, 0, 1, 0], [6, 5, 0]);
-minSum([2, 0, 2, 0], [1, 4]);
+threeConsecutiveOdds([2, 6, 4, 1]);
+threeConsecutiveOdds([1, 2, 34, 3, 4, 5, 7, 23, 12]);
