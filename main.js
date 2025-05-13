91b9707e4e16bbe5c8a215e1612f96a629c00dd9
Jesse_Carter <jessetcarter@hotmail.com>
2025-05-13 17:49:15 -0600

    M3335 solved TS

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
 "use strict";
-function findEvenNumbers(digits) {
-    const digitSet = new Set([]);
-    for (let i = 0; i < digits.length; i++) {
-        if (digits[i] === 0) {
-            continue; // Skip zero
-        }
-        for (let j = 0; j < digits.length; j++) {
-            if (i === j) {
-                continue; // Skip same digit
-            }
-            for (let k = 0; k < digits.length; k++) {
-                if (i === k || j === k) {
-                    continue; // Skip same digit
-                }
-                const num = digits[i] * 100 + digits[j] * 10 + digits[k];
-                if (num % 2 === 0 && num >= 100 && !digitSet.has(num)) {
-                    digitSet.add(num);
-                }
-            }
-        }
+function lengthAfterTransformations(s, t) {
+    let MOD = 1000000007;
+    let n = s.length;
+    let cnt = new Array(26).fill(0);
+    for (let i = 0; i < n; i++) {
+        cnt[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
     }
-    // Convert Set to Array and sort it
-    const sortedArray = Array.from(digitSet).sort((a, b) => a - b);
-    //Array.from(digitSet).sort((a, b) => a - b);
-    console.log(sortedArray);
-    return sortedArray;
+    let ans = 0;
+    for (let round = 0; round < t; round++) {
+        let nxt = new Array(26).fill(0);
+        nxt[0] = cnt[25];
+        nxt[1] = (cnt[25] + cnt[0]) % MOD;
+        for (let i = 2; i < 26; i++) {
+            nxt[i] = cnt[i - 1];
+        }
+        cnt = nxt;
+    }
+    for (let i = 0; i < 26; i++) {
+        ans = (ans + cnt[i]) % MOD;
+    }
+    console.log(ans);
+    return ans;
 }
 ;
-findEvenNumbers([2, 1, 3, 0]);
-findEvenNumbers([2, 2, 8, 8, 2]);
-findEvenNumbers([3, 7, 5]);
+lengthAfterTransformations("abcyy", 2);
+lengthAfterTransformations("azbk", 1);
--- main.ts
+++ main.ts
@@ -1,31 +1,26 @@
-function findEvenNumbers(digits: number[]): number[] {
-    const digitSet = new Set<number>([]);
-    for (let i = 0; i < digits.length; i++) {
-        if (digits[i] === 0) {
-            continue; // Skip zero
-        }
-        for (let j = 0; j < digits.length; j++) {
-            if (i === j) {
-                continue; // Skip same digit
-            }
-            for (let k = 0; k < digits.length; k++) {
-                if (i === k || j === k) {
-                    continue; // Skip same digit
-                }
-                const num = digits[i] * 100 + digits[j] * 10 + digits[k];
-                if (num % 2 === 0 && num >= 100 && !digitSet.has(num)) {
-                    digitSet.add(num);
-                }
-            }
-        }
+function lengthAfterTransformations(s: string, t: number): number {
+    let MOD: number = 1000000007;
+    let n: number = s.length;
+    let cnt: number[] = new Array(26).fill(0);
+    for (let i: number = 0; i < n; i++) {
+        cnt[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
     }
-    // Convert Set to Array and sort it
-    const sortedArray = Array.from(digitSet).sort((a, b) => a - b);
-    //Array.from(digitSet).sort((a, b) => a - b);
-    console.log(sortedArray);
-    return sortedArray;
+    let ans: number = 0;
+    for (let round: number = 0; round < t; round++) {
+         let nxt: number[] = new Array(26).fill(0);
+         nxt[0] = cnt[25];
+         nxt[1] = (cnt[25] + cnt[0]) % MOD;
+         for (let i: number = 2; i < 26; i++) {
+             nxt[i] = cnt[i - 1];
+         }
+         cnt = nxt;
+    }
+    for (let i: number = 0; i < 26; i++) {
+        ans = (ans + cnt[i]) % MOD;
+    }
+    console.log(ans);
+    return ans;
 };
 
-findEvenNumbers([2,1,3,0]);
-findEvenNumbers([2,2,8,8,2]);
-findEvenNumbers([3,7,5]);
+lengthAfterTransformations("abcyy",2);
+lengthAfterTransformations("azbk",1);
