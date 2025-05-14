441c18327f9de3f517a15447d713ccf8dbb10fac
Jesse_Carter <jessetcarter@hotmail.com>
2025-05-14 12:47:35 -0600

    H3337 solved TS

--- main.js
+++ main.js
@@ -1,27 +1,69 @@
 "use strict";
-function lengthAfterTransformations(s, t) {
-    let MOD = 1000000007;
-    let n = s.length;
-    let cnt = new Array(26).fill(0);
-    for (let i = 0; i < n; i++) {
-        cnt[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
-    }
-    let ans = 0;
-    for (let round = 0; round < t; round++) {
-        let nxt = new Array(26).fill(0);
-        nxt[0] = cnt[25];
-        nxt[1] = (cnt[25] + cnt[0]) % MOD;
-        for (let i = 2; i < 26; i++) {
-            nxt[i] = cnt[i - 1];
+let MOD = BigInt(1000000007);
+let L = 26;
+class Mat {
+    constructor(copyFrom = null) {
+        this.a = Array.from({ length: L }, () => new Array(L).fill(BigInt(0)));
+        if (copyFrom) {
+            for (let i = 0; i < L; i++) {
+                for (let j = 0; j < L; j++) {
+                    this.a[i][j] = copyFrom.a[i][j];
+                }
+            }
         }
-        cnt = nxt;
     }
-    for (let i = 0; i < 26; i++) {
-        ans = (ans + cnt[i]) % MOD;
+    mul(other) {
+        const res = new Mat();
+        for (let i = 0; i < L; i++) {
+            for (let j = 0; j < L; j++) {
+                for (let k = 0; k < L; k++) {
+                    res.a[i][j] = (res.a[i][j] + this.a[i][k] * other.a[k][j]) % MOD;
+                }
+            }
+        }
+        return res;
+    }
+}
+function I() {
+    const m = new Mat();
+    for (let i = 0; i < L; i++) {
+        m.a[i][i] = 1n;
+    }
+    return m;
+}
+function quickMul(x, y) {
+    let res = I();
+    let cur = new Mat(x);
+    while (y > 0) {
+        if (y & 1) {
+            res = res.mul(cur);
+        }
+        cur = cur.mul(cur);
+        y >>= 1;
+    }
+    return res;
+}
+function lengthAfterTransformations(s, t, nums) {
+    const T = new Mat();
+    for (let i = 0; i < L; i++) {
+        for (let j = 1; j <= nums[i]; j++) {
+            T.a[(i + j) % L][i] = 1n;
+        }
+    }
+    const res = quickMul(T, t);
+    const f = new Array(L).fill(0n);
+    for (const ch of s) {
+        f[ch.charCodeAt(0) - "a".charCodeAt(0)] += 1n;
+    }
+    let ans = 0n;
+    for (let i = 0; i < L; i++) {
+        for (let j = 0; j < L; j++) {
+            ans = (ans + res.a[i][j] * f[j]) % MOD;
+        }
     }
     console.log(ans);
-    return ans;
+    return Number(ans);
 }
 ;
-lengthAfterTransformations("abcyy", 2);
-lengthAfterTransformations("azbk", 1);
+lengthAfterTransformations("abcyy", 2, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2]);
+lengthAfterTransformations("azbk", 1, [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]);
--- main.ts
+++ main.ts
@@ -1,26 +1,88 @@
-function lengthAfterTransformations(s: string, t: number): number {
-    let MOD: number = 1000000007;
-    let n: number = s.length;
-    let cnt: number[] = new Array(26).fill(0);
-    for (let i: number = 0; i < n; i++) {
-        cnt[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+let MOD = BigInt(1000000007);
+let L = 26;
+
+class Mat {
+    a: bigint[][];
+
+    constructor(copyFrom: Mat | null = null) {
+        this.a = Array.from({ length: L }, 
+            () => new Array(L).fill(BigInt(0))
+        );
+        
+        if (copyFrom) {
+            for (let i = 0; i < L; i++) {
+                for (let j = 0; j < L; j++) {
+                    this.a[i][j] = copyFrom.a[i][j];
+                }
+            }
+        }
     }
-    let ans: number = 0;
-    for (let round: number = 0; round < t; round++) {
-         let nxt: number[] = new Array(26).fill(0);
-         nxt[0] = cnt[25];
-         nxt[1] = (cnt[25] + cnt[0]) % MOD;
-         for (let i: number = 2; i < 26; i++) {
-             nxt[i] = cnt[i - 1];
-         }
-         cnt = nxt;
+
+    mul(other: Mat): Mat {
+        const res = new Mat();
+        for (let i = 0; i < L; i++) {
+            for (let j = 0; j < L; j++) {
+                for (let k = 0; k < L; k++) {
+                    res.a[i][j] = (res.a[i][j] + this.a[i][k] * other.a[k][j]) % MOD;
+                }
+            }
+        }
+        return res;
     }
-    for (let i: number = 0; i < 26; i++) {
-        ans = (ans + cnt[i]) % MOD;
+}
+
+function I(): Mat {
+    const m = new Mat();
+    for (let i = 0; i < L; i++) {
+        m.a[i][i] = 1n;
+    }
+    return m;
+}
+
+function quickMul(x: Mat, y: number): Mat {
+    let res = I();
+    let cur = new Mat(x);
+    while (y > 0) {
+        if (y & 1) {
+            res = res.mul(cur);
+        }
+        cur = cur.mul(cur);
+        y >>= 1;
+    }
+    return res;
+}
+    
+function lengthAfterTransformations(s: string, t: number, nums: number[]): number {
+    const T = new Mat();
+    for (let i = 0; i < L; i++) {
+        for (let j = 1; j <= nums[i]; j++) {
+            T.a[(i + j) % L][i] = 1n;
+        }
+    }
+
+    const res = quickMul(T, t);
+    const f: bigint[] = new Array(L).fill(0n);
+    for (const ch of s) {
+        f[ch.charCodeAt(0) - "a".charCodeAt(0)] += 1n;
+    }
+
+    let ans = 0n;
+    for (let i = 0; i < L; i++) {
+        for (let j = 0; j < L; j++) {
+            ans = (ans + res.a[i][j] * f[j]) % MOD;
+        }
     }
     console.log(ans);
-    return ans;
+    return Number(ans);
 };
 
-lengthAfterTransformations("abcyy",2);
-lengthAfterTransformations("azbk",1);
+lengthAfterTransformations(
+    "abcyy",
+    2,
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2]
+);
+lengthAfterTransformations(
+    "azbk",
+    1,
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
+);
