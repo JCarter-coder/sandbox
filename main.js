08ef83c4252d6da63727bcfee2674842ce67222e
Jesse_Carter <jessetcarter@hotmail.com>
2025-05-16 15:23:08 -0600

    M361 solved TS

--- main.js
+++ main.js
@@ -1,42 +1,45 @@
 "use strict";
-function getWordsInLongestSubsequence(words, groups) {
-    const N = words.length;
-    const dp = new Array(N).fill(1);
-    const prev = new Array(N).fill(-1);
-    const check = (s1, s2) => {
-        if (s1.length !== s2.length)
-            return false;
-        let diff = 0;
-        for (let i = 0; i < s1.length; i++) {
-            if (s1[i] !== s2[i]) {
-                if (++diff > 1)
-                    return false;
+function maxKilledEnemies(grid) {
+    if (grid.length === 0 || grid[0].length === 0) {
+        return 0;
+    }
+    let rows = grid.length;
+    let cols = grid[0].length;
+    let maxCount = 0;
+    let rowHits = 0;
+    let colHits = new Array(cols).fill(0);
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            if (col === 0 || grid[row][col - 1] === 'W') {
+                rowHits = 0;
+                for (let k = col; k < cols; k++) {
+                    if (grid[row][k] === 'W') {
+                        break;
+                    }
+                    else if (grid[row][k] === 'E') {
+                        rowHits++;
+                    }
+                }
+            }
+            if (row === 0 || grid[row - 1][col] === 'W') {
+                colHits[col] = 0;
+                for (let k = row; k < rows; k++) {
+                    if (grid[k][col] === 'W') {
+                        break;
+                    }
+                    else if (grid[k][col] === 'E') {
+                        colHits[col]++;
+                    }
+                }
+            }
+            if (grid[row][col] === '0') {
+                maxCount = Math.max(maxCount, rowHits + colHits[col]);
             }
         }
-        return diff === 1;
-    };
-    let maxIndex = 0;
-    for (let i = 1; i < N; i++) {
-        for (let j = 0; j < i; j++) {
-            if (check(words[i], words[j]) &&
-                dp[j] + 1 > dp[i] &&
-                groups[i] !== groups[j]) {
-                dp[i] = dp[j] + 1;
-                prev[i] = j;
-            }
-        }
-        if (dp[i] > dp[maxIndex]) {
-            maxIndex = i;
-        }
     }
-    const ans = [];
-    for (let i = maxIndex; i >= 0; i = prev[i]) {
-        ans.push(words[i]);
-    }
-    ans.reverse();
-    console.log(ans);
-    return ans;
+    console.log(maxCount);
+    return maxCount;
 }
 ;
-getWordsInLongestSubsequence(["bab", "dab", "cab"], [1, 2, 2]);
-getWordsInLongestSubsequence(["a", "b", "c", "d"], [1, 2, 3, 4]);
+maxKilledEnemies([["0", "E", "0", "0"], ["E", "0", "W", "E"], ["0", "E", "0", "0"]]);
+maxKilledEnemies([["W", "W", "W"], ["0", "0", "0"], ["E", "E", "E"]]);
