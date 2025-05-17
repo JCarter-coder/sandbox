a88fe9c904fd3d352a509638293fe9eb826f6283
Jesse_Carter <jessetcarter@hotmail.com>
2025-05-17 11:36:02 -0600

    M75 solved TS

--- main.ts
+++ main.ts
@@ -1,46 +1,24 @@
-function maxKilledEnemies(grid: string[][]): number {
-    if (grid.length === 0 || grid[0].length === 0) {
-        return 0;
-    }
-
-    let rows = grid.length;
-    let cols = grid[0].length;
-    let maxCount = 0;
-    let rowHits = 0;
-    let colHits = new Array(cols).fill(0);
-
-    for (let row = 0; row < rows; row++) {
-        for (let col = 0; col < cols; col++) {
-            if (col === 0 || grid[row][col - 1] === 'W') {
-                rowHits = 0;
-                for (let k = col; k < cols; k++) {
-                    if (grid[row][k] === 'W') {
-                        break;
-                    } else if (grid[row][k] === 'E') {
-                        rowHits++;
-                    }
-                }
-            }
-
-            if (row === 0 || grid[row - 1][col] === 'W') {
-                colHits[col] = 0;
-                for (let k = row; k < rows; k++) {
-                    if (grid[k][col] === 'W') {
-                        break;
-                    } else if (grid[k][col] === 'E') {
-                        colHits[col]++;
-                    }
-                }
-            }
-
-            if (grid[row][col] === '0') {
-                maxCount = Math.max(maxCount, rowHits + colHits[col]);
-            }
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function sortColors(nums: number[]): void {
+    let pointer0 = 0;
+    let pointer1 = 0;
+    let pointer2 = nums.length - 1;
+    while (pointer1 <= pointer2) {
+        if (nums[pointer1] === 0) {
+            [nums[pointer0], nums[pointer1]] = [nums[pointer1], nums[pointer0]];
+            pointer0++;
+            pointer1++;
+        } else if (nums[pointer1] === 1) {
+            pointer1++;
+        } else {
+            [nums[pointer1], nums[pointer2]] = [nums[pointer2], nums[pointer1]];
+            pointer2--;
         }
     }
-    console.log(maxCount);
-    return maxCount;
+    console.log(nums);
 };
 
-maxKilledEnemies([["0","E","0","0"],["E","0","W","E"],["0","E","0","0"]]);
-maxKilledEnemies([["W","W","W"],["0","0","0"],["E","E","E"]]);
+sortColors([2,0,2,1,1,0]);
+sortColors([2,0,1]);
