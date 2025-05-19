51c1d06e7e369efdeacedc5867ce05c9a710e368
Jesse_Carter <jessetcarter@hotmail.com>
2025-05-19 17:16:13 -0600

    E3024 solved TS

--- main.js
+++ main.js
@@ -1,65 +1,25 @@
 "use strict";
-function colorTheGrid(m, n) {
-    let MOD = 1000000007;
-    const valid = new Map();
-    const maskEnd = Math.pow(3, m);
-    for (let mask = 0; mask < maskEnd; mask++) {
-        const color = [];
-        let mm = mask;
-        for (let i = 0; i < m; i++) {
-            color.push(mm % 3);
-            mm = Math.floor(mm / 3);
-        }
-        let check = true;
-        for (let i = 0; i < m - 1; i++) {
-            if (color[i] === color[i + 1]) {
-                check = false;
-                break;
-            }
-        }
-        if (check) {
-            valid.set(mask, color);
-        }
+function triangleType(nums) {
+    let a = nums[0];
+    let b = nums[1];
+    let c = nums[2];
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        console.log("none");
+        return "none";
     }
-    const adjacent = new Map();
-    for (const [mask1, color1] of valid.entries()) {
-        for (const [mask2, color2] of valid.entries()) {
-            let check = true;
-            for (let i = 0; i < m; i++) {
-                if (color1[i] === color2[i]) {
-                    check = false;
-                    break;
-                }
-            }
-            if (check) {
-                if (!adjacent.has(mask1)) {
-                    adjacent.set(mask1, []);
-                }
-                adjacent.get(mask1).push(mask2);
-            }
-        }
+    else if (a === b && b === c) {
+        console.log("equilateral");
+        return "equilateral";
     }
-    let f = new Map();
-    for (const [mask, _] of valid.entries()) {
-        f.set(mask, 1);
+    else if (a === b || a === c || b === c) {
+        console.log("isosceles");
+        return "isosceles";
     }
-    for (let i = 1; i < n; i++) {
-        const g = new Map();
-        for (const [mask2, _] of valid.entries()) {
-            for (const mask1 of adjacent.get(mask2) || []) {
-                g.set(mask2, ((g.get(mask2) || 0) + (f.get(mask1)) % MOD));
-            }
-        }
-        f = g;
+    else {
+        console.log("scalene");
+        return "scalene";
     }
-    let ans = 0;
-    for (const num of f.values()) {
-        ans = (ans + num) % MOD;
-    }
-    console.log(ans);
-    return ans;
 }
 ;
-colorTheGrid(1, 1);
-colorTheGrid(1, 2);
-colorTheGrid(5, 5);
+triangleType([3, 3, 3]);
+triangleType([3, 4, 5]);
