0218f05f64dd5dac474a7ef9fabc424c0a8be2af
Jesse_Carter <jessetcarter@hotmail.com>
2025-05-26 17:30:07 -0600

    H1857 solved TS

--- main.js
+++ main.js
@@ -1,39 +1,44 @@
 "use strict";
-function longestPalindrome(words) {
-    const stringCount = {};
-    for (const word of words) {
-        console.log(word);
-        stringCount[word] = stringCount[word] + 1 || 1;
-    }
-    console.log(stringCount);
-    let result = 0;
-    let central = false;
-    Object.keys(stringCount).forEach((key) => {
-        if (key.charAt(0) === key.charAt(1)) {
-            console.log(key, stringCount[key]);
-            if (stringCount[key] % 2 === 0) {
-                result += stringCount[key];
-            }
-            else {
-                result += stringCount[key] - 1;
-                central = true;
+function largestPathValue(colors, edges) {
+    const dfs = (node, colors, adj, count, visit, inStack) => {
+        if (inStack[node])
+            return Number.MAX_VALUE;
+        if (visit[node])
+            return count[node][colors.charCodeAt(node) - 97];
+        inStack[node] = true;
+        visit[node] = true;
+        if (adj.has(node)) {
+            for (const neighbor of adj.get(node) || []) {
+                if (dfs(neighbor, colors, adj, count, visit, inStack) === Number.MAX_VALUE) {
+                    return Number.MAX_VALUE;
+                }
+                for (let i = 0; i < 26; i++) {
+                    count[node][i] = Math.max(count[node][i], count[neighbor][i]);
+                }
             }
         }
-        else if (key.charAt(0) < key.charAt(1)) {
-            const reverseKey = key.split('').reverse().join('');
-            if (stringCount[reverseKey]) {
-                result += Math.min(stringCount[key], stringCount[reverseKey]) * 2;
-            }
+        count[node][colors.charCodeAt(node) - 97]++;
+        inStack[node] = false;
+        return count[node][colors.charCodeAt(node) - 97];
+    };
+    const n = colors.length;
+    const adj = new Map();
+    for (const [u, v] of edges) {
+        if (!adj.has(u)) {
+            adj.set(u, []);
         }
-    });
-    if (central) {
-        result++;
+        adj.get(u).push(v);
     }
-    console.log(result * 2);
-    return result * 2;
+    const count = Array.from({ length: n }, () => Array(26).fill(0));
+    const visit = Array(n).fill(false);
+    const inStack = Array(n).fill(false);
+    let maxCount = 0;
+    for (let i = 0; i < n; i++) {
+        maxCount = Math.max(maxCount, dfs(i, colors, adj, count, visit, inStack));
+    }
+    console.log(maxCount === Number.MAX_VALUE ? -1 : maxCount);
+    return maxCount === Number.MAX_VALUE ? -1 : maxCount;
 }
 ;
-//longestPalindrome(["lc","cl","gg"]);
-//longestPalindrome(["ab","ty","yt","lc","cl","ab"]);
-//longestPalindrome(["cc","ll","xx"]);
-longestPalindrome(["dd", "aa", "bb", "dd", "aa", "dd", "bb", "dd", "aa", "cc", "bb", "cc", "dd", "cc"]);
+largestPathValue("abaca", [[0, 1], [0, 2], [2, 3], [3, 4]]);
+largestPathValue("a", [[0, 0]]);
--- main.ts
+++ main.ts
@@ -1,37 +1,54 @@
-function longestPalindrome(words: string[]): number {
-    const stringCount: {[key: string]: number } = {};
-    for (const word of words) {
-        console.log(word);
-        stringCount[word] = stringCount[word] + 1 || 1;
-    }
-    console.log(stringCount);
-    let result = 0;
-    let central = false;
-    Object.keys(stringCount).forEach((key) => {
-        if (key.charAt(0) === key.charAt(1)) {
-            console.log(key, stringCount[key]);
-            if (stringCount[key] % 2 === 0) {
-                result += stringCount[key];
-            }
-            else {
-                result += stringCount[key] - 1;
-                central = true;
-            }
-        } else if (key.charAt(0) < key.charAt(1)) {
-            const reverseKey = key.split('').reverse().join('');
-            if (stringCount[reverseKey]) {
-                result += Math.min(stringCount[key], stringCount[reverseKey]) * 2;
+function largestPathValue(colors: string, edges: number[][]): number {
+    const dfs = (
+        node: number,
+        colors: string,
+        adj: Map<number, number[]>,
+        count: number[][],
+        visit: boolean[],
+        inStack: boolean[]
+    ): number => {
+        if (inStack[node]) return Number.MAX_VALUE;
+        if (visit[node]) return count[node][colors.charCodeAt(node) - 97];
+
+        inStack[node] = true;
+        visit[node] = true;
+
+        if (adj.has(node)) {
+            for (const neighbor of adj.get(node) || []) {
+                if (dfs(neighbor, colors, adj, count, visit, inStack) === Number.MAX_VALUE) {
+                    return Number.MAX_VALUE;
+                }
+                for (let i = 0; i < 26; i++) {
+                    count[node][i] = Math.max(count[node][i], count[neighbor][i]);
+                }
             }
         }
-    })
-    if (central) {
-        result++;
+
+        count[node][colors.charCodeAt(node) - 97]++;
+        inStack[node] = false;
+
+        return count[node][colors.charCodeAt(node) - 97];
     }
-    console.log(result * 2);
-    return result * 2;
+
+    const n = colors.length;
+    const adj = new Map<number, number[]>();
+    for (const [u, v] of edges) {
+        if (!adj.has(u)) {
+            adj.set(u, []);
+        }
+        adj.get(u)!.push(v);
+    }
+
+    const count: number[][] = Array.from({ length: n }, () => Array(26).fill(0));
+    const visit: boolean[] = Array(n).fill(false);
+    const inStack: boolean[] = Array(n).fill(false);
+    let maxCount = 0;
+    for (let i = 0; i < n; i++) {
+        maxCount = Math.max(maxCount, dfs(i, colors, adj, count, visit, inStack));
+    }
+    console.log(maxCount === Number.MAX_VALUE ? -1 : maxCount);
+    return maxCount === Number.MAX_VALUE ? -1 : maxCount;
 };
 
-//longestPalindrome(["lc","cl","gg"]);
-//longestPalindrome(["ab","ty","yt","lc","cl","ab"]);
-//longestPalindrome(["cc","ll","xx"]);
-longestPalindrome(["dd","aa","bb","dd","aa","dd","bb","dd","aa","cc","bb","cc","dd","cc"]);
+largestPathValue("abaca",[[0,1],[0,2],[2,3],[3,4]]);
+largestPathValue("a",[[0,0]]);
