7ff3987153825f435ab21d5121bd6c35bd0def57
Jesse_Carter <jessetcarter@hotmail.com>
2025-05-28 08:43:35 -0600

    M3372 solved TS

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
 "use strict";
-function differenceOfSums(n, m) {
-    let num1 = 0;
-    let num2 = 0;
-    for (let i = 1; i <= n; i++) {
-        if (i % m === 0) {
-            num2 += i;
+function maxTargetNodes(edges1, edges2, k) {
+    function dfs(node, parent, children, k) {
+        if (k < 0) {
+            return 0;
         }
-        else {
-            num1 += i;
+        let res = 1;
+        for (const child of children[node]) {
+            if (child === parent) {
+                continue;
+            }
+            res += dfs(child, node, children, k - 1);
         }
+        return res;
     }
-    console.log(num1 - num2);
-    return num1 - num2;
+    function build(edges, k) {
+        const n = edges.length + 1;
+        const children = Array.from({ length: n }, () => []);
+        for (const [u, v] of edges) {
+            children[u].push(v);
+            children[v].push(u);
+        }
+        const res = Array(n);
+        for (let i = 0; i < n; i++) {
+            res[i] = dfs(i, -1, children, k);
+        }
+        return res;
+    }
+    const n = edges1.length + 1;
+    const count1 = build(edges1, k);
+    const count2 = build(edges2, k - 1);
+    const maxCount2 = Math.max(...count2);
+    const res = Array(n);
+    for (let i = 0; i < n; i++) {
+        res[i] = count1[i] + maxCount2;
+    }
+    console.log(res);
+    return res;
 }
 ;
-differenceOfSums(10, 3);
-differenceOfSums(5, 6);
-differenceOfSums(5, 1);
+maxTargetNodes([[0, 1], [0, 2], [2, 3], [2, 4]], [[0, 1], [0, 2], [0, 3], [2, 7], [1, 4], [4, 5], [4, 6]], 2);
+maxTargetNodes([[0, 1], [0, 2], [0, 3], [0, 4]], [[0, 1], [1, 2], [2, 3]], 1);
--- main.ts
+++ main.ts
@@ -1,17 +1,56 @@
-function differenceOfSums(n: number, m: number): number {
-    let num1 = 0;
-    let num2 = 0;
-    for (let i = 1; i <= n; i++) {
-        if (i % m === 0) {
-            num2 += i;
-        } else {
-            num1 += i;
+function maxTargetNodes(edges1: number[][], edges2: number[][], k: number): number[] {
+    function dfs(
+        node: number,
+        parent: number,
+        children: number[][],
+        k: number,
+    ): number {
+        if (k < 0) {
+            return 0;
         }
+        let res = 1;
+        for (const child of children[node]) {
+            if (child === parent) {
+                continue;
+            }
+            res += dfs(child, node, children, k - 1);
+        }
+        return res;
     }
-    console.log(num1 - num2);
-    return num1 - num2;
+
+    function build(edges: number[][], k: number): number[] {
+        const n = edges.length + 1;
+        const children: number[][] = Array.from({ length: n }, () => []);
+        for (const [u, v] of edges) {
+            children[u].push(v);
+            children[v].push(u);
+        }
+        const res: number[] = Array(n);
+        for (let i = 0; i < n; i++) {
+            res[i] = dfs(i, -1, children, k);
+        }
+        return res;
+    }
+
+    const n = edges1.length + 1;
+    const count1 = build(edges1, k);
+    const count2 = build(edges2, k - 1);
+    const maxCount2 = Math.max(...count2);
+    const res: number[] = Array(n);
+    for (let i = 0; i < n; i++) {
+        res[i] = count1[i] + maxCount2;
+    }
+    console.log(res);
+    return res;
 };
 
-differenceOfSums(10,3);
-differenceOfSums(5,6);
-differenceOfSums(5,1);
+maxTargetNodes(
+    [[0,1],[0,2],[2,3],[2,4]],
+    [[0,1],[0,2],[0,3],[2,7],[1,4],[4,5],[4,6]],
+    2
+);
+maxTargetNodes(
+    [[0,1],[0,2],[0,3],[0,4]],
+    [[0,1],[1,2],[2,3]],
+    1
+);
