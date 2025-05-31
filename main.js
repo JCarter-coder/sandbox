766cb68f9bacaa9a829956b2967f953e87aa1126
Jesse_Carter <jessetcarter@hotmail.com>
2025-05-31 09:05:16 -0600

    H3373 solved TS

--- main.js
+++ main.js
@@ -1,33 +1,38 @@
 "use strict";
-function closestMeetingNode(edges, node1, node2) {
-    const dfs = (node, edges, dist, visit) => {
-        visit[node] = true;
-        let neighbor = edges[node];
-        if (neighbor !== -1 && !visit[neighbor]) {
-            dist[neighbor] = dist[node] + 1;
-            dfs(neighbor, edges, dist, visit);
+function maxTargetNodes(edges1, edges2) {
+    const dfs = (node, parent, depth, children, color) => {
+        let res = 1 - (depth % 2);
+        color[node] = depth % 2;
+        for (const child of children[node]) {
+            if (child === parent)
+                continue;
+            res += dfs(child, node, depth + 1, children, color);
         }
+        return res;
     };
-    let n = edges.length;
-    const dist1 = Array(n).fill(Number.MAX_VALUE);
-    const dist2 = Array(n).fill(Number.MAX_VALUE);
-    dist1[node1] = 0;
-    dist2[node2] = 0;
-    const visit1 = Array(n).fill(false);
-    const visit2 = Array(n).fill(false);
-    dfs(node1, edges, dist1, visit1);
-    dfs(node2, edges, dist2, visit2);
-    let minDistNode = -1;
-    let minDistTillNow = Number.MAX_VALUE;
-    for (let currNode = 0; currNode < n; currNode++) {
-        if (minDistTillNow > Math.max(dist1[currNode], dist2[currNode])) {
-            minDistTillNow = Math.max(dist1[currNode], dist2[currNode]);
-            minDistNode = currNode;
+    const build = (edges, color) => {
+        const n = edges.length + 1;
+        const children = Array.from({ length: n }, () => []);
+        for (const [u, v] of edges) {
+            children[u].push(v);
+            children[v].push(u);
         }
+        const res = dfs(0, -1, 0, children, color);
+        return [res, n - res];
+    };
+    const n = edges1.length + 1;
+    const m = edges2.length + 1;
+    const color1 = Array(n).fill(0);
+    const color2 = Array(m).fill(0);
+    const count1 = build(edges1, color1);
+    const count2 = build(edges2, color2);
+    const res = new Array(n).fill(0);
+    for (let i = 0; i < n; i++) {
+        res[i] = count1[color1[i]] + Math.max(count2[0], count2[1]);
     }
-    console.log(minDistNode);
-    return minDistNode;
+    console.log(res);
+    return res;
 }
 ;
-closestMeetingNode([2, 2, 3, -1], 0, 1);
-closestMeetingNode([1, 2, -1], 0, 2);
+maxTargetNodes([[0, 1], [0, 2], [2, 3], [2, 4]], [[0, 1], [0, 2], [0, 3], [2, 7], [1, 4], [4, 5], [4, 6]]);
+maxTargetNodes([[0, 1], [0, 2], [0, 3], [0, 4]], [[0, 1], [1, 2], [2, 3]]);
--- main.ts
+++ main.ts
@@ -1,42 +1,47 @@
-function closestMeetingNode(edges: number[], node1: number, node2: number): number {
+function maxTargetNodes(edges1: number[][], edges2: number[][]): number[] {
     const dfs = (
         node: number,
-        edges: number[],
-        dist: number[],
-        visit: boolean[]
-    ): void => {
-        visit[node] = true;
-        let neighbor = edges[node];
-        if (neighbor !== -1 && !visit[neighbor]) {
-            dist[neighbor] = dist[node] + 1;
-            dfs(neighbor, edges, dist, visit);
+        parent: number,
+        depth: number,
+        children: number[][],
+        color: number[]
+    ): number => {
+        let res = 1 - (depth % 2);
+        color[node] = depth % 2;
+        for (const child of children[node]) {
+            if (child === parent) continue;
+            res += dfs(child, node, depth + 1, children, color);
         }
+        return res;
     }
 
-    let n = edges.length;
-    const dist1: number[] = Array(n).fill(Number.MAX_VALUE);
-    const dist2: number[] = Array(n).fill(Number.MAX_VALUE);
-    dist1[node1] = 0;
-    dist2[node2] = 0;
-
-    const visit1: boolean[] = Array(n).fill(false);
-    const visit2: boolean[] = Array(n).fill(false);
-
-    dfs(node1, edges, dist1, visit1);
-    dfs(node2, edges, dist2, visit2);
-
-    let minDistNode = -1;
-    let minDistTillNow = Number.MAX_VALUE;
-    for (let currNode = 0; currNode < n; currNode++) {
-        if (minDistTillNow > Math.max(dist1[currNode], dist2[currNode])) {
-            minDistTillNow = Math.max(dist1[currNode], dist2[currNode]);
-            minDistNode = currNode;
+    const build = (
+        edges: number[][],
+        color: number[]
+    ): number[] => {
+        const n = edges.length + 1;
+        const children: number[][] = Array.from({ length: n }, () => []);
+        for (const [u, v] of edges) {
+            children[u].push(v);
+            children[v].push(u);
         }
+        const res = dfs(0, -1, 0, children, color);
+        return [res, n - res];
     }
 
-    console.log(minDistNode);
-    return minDistNode;
+    const n = edges1.length + 1;
+    const m = edges2.length + 1;
+    const color1: number[] = Array(n).fill(0);
+    const color2: number[] = Array(m).fill(0);
+    const count1 = build(edges1, color1);
+    const count2 = build(edges2, color2);
+    const res: number[] = new Array(n).fill(0);
+    for (let i = 0; i < n; i++) {
+        res[i] = count1[color1[i]] + Math.max(count2[0], count2[1]);
+    }
+    console.log(res);
+    return res;
 };
 
-closestMeetingNode([2,2,3,-1],0,1);
-closestMeetingNode([1,2,-1],0,2);
+maxTargetNodes([[0,1],[0,2],[2,3],[2,4]],[[0,1],[0,2],[0,3],[2,7],[1,4],[4,5],[4,6]]);
+maxTargetNodes([[0,1],[0,2],[0,3],[0,4]],[[0,1],[1,2],[2,3]]);
