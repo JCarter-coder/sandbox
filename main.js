1a9ade6a03854d18e9f0154ee32e2b36ff710c4c
Jesse_Carter <jessetcarter@hotmail.com>
2025-06-02 16:50:05 -0600

    H135 solved TS

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
 "use strict";
-function distributeCandies(n, limit) {
-    const calculateCandies = (x) => {
-        if (x < 0)
-            return 0;
-        return Number(BigInt(x) * (BigInt(x) - 1n) / 2n);
+function candy(ratings) {
+    const count = (n) => {
+        return (n * (n + 1)) / 2;
     };
-    let result = (calculateCandies(n + 2) -
-        3 * calculateCandies(n - limit + 1) +
-        3 * calculateCandies(n - (limit + 1) * 2 + 2) -
-        calculateCandies(n - 3 * (limit + 1) + 2));
-    console.log(result);
-    return result;
+    if (ratings.length <= 1) {
+        console.log(ratings.length);
+        return ratings.length;
+    }
+    let candies = 0;
+    let up = 0;
+    let down = 0;
+    let oldSlope = 0;
+    for (let i = 1; i < ratings.length; i++) {
+        let newSlope = (ratings[i] > ratings[i - 1] ?
+            1 :
+            ratings[i] < ratings[i - 1] ?
+                -1 :
+                0);
+        if ((oldSlope > 0 && newSlope == 0) ||
+            (oldSlope < 0 && newSlope >= 0)) {
+            candies += count(up) + count(down) + Math.max(up, down);
+            up = 0;
+            down = 0;
+        }
+        if (newSlope > 0) {
+            up++;
+        }
+        else if (newSlope < 0) {
+            down++;
+        }
+        else {
+            candies++;
+        }
+        oldSlope = newSlope;
+    }
+    candies += count(up) + count(down) + Math.max(up, down) + 1;
+    console.log(candies);
+    return candies;
 }
 ;
-distributeCandies(5, 2);
-distributeCandies(3, 3);
+candy([1, 0, 2]);
+candy([1, 2, 2]);
--- main.ts
+++ main.ts
@@ -1,18 +1,45 @@
-function distributeCandies(n: number, limit: number): number {
-    const calculateCandies = (x: number): number => {
-        if (x < 0) return 0;
-        return Number(BigInt(x) * (BigInt(x) - 1n) / 2n);
+function candy(ratings: number[]): number {
+    const count = (n: number): number => {
+        return (n * (n + 1)) / 2;
     }
 
-    let result = (
-        calculateCandies(n + 2) -
-        3 * calculateCandies(n - limit + 1) +
-        3 * calculateCandies(n - (limit + 1) * 2 + 2) -
-        calculateCandies(n - 3 * (limit + 1) + 2)
-    );
-    console.log(result);
-    return result;
+    if (ratings.length <= 1) {
+        console.log(ratings.length);
+        return ratings.length;
+    }
+    let candies = 0;
+    let up = 0;
+    let down = 0;
+    let oldSlope = 0;
+    for (let i = 1; i < ratings.length; i++) {
+        let newSlope = (
+            ratings[i] > ratings[i - 1] ? 
+            1 :
+            ratings[i] < ratings[i - 1] ?
+            -1 :
+            0
+        );
+        if (
+            (oldSlope > 0 && newSlope == 0) ||
+            (oldSlope < 0 && newSlope >= 0)
+        ) {
+            candies += count(up) + count(down) + Math.max(up, down);
+            up = 0;
+            down = 0;
+        }
+        if (newSlope > 0) {
+            up++;
+        } else if (newSlope < 0) {
+            down++;
+        } else {
+            candies++;
+        }
+        oldSlope = newSlope;
+    }
+    candies += count(up) + count(down) + Math.max(up, down) + 1;
+    console.log(candies);
+    return candies;
 };
 
-distributeCandies(5,2);
-distributeCandies(3,3);
+candy([1,0,2]);
+candy([1,2,2]);
