f4fff2a15752261a4a2ac458b1ff2fa063a4f272
Jesse_Carter <jessetcarter@hotmail.com>
2025-06-13 16:44:56 -0600

    M2616 solved TS

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 "use strict";
-function maxAdjacentDistance(nums) {
-    let maxDistance = Math.abs(nums[0] - nums[nums.length - 1]);
-    for (let i = 0; i < nums.length - 1; i++) {
-        maxDistance = Math.max(maxDistance, Math.abs(nums[i] - nums[i + 1]));
+function minimizeMax(nums, p) {
+    const countValidPairs = (nums, threshold) => {
+        let index = 0;
+        let count = 0;
+        while (index < nums.length - 1) {
+            if (nums[index + 1] - nums[index] <= threshold) {
+                count++;
+                index++;
+            }
+            index++;
+        }
+        return count;
+    };
+    nums.sort((a, b) => a - b);
+    let n = nums.length;
+    let left = 0;
+    let right = nums[n - 1] - nums[0];
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        if (countValidPairs(nums, mid) >= p) {
+            right = mid;
+        }
+        else {
+            left = mid + 1;
+        }
     }
-    console.log(maxDistance);
-    return maxDistance;
+    console.log(left);
+    return left;
 }
 ;
-maxAdjacentDistance([1, 2, 4]);
-maxAdjacentDistance([-5, -10, -5]);
+minimizeMax([10, 1, 2, 7, 1, 3], 2);
+minimizeMax([4, 2, 1, 2], 1);
