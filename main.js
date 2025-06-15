01a13bdbf12107ebbbb19823c016e2af546601f1
Jesse_Carter <jessetcarter@hotmail.com>
2025-06-14 21:21:41 -0600

    M1432 solved TS

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
 "use strict";
-function minMaxDifference(num) {
-    let s = num.toString();
-    let t = s;
-    let pos = 0;
-    while (pos < s.length && s[pos] === '9') {
-        pos++;
+function maxDiff(num) {
+    const replace = (s, x, y) => s.split(x).join(y);
+    let minNum = num.toString();
+    let maxNum = num.toString();
+    for (let digit of maxNum) {
+        if (digit !== '9') {
+            maxNum = replace(maxNum, digit, '9');
+            break;
+        }
     }
-    if (pos < s.length) {
-        s = s.replace(new RegExp(s[pos], 'g'), '9');
+    for (let i = 0; i < minNum.length; i++) {
+        let digit = minNum[i];
+        if (i === 0) {
+            if (digit !== '1') {
+                minNum = replace(minNum, digit, '1');
+                break;
+            }
+        }
+        else {
+            if (digit !== '0' && digit !== minNum[0]) {
+                minNum = replace(minNum, digit, '0');
+                break;
+            }
+        }
     }
-    t = t.replace(new RegExp(t[0], 'g'), '0');
-    console.log(parseInt(s) - parseInt(t));
-    return parseInt(s) - parseInt(t);
+    console.log(parseInt(maxNum) - parseInt(minNum));
+    return parseInt(maxNum) - parseInt(minNum);
 }
 ;
-minMaxDifference(11891); // Expected output: 99009
-minMaxDifference(90); // Expected output: 99
+maxDiff(555);
+maxDiff(9);
+maxDiff(123456);
