c57d9b9829f3650abd7a3ac841c6f5fae6454007
Jesse_Carter <jessetcarter@hotmail.com>
2025-06-19 16:01:22 -0600

    M2294 solved TS

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 "use strict";
-function divideArray(nums, k) {
+function partitionArray(nums, k) {
     nums.sort((a, b) => a - b);
-    const result = [];
-    for (let i = 0; i < nums.length; i += 3) {
-        if (nums[i + 2] - nums[i] > k) {
-            console.log([]);
-            return [];
+    let result = 1;
+    let rec = nums[0];
+    for (let num of nums) {
+        if (num - rec > k) {
+            result++;
+            rec = num;
         }
-        result[i / 3] = [nums[i], nums[i + 1], nums[i + 2]];
     }
     console.log(result);
     return result;
 }
 ;
-divideArray([1, 3, 4, 8, 7, 9, 3, 5, 1], 2);
-divideArray([2, 4, 2, 2, 5, 2], 2);
-divideArray([4, 2, 9, 8, 2, 12, 7, 12, 10, 5, 8, 5, 5, 7, 9, 2, 5, 11], 14);
+partitionArray([3, 6, 1, 2, 5], 2);
+partitionArray([1, 2, 3], 1);
+partitionArray([2, 2, 4, 5], 0);
