5215788ba8cecf037d97458fbd24ebd1a5d5d8f9
Jesse_Carter <jessetcarter@hotmail.com>
2025-06-22 09:36:12 -0600

    M3085 solved TS

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 "use strict";
-function divideString(s, k, fill) {
-    const result = [];
-    const n = s.length;
-    for (let i = 0; i < n; i += k) {
-        const part = s.slice(i, i + k);
-        if (part.length < k) {
-            result.push(part + fill.repeat(k - part.length));
+function minimumDeletions(word, k) {
+    const cnt = new Map();
+    for (const c of word) {
+        cnt.set(c, (cnt.get(c) || 0) + 1);
+    }
+    let result = word.length;
+    for (const a of cnt.values()) {
+        let deleted = 0;
+        for (const b of cnt.values()) {
+            if (a > b) {
+                deleted += b;
+            }
+            else if (b > a + k) {
+                deleted += b - (a + k);
+            }
         }
-        else {
-            result.push(part);
-        }
+        result = Math.min(result, deleted);
     }
     console.log(result);
     return result;
 }
 ;
-divideString("abcdefghi", 3, "x"); // ["abc", "def", "ghi"]
-divideString("abcdefghij", 3, "x"); // ["abc", "def", "ghi", "jxx"]
+minimumDeletions("aabcaba", 0);
+minimumDeletions("dabdcbdcdcd", 2);
+minimumDeletions("aaabaaa", 2);
--- main.ts
+++ main.ts
@@ -1,17 +1,25 @@
-function divideString(s: string, k: number, fill: string): string[] {
-    const result: string[] = [];
-    const n = s.length;
-    for (let i = 0; i < n; i += k) {
-        const part = s.slice(i, i + k);
-        if (part.length < k) {
-            result.push(part + fill.repeat(k - part.length));
-        } else {
-            result.push(part);
+function minimumDeletions(word: string, k: number): number {
+    const cnt = new Map<string, number>();
+    for (const c of word) {
+        cnt.set(c, (cnt.get(c) || 0) + 1);
+    }
+    let result = word.length;
+    for (const a of cnt.values()) {
+        let deleted = 0;
+        for (const b of cnt.values()) {
+            if (a > b) {
+                deleted += b;
+            }
+            else if (b > a + k) {
+                deleted += b - (a + k);
+            }
         }
+        result = Math.min(result, deleted);
     }
     console.log(result);
     return result;
 };
 
-divideString("abcdefghi", 3, "x"); // ["abc", "def", "ghi"]
-divideString("abcdefghij", 3, "x"); // ["abc", "def", "ghi", "jxx"]
+minimumDeletions("aabcaba",0);
+minimumDeletions("dabdcbdcdcd",2);
+minimumDeletions("aaabaaa",2);
